a9e034845a908c001a74872308890211cc1694a5
Duco <duco@winterwerp.it>
2018-05-02 20:35:54 +0200

    Added empty console app

---
+++ src/Placeholder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Placeholder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
