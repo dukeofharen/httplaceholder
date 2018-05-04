c91c8d829c153cf8b26ebeede1d0d5076b79054f
Duco <duco@winterwerp.it>
2018-05-04 22:54:24 +0200

    Added logging for StubRequestExecutor

--- src/Placeholder.Implementation/Implementations/StubRequestExecutor.cs
+++ src/Placeholder.Implementation/Implementations/StubRequestExecutor.cs
@@ -3,56 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Placeholder.Models;
 
 namespace Placeholder.Implementation.Implementations
 {
    internal class StubRequestExecutor : IStubRequestExecutor
    {
+      private readonly ILogger<StubRequestExecutor> _logger;
       private readonly IServiceProvider _serviceProvider;
       private readonly IStubManager _stubContainer;
 
       public StubRequestExecutor(
+         ILogger<StubRequestExecutor> logger,
          IServiceProvider serviceProvider,
          IStubManager stubContainer)
       {
+         _logger = logger;
          _serviceProvider = serviceProvider;
          _stubContainer = stubContainer;
       }
 
       public async Task<ResponseModel> ExecuteRequestAsync()
       {
-         var conditionCheckers = (IEnumerable<IConditionChecker>)_serviceProvider.GetServices(typeof(IConditionChecker));
+         var conditionCheckers = ((IEnumerable<IConditionChecker>)_serviceProvider.GetServices(typeof(IConditionChecker))).ToArray();
+         _logger.LogInformation($"Following conditions found: {string.Join(", ", conditionCheckers.Select(c => c.GetType().ToString()))}");
+
          string[] stubIds = null;
          foreach (var checker in conditionCheckers)
          {
+            _logger.LogInformation($"Checking request with condition '{checker.GetType()}'.");
             stubIds = (await checker.ValidateAsync(stubIds)).ToArray();
             if (stubIds == null)
             {
                // If the resulting list is null, it means the check wasn't executed because it wasn't configured. Continue with the next condition.
+               _logger.LogInformation($"'{nameof(stubIds)}' array for condition '{checker.GetType()}' was null, which means the condition was not executed and not configured.");
                continue;
             }
 
             if (!stubIds.Any())
             {
                // If the resulting list is not null, but empty, the condition did not pass and the response should be returned prematurely.
-               // TODO sensible exception error
-               throw new Exception();
+               throw new Exception($"The '{nameof(stubIds)}' array for condition was empty, which means the condition was configured but the request did not pass.");
             }
          }
 
          if (stubIds == null)
          {
             // No conditions passed or no conditions configured. Throw an exception.
-            // TODO sensible exception error
-            throw new Exception();
+            throw new Exception($"'{nameof(stubIds)}' was null, which means no condition was passed or no conditions are configured.");
          }
 
          if (stubIds.Length > 1)
          {
             // Multiple conditions found; don't know which one to choose. Throw an exception.
-            // TODO sensible exception error
-            throw new Exception();
+            throw new Exception($"'{nameof(stubIds)}' contains '{stubIds.Length}' stub IDs, which means no choice can be made.");
          }
 
          string finalStubId = stubIds.Single();
@@ -61,19 +66,24 @@ namespace Placeholder.Implementation.Implementations
          var stub = _stubContainer.GetStubById(finalStubId);
          if (stub != null)
          {
+            _logger.LogInformation($"Stub with ID '{finalStubId}' found; returning response.");
             var response = new ResponseModel
             {
                StatusCode = 200
             };
 
             response.StatusCode = stub.Response?.StatusCode ?? 200;
+            _logger.LogInformation($"Found HTTP status code '{response.StatusCode}'.");
+
             response.Body = stub.Response?.Text;
+            _logger.LogInformation($"Found body '{response.Body}'");
 
             var stubResponseHeaders = stub.Response?.Headers;
             if (stubResponseHeaders != null)
             {
                foreach (var header in stubResponseHeaders)
                {
+                  _logger.LogInformation($"Found header '{header.Key}' with value '{header.Value}'.");
                   response.Headers.Add(header.Key, header.Value);
                }
             }
@@ -81,8 +91,7 @@ namespace Placeholder.Implementation.Implementations
             return response;
          }
 
-         // TODO throw exception of zo
-         throw new Exception();
+         throw new Exception($"Stub with ID '{finalStubId}' unexpectedly not found.");
       }
    }
 }
