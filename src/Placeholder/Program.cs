fb9daff1e49f8dab29c91dcd9f2fe849f3aeac70
Duco <duco@winterwerp.it>
2018-05-09 21:15:59 +0200

    Added facilities to assign custom port number

--- src/Placeholder/Program.cs
+++ src/Placeholder/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Placeholder.Utilities;
 
 namespace Placeholder
 {
@@ -20,11 +22,19 @@ namespace Placeholder
          }
       }
 
-      public static IWebHost BuildWebHost(string[] args) =>
-         WebHost.CreateDefaultBuilder(args)
+      public static IWebHost BuildWebHost(string[] args)
+      {
+         var argsDictionary = args.Parse();
+         int port = argsDictionary.GetValue("port", 5000);
+         return WebHost.CreateDefaultBuilder(args)
             .UseStartup<Startup>()
             .ConfigureAppConfiguration((hostingContext, config) => config.AddCommandLine(args))
-            .UseKestrel(options => options.AddServerHeader = false)
+            .UseKestrel(options =>
+            {
+               options.AddServerHeader = false;
+               options.Listen(IPAddress.Loopback, port);
+            })
             .Build();
+      }
    }
 }
