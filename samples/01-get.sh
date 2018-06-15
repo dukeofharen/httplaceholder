9bd0d3b12faec7d1fa23c157503ae3f8f43ff7fa
Duco <duco@winterwerp.it>
2018-06-15 23:14:55 +0200

    Updated samples with negative conditions

--- samples/01-get.sh
+++ samples/01-get.sh
@@ -5,6 +5,9 @@ echo "=========="
 curl -v "http://localhost:5000/users?id=14&filter=last_name"
 echo "=========="
 
+curl -v "http://localhost:5000/users?id=15&filter=last_name&last_name=Johnson"
+echo "=========="
+
 curl -v "http://localhost:5000/users?id=18"
 echo "=========="
 
