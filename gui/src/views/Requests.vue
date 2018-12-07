439462e185930737739d1c3a97ac1803e09c7af0
Duco <mail@ducode.org>
2018-12-07 15:58:37 +0100

    Added confirm on delete all requests in UI

--- gui/src/views/Requests.vue
+++ gui/src/views/Requests.vue
@@ -11,7 +11,7 @@
         </div>
       </div>
       <div class="col-md-1">
-        <a class="btn btn-primary" v-on:click="deleteAllRequests" title="Delete all requests"><span class="fa fa-trash">&nbsp;</span></a>
+        <a class="btn btn-danger" v-on:click="deleteAllRequests" title="Delete all requests"><span class="fa fa-trash">&nbsp;</span></a>
       </div>
       <div class="col-md-1">
         <a class="btn btn-success" v-on:click="getRequests" title="Refresh"><span class="fa fa-refresh">&nbsp;</span></a>
@@ -23,6 +23,7 @@
 
 <script>
 import Request from "@/components/Request";
+import resources from "@/resources";
 
 export default {
   name: "requests",
@@ -35,12 +36,12 @@ export default {
   components: {
     Request
   },
-  created () {
-    this.getRequests()
+  created() {
+    this.getRequests();
   },
   computed: {
     requests() {
-      return this.$store.getters.getRequests
+      return this.$store.getters.getRequests;
     }
   },
   methods: {
@@ -50,7 +51,7 @@ export default {
       } else {
         this.filteredRequests = this.requests.filter(
           r =>
-            r.executingStubId && r.executingStubId.includes(newValue) ||
+            (r.executingStubId && r.executingStubId.includes(newValue)) ||
             r.requestParameters.url.includes(newValue)
         );
       }
@@ -62,10 +63,12 @@ export default {
       }
     },
     deleteAllRequests() {
-      this.$store.dispatch('clearRequests')
+      if (confirm(resources.areYouSure)) {
+        this.$store.dispatch("clearRequests");
+      }
     },
     getRequests() {
-      this.$store.dispatch('getRequests')
+      this.$store.dispatch("getRequests");
     },
     clearInput() {
       this.searchTerm = "";
@@ -78,9 +81,9 @@ export default {
     $route(from, to) {
       this.handleUrlSearch();
     },
-    requests (newRequests) {
-      this.filteredRequests = newRequests
-      this.handleUrlSearch()
+    requests(newRequests) {
+      this.filteredRequests = newRequests;
+      this.handleUrlSearch();
     }
   }
 };
