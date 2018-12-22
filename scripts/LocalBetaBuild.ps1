6eb297a765f6768132659c9fd7cab9133754bda3
Duco <duco@winterwerp.it>
2018-12-23 00:04:46 +0100

    Added scripts for building and publishing NuGet package

--- scripts/LocalBetaBuild.ps1
+++ scripts/LocalBetaBuild.ps1
@@ -1,6 +1,7 @@
 $env:APPVEYOR_BUILD_NUMBER = "1234"
 $env:APPVEYOR_REPO_COMMIT_MESSAGE = "[beta] New version"
 $env:APPVEYOR_REPO_BRANCH = "dev"
+$env:nuget_api_key = "bla"
 
 . "$PSScriptRoot\Determine-Variables.ps1"
 . "$PSScriptRoot\Patch-Versions.ps1"
