2abd61e1ee9db1e51b43b76fb26e1c20248d27a5
Duco <duco@winterwerp.it>
2018-12-23 00:17:15 +0100

    .NET Core 2.2

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -36,7 +36,7 @@ foreach($unitTest in $unitTestProjects)
 
 # Generating swagger.json file
 Write-Host "Generating swagger.json file"
-$swaggerGenBinFile = Join-Path -Path $srcFolder "HttPlaceholder.SwaggerGenerator\bin\Debug\netcoreapp2.1\HttPlaceholder.SwaggerGenerator.dll"
+$swaggerGenBinFile = Join-Path -Path $srcFolder "HttPlaceholder.SwaggerGenerator\bin\Debug\netcoreapp2.2\HttPlaceholder.SwaggerGenerator.dll"
 & dotnet $swaggerGenBinFile
 Assert-Cmd-Ok
 
