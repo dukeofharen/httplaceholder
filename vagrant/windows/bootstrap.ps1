f00b648d85b91dbc38070562156345f6632d4522
Duco <duco@winterwerp.it>
2019-02-22 21:37:35 +0100

    Added bootstrap.ps1 for Windows vagrant box for opening ports for HttPlaceholder

---
+++ vagrant/windows/bootstrap.ps1
@@ -0,0 +1,2 @@
+netsh advfirewall firewall add rule name="Open Port 4567" dir=in action=allow protocol=TCP localport=4567
+netsh advfirewall firewall add rule name="Open Port 5000" dir=in action=allow protocol=TCP localport=5000
