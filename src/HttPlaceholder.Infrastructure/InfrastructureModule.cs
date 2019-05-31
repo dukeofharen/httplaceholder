947abd14c41e4578bd958be65d30dc681c841a24
Duco <duco@winterwerp.it>
2019-05-31 20:52:41 +0200

    Introduced Common and Infrastructure projects

---
+++ src/HttPlaceholder.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,14 @@
+﻿using HttPlaceholder.Common;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HttPlaceholder.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfrastructureModule(this IServiceCollection services)
+        {
+            services.AddSingleton<IDateTime, MachineDateTime>();
+            return services;
+        }
+    }
+}
