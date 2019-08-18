e133ce09f1e272029646ed12c0b19947deb2d2b0
Duco <git@ducode.org>
2019-08-18 19:28:09 +0200

    Added handler for query parameters

---
+++ src/HttPlaceholder.Application/StubExecution/RequestStubGeneration/Implementations/QueryParamHandler.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using HttPlaceholder.Domain;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace HttPlaceholder.Application.StubExecution.RequestStubGeneration.Implementations
+{
+    public class QueryParamHandler : IRequestStubGenerationHandler
+    {
+        public Task<bool> HandleStubGenerationAsync(RequestResultModel request, StubModel stub)
+        {
+            var uri = new Uri(request.RequestParameters.Url);
+            var query = QueryHelpers.ParseQuery(uri.Query);
+            if (!query.Any())
+            {
+                return Task.FromResult(false);
+            }
+
+            stub.Conditions.Url.Query = query.ToDictionary(q => q.Key, q => q.Value.ToString());
+            return Task.FromResult(true);
+        }
+    }
+}
