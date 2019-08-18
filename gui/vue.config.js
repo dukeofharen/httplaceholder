c1cd2aa95d322da5af3b515f7e0c9654340b4412
Duco <git@ducode.org>
2019-08-18 15:47:37 +0200

    Fixed stupid portfinder issue: see https://github.com/vuejs/vue-cli/issues/4452

--- gui/vue.config.js
+++ gui/vue.config.js
@@ -7,6 +7,7 @@ module.exports = {
     config.resolve.alias.urls = process.env.NODE_ENV === 'production' ? path.join(__dirname, 'urls.prod') : path.join(__dirname, 'urls.dev')
   },
   devServer: {
+    port: 8080,
     proxy: {
       '/ph-api': {
         target: 'http://localhost:5000'
