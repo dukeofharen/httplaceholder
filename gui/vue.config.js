11291bb2b4ff05e4513c9e2ebbca1db2fd4b7596
Duco <git@ducode.org>
2020-04-17 11:09:09 +0200

    Updated HTML title

--- gui/vue.config.js
+++ gui/vue.config.js
@@ -1,24 +1,32 @@
 var path = require("path");
 
 module.exports = {
-  lintOnSave: false,
-  publicPath: "",
-  configureWebpack: config => {
-    config.resolve.alias.urls =
-      process.env.NODE_ENV === "production"
-        ? path.join(__dirname, "urls.prod")
-        : path.join(__dirname, "urls.dev");
-  },
-  devServer: {
-    port: 8080,
-    proxy: {
-      "/ph-api": {
-        target: "http://localhost:5000"
-      },
-      "/requestHub": {
-        target: "http://localhost:5000",
-        ws: true
-      }
+    lintOnSave: false,
+    publicPath: "",
+    configureWebpack: config => {
+        config.resolve.alias.urls =
+            process.env.NODE_ENV === "production"
+                ? path.join(__dirname, "urls.prod")
+                : path.join(__dirname, "urls.dev");
+    },
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'HttPlaceholder';
+                return args
+            });
+    },
+    devServer: {
+        port: 8080,
+        proxy: {
+            "/ph-api": {
+                target: "http://localhost:5000"
+            },
+            "/requestHub": {
+                target: "http://localhost:5000",
+                ws: true
+            }
+        }
     }
-  }
 };
