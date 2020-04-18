01bf95a3e2ceb337bf8e2c9b1057c55f7f8cca42
Duco <git@ducode.org>
2020-04-18 22:16:13 +0200

    Refactored ConfigurationParser and merged with ConfigurationUtilities

--- src/HttPlaceholder.Configuration/Utilities/ConfigurationParser.cs
+++ src/HttPlaceholder.Configuration/Utilities/ConfigurationParser.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using HttPlaceholder.Common;
 using HttPlaceholder.Common.Utilities;
-using HttPlaceholder.Infrastructure;
+using HttPlaceholder.Configuration.Attributes;
 using HttPlaceholder.Infrastructure.Implementations;
 using Newtonsoft.Json;
 
@@ -30,23 +33,33 @@ namespace HttPlaceholder.Configuration.Utilities
 
         public IDictionary<string, string> ParseConfiguration(string[] args)
         {
+            var argsDictionary = DetermineBaseArgsDictionary(args);
+            argsDictionary = argsDictionary.ToDictionary(d => d.Key.ToLower(), d => d.Value);
+            EnsureDefaultValuesAreAdded(argsDictionary);
+            return BuildFinalArgsDictionary(argsDictionary);
+        }
+
+        private IDictionary<string, string> DetermineBaseArgsDictionary(string[] args)
+        {
             var argsDictionary = args.Parse();
-            if (argsDictionary.TryGetValue(ConfigKeys.ConfigJsonLocationKey, out var configJsonPath))
+            var configPath = Path.Combine(_assemblyService.GetCallingAssemblyRootPath(), "config.json");
+
+            if (argsDictionary.TryGetValue(ConfigKeys.ConfigJsonLocationKey.ToLower(), out var configJsonPath))
             {
+                // Read the settings from a given file if the correct config key is set
                 if (!_fileService.FileExists(configJsonPath))
                 {
                     throw new FileNotFoundException($"File '{configJsonPath}' not found.");
                 }
 
-                ConsoleHelpers.WriteLineColor($"Reading configuration from '{configJsonPath}'.", ConsoleColor.Green, ConsoleColor.Black);
+                ConsoleHelpers.WriteLineColor($"Reading configuration from '{configJsonPath}'.", ConsoleColor.Green,
+                    ConsoleColor.Black);
                 var config = _fileService.ReadAllText(configJsonPath);
-                return JsonConvert.DeserializeObject<Dictionary<string, string>>(config);
+                argsDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(config);
             }
-
-            var configPath = Path.Combine(_assemblyService.GetCallingAssemblyRootPath(), "config.json");
-            if (args.Length == 0 && _fileService.FileExists(configPath))
+            else if (args.Length == 0 && _fileService.FileExists(configPath))
             {
-                // If a config file is found, try to load and parse it instead of the arguments.
+                // Read the settings from the installation directory.
                 Console.WriteLine($"Config file found at '{configPath}', so trying to parse that configuration.");
 
                 ConsoleHelpers.WriteLineColor(
@@ -55,10 +68,75 @@ namespace HttPlaceholder.Configuration.Utilities
                     ConsoleColor.Black);
 
                 var config = _fileService.ReadAllText(configPath);
-                return JsonConvert.DeserializeObject<Dictionary<string, string>>(config);
+                argsDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(config);
             }
 
             return argsDictionary;
+        }
+
+        private static void EnsureDefaultValuesAreAdded(IDictionary<string, string> argsDictionary)
+        {
+            argsDictionary.EnsureEntryExists(ConfigKeys.PortKey, 5000);
+            argsDictionary.EnsureEntryExists(ConfigKeys.PfxPathKey,
+                Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "key.pfx"));
+            argsDictionary.EnsureEntryExists(ConfigKeys.PfxPasswordKey, "1234");
+            argsDictionary.EnsureEntryExists(ConfigKeys.HttpsPortKey, 5050);
+            argsDictionary.EnsureEntryExists(ConfigKeys.UseHttpsKey, false);
+            argsDictionary.EnsureEntryExists(ConfigKeys.EnableUserInterface, true);
+        }
+
+        private static IDictionary<string, string> BuildFinalArgsDictionary(IDictionary<string, string> argsDictionary)
+        {
+            var envVars = Environment.GetEnvironmentVariables()
+                .Cast<DictionaryEntry>()
+                .ToDictionary(de => (string)de.Key, de => (string)de.Value);
+            var configDictionary = new Dictionary<string, string>();
+            foreach (var constant in GetConfigKeyMetadata())
+            {
+                if (!string.IsNullOrWhiteSpace(constant.Path))
+                {
+                    // First, add the environment variables to the configuration.
+                    if (envVars.TryGetValue(constant.Key, out var envVar))
+                    {
+                        configDictionary.Add(constant.Path, envVar);
+                        continue;
+                    }
+
+                    // Then, add the default values and values passed through the command line arguments.
+                    if (argsDictionary.TryGetValue(constant.Key, out var value))
+                    {
+                        configDictionary.Add(constant.Path, value);
+                    }
+                }
+            }
+
+            return configDictionary;
+        }
+
+        public static IEnumerable<ConfigMetadataModel> GetConfigKeyMetadata()
+        {
+            var result = new List<ConfigMetadataModel>();
+            foreach (var constant in ReflectionUtilities.GetConstants(typeof(ConfigKeys)))
+            {
+                var attribute = constant.CustomAttributes.FirstOrDefault();
+                if (attribute != null && attribute.AttributeType == typeof(ConfigKeyAttribute))
+                {
+                    result.Add(new ConfigMetadataModel
+                    {
+                        Key = (constant.GetValue(constant) as string).ToLower(),
+                        Description =
+                            attribute.NamedArguments.Single(a => a.MemberName == "Description").TypedValue
+                                .Value as string,
+                        Example =
+                            attribute.NamedArguments.Single(a => a.MemberName == "Example").TypedValue
+                                .Value as string,
+                        Path = attribute.NamedArguments.FirstOrDefault(a => a.MemberName == "ConfigPath").TypedValue
+                            .Value as string
+                    });
+                }
+            }
+
+            return result;
         }
     }
 }
