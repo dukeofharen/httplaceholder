b2565bf6c08c758b5752a81cbe3868a3c2bc21d8
Duco <git@ducode.org>
2020-04-18 13:26:22 +0200

    Random refactorings

--- gui/src/utils/stringFormat.js
+++ gui/src/utils/stringFormat.js
@@ -1,7 +1,7 @@
 // Source: https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
 if (!String.prototype.format) {
   String.prototype.format = function() {
-    var args = arguments;
+    const args = arguments;
     return this.replace(/{(\d+)}/g, function(match, number) {
       return typeof args[number] != "undefined" ? args[number] : match;
     });
