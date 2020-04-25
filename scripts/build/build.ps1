08eb51515278ec6de75b1696eb01f21968eb0856
Duco <git@ducode.org>
2020-04-25 16:06:29 +0200

    Undo temp fix

--- scripts/build/build.ps1
+++ scripts/build/build.ps1
@@ -18,35 +18,35 @@ New-Item -ItemType Directory $distFolder
 
 . "$PSScriptRoot/../Functions.ps1"
 . "$PSScriptRoot/01-Set-Vars.ps1"
-#. "$PSScriptRoot/02-Patch-Csproj-Versions.ps1" -srcFolder $srcFolder
-#if ($runUnitTests) {
-#    . "$PSScriptRoot/03-Run-Tests.ps1" -srcFolder $srcFolder
-#}
-#
-#. "$PSScriptRoot/04-Build-Gui.ps1" -srcFolder $srcFolder -rootFolder $rootFolder
-#. "$PSScriptRoot/05-Build-Windows.ps1" `
-#    -srcFolder $srcFolder `
-#    -mainProjectFile $mainProjectFile `
-#    -distFolder $distFolder `
-#    -rootFolder $rootFolder
-#. "$PSScriptRoot\06-Build-Linux.ps1" `
-#    -srcFolder $srcFolder `
-#    -mainProjectFile $mainProjectFile `
-#    -distFolder $distFolder `
-#    -rootFolder $rootFolder
-#. "$PSScriptRoot\07-Build-OsX.ps1" `
-#    -srcFolder $srcFolder `
-#    -mainProjectFile $mainProjectFile `
-#    -distFolder $distFolder `
-#    -rootFolder $rootFolder
-#. "$PSScriptRoot\08-Create-OpenAPI-File.ps1" `
-#    -srcFolder $srcFolder `
-#    -distFolder $distFolder
-#. "$PSScriptRoot\09-Build-NuGet.ps1" `
-#    -srcFolder $srcFolder `
-#    -distFolder $distFolder
-#. "$PSScriptRoot\10-Build-Tool.ps1" `
-#    -srcFolder $srcFolder `
-#    -mainProjectFile $mainProjectFile `
-#    -distFolder $distFolder
-. "$PSScriptRoot\11-Build-Publish-Docker.ps1"
+. "$PSScriptRoot/02-Patch-Csproj-Versions.ps1" -srcFolder $srcFolder
+if ($runUnitTests) {
+    . "$PSScriptRoot/03-Run-Tests.ps1" -srcFolder $srcFolder
+}
+
+. "$PSScriptRoot/04-Build-Gui.ps1" -srcFolder $srcFolder -rootFolder $rootFolder
+. "$PSScriptRoot/05-Build-Windows.ps1" `
+    -srcFolder $srcFolder `
+    -mainProjectFile $mainProjectFile `
+    -distFolder $distFolder `
+    -rootFolder $rootFolder
+. "$PSScriptRoot\06-Build-Linux.ps1" `
+    -srcFolder $srcFolder `
+    -mainProjectFile $mainProjectFile `
+    -distFolder $distFolder `
+    -rootFolder $rootFolder
+. "$PSScriptRoot\07-Build-OsX.ps1" `
+    -srcFolder $srcFolder `
+    -mainProjectFile $mainProjectFile `
+    -distFolder $distFolder `
+    -rootFolder $rootFolder
+. "$PSScriptRoot\08-Create-OpenAPI-File.ps1" `
+    -srcFolder $srcFolder `
+    -distFolder $distFolder
+. "$PSScriptRoot\09-Build-NuGet.ps1" `
+    -srcFolder $srcFolder `
+    -distFolder $distFolder
+. "$PSScriptRoot\10-Build-Tool.ps1" `
+    -srcFolder $srcFolder `
+    -mainProjectFile $mainProjectFile `
+    -distFolder $distFolder
+#. "$PSScriptRoot\11-Build-Publish-Docker.ps1" # This appears to be broken at the moment. Fix it later.
