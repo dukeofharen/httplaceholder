4781ed2ef7936b1a45205a15352d777b5ec735a6
Duke <github@ducode.org>
2020-08-22 13:25:49 +0000

    Feature/api return json by default (#29)
    * Added failing unit test
    
    * Implemented fix
    
    Co-authored-by: Duco <git@ducode.org>

---
+++ src/HttPlaceholder.Tests/Integration/RestApi/RestApiGeneralIntegrationTests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using HttPlaceholder.Dto.v1.Requests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace HttPlaceholder.Tests.Integration.RestApi
+{
+    [TestClass]
+    public class RestApiGeneralIntegrationTests : RestApiIntegrationTestBase
+    {
+        [TestInitialize]
+        public void Initialize() => InitializeRestApiIntegrationTest();
+
+        [TestCleanup]
+        public void Cleanup() => CleanupRestApiIntegrationTest();
+
+        [TestMethod]
+        public async Task RestApiIntegration_GetAllRequests_ShouldReturnResponseAsJsonByDefault()
+        {
+            // First, do a request to the stub for adding at least 1 request.
+            var stubUrl = $"{TestServer.BaseAddress}non-existing-stub";
+            using var stubResponse = await Client.GetAsync(stubUrl);
+            Assert.AreEqual(HttpStatusCode.InternalServerError, stubResponse.StatusCode);
+
+            // Then, fetch all requests from the API.
+            var apiUrl = $"{TestServer.BaseAddress}ph-api/requests";
+            using var apiResponse = await Client.GetAsync(apiUrl);
+            Assert.AreEqual(HttpStatusCode.OK, apiResponse.StatusCode);
+            var content = await apiResponse.Content.ReadAsStringAsync();
+
+            // Verify that the response is JSON instead of YAML.
+            Assert.IsFalse(content.StartsWith("- "));
+            try
+            {
+                var parsed = JsonConvert.DeserializeObject<RequestResultDto[]>(content);
+                var request = parsed.Single();
+                Assert.IsFalse(string.IsNullOrWhiteSpace(request.CorrelationId));
+            }
+            catch
+            {
+                Assert.Fail("Request JSON could not be parsed correctly.");
+            }
+        }
+    }
+}
