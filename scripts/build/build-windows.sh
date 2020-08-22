8a6b7dd9dea2eb3609bf353caf453059bd8b47b4
Duco <git@ducode.org>
2020-08-22 18:46:29 +0200

    Patch version during publish

--- scripts/build/build-windows.sh
+++ scripts/build/build-windows.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ "$1" = "" ]; then
+    echo "Please provide version"
+    exit 1
+fi
+
+VERSION=$1
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 WIN_DIST_DIR=$ROOT_DIR/dist/windows
 INSTALL_SCRIPT_DIR=$ROOT_DIR/scripts/build/installscripts/windows
@@ -7,7 +13,13 @@ mkdir $ROOT_DIR/dist
 mkdir $WIN_DIST_DIR
 
 cd src/HttPlaceholder
-dotnet publish $mainProjectFile --configuration=release --runtime=win-x64 /p:PublishTrimmed=true -o $WIN_DIST_DIR
+dotnet publish --configuration=release \
+    --runtime=win-x64 \
+    /p:PublishTrimmed=true \
+    /p:Version=$VERSION \
+    /p:AssemblyVersion=$VERSION \
+    /p:FileVersion=$VERSION \
+    -o $WIN_DIST_DIR
 
 cp -r $INSTALL_SCRIPT_DIR/. $WIN_DIST_DIR
 mv $WIN_DIST_DIR/web.config $WIN_DIST_DIR/_web.config
