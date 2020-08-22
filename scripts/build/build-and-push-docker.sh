236443bf048d49c99ccb964cba4fccb9c87d67e3
Duco <git@ducode.org>
2020-08-22 22:45:52 +0200

    Docker build script fix

--- scripts/build/build-and-push-docker.sh
+++ scripts/build/build-and-push-docker.sh
@@ -5,9 +5,8 @@ if [[ "$1" = "" ]]; then
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-TAG="$1"
+TAG=$1
 REPO_NAME="dukeofharen/httplaceholder"
-cd "$DIR/../.."
 docker build -t ${REPO_NAME}:${TAG} .
 docker tag ${REPO_NAME}:${TAG} ${REPO_NAME}:latest
 docker push ${REPO_NAME}:${TAG}
