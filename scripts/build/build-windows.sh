34166419ccbac8356b0ac246c52e858bea4ac41b
Duco <git@ducode.org>
2020-08-22 17:49:08 +0200

    Added step for building Windows binary

---
+++ scripts/build/build-windows.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WIN_DIST_DIR=$DIR/dist/windows
+INSTALL_SCRIPT_DIR=scripts/build/installscripts/windows
+
+mkdir $DIR/dist
+mkdir $WIN_DIST_DIR
+
+cd src/HttPlaceholder
+dotnet publish $mainProjectFile --configuration=release --runtime=win-x64 /p:PublishTrimmed=true -o $WIN_DIST_DIR
+
+cp -r $INSTALL_SCRIPT_DIR/. $WIN_DIST_DIR
+mv $WIN_DIST_DIR/web.config $WIN_DIST_DIR/_web.config
