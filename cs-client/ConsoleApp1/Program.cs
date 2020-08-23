83acad4aec6892c67b1a90fb3d7771dba125b54f
Duco <git@ducode.org>
2020-08-23 21:04:58 +0200

    Added config extension method for adding basic authentication to client

--- cs-client/ConsoleApp1/Program.cs
+++ cs-client/ConsoleApp1/Program.cs
@@ -15,12 +15,8 @@ namespace ConsoleApp1
             {
                 var requests = new RequestApi(new Configuration
                 {
-                    BasePath = "http://localhost:5000",
-                    DefaultHeaders = new Dictionary<string, string>
-                    {
-                        {"Authorization", $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes("duco:pass"))}"}
-                    }
-                });
+                    BasePath = "http://localhost:5000"
+                }.AddBasicAuthentication("duco", "pass"));
                 var requestResult = await requests.RequestGetAllAsync();
                 var requestResult1 = await requests.RequestGetByStubIdAsync("test123");
             }
