a1391bdc7e62f87c15af56ada88cec56265413a9
Duke <github@ducode.org>
2020-08-28 19:51:41 +0000

    Feature/case insensitive filter (#33)
    * Case insensitive search for stubs
    
    * Case insensitive search for requests
    
    Co-authored-by: Duco <git@ducode.org>

--- gui/src/views/Requests.vue
+++ gui/src/views/Requests.vue
@@ -5,13 +5,13 @@
       <v-row>
         <v-col class="buttons">
           <v-btn title="Refresh" @click="initialize" color="success"
-            >Refresh
+          >Refresh
           </v-btn>
           <v-btn
             title="Delete all requests"
             @click.stop="deleteAllDialog = true"
             color="error"
-            >Delete all requests
+          >Delete all requests
           </v-btn>
         </v-col>
       </v-row>
@@ -44,10 +44,10 @@
           <v-card-actions>
             <div class="flex-grow-1"></div>
             <v-btn color="green darken-1" text @click="deleteAllDialog = false"
-              >No
+            >No
             </v-btn>
             <v-btn color="green darken-1" text @click="deleteAllRequests"
-              >Yes
+            >Yes
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -57,98 +57,103 @@
 </template>
 
 <script>
-import Request from "@/components/Request";
-import { HubConnectionBuilder } from "@aspnet/signalr";
-import { actionNames } from "@/store/storeConstants";
-import { toastSuccess } from "@/utils/toastUtil";
-import { resources } from "@/shared/resources";
+  import Request from "@/components/Request";
+  import {HubConnectionBuilder} from "@aspnet/signalr";
+  import {actionNames} from "@/store/storeConstants";
+  import {toastSuccess} from "@/utils/toastUtil";
+  import {resources} from "@/shared/resources";
 
-export default {
-  name: "requests",
-  data() {
-    return {
-      requests: [],
-      tenantNames: [],
-      searchTerm: "",
-      selectedTenantName: "",
-      connection: {},
-      deleteAllDialog: false
-    };
-  },
-  components: {
-    Request
-  },
-  async created() {
-    this.initializeSignalR();
-    await this.initialize();
-  },
-  destroyed() {
-    this.connection.stop();
-  },
-  computed: {
-    filteredRequests() {
-      let result = this.requests;
-      if (this.searchTerm) {
-        result = result.filter(
-          r =>
-            (r.executingStubId &&
-              r.executingStubId.includes(this.searchTerm)) ||
-            r.requestParameters.url.includes(this.searchTerm)
+  export default {
+    name: "requests",
+    data() {
+      return {
+        requests: [],
+        tenantNames: [],
+        searchTerm: "",
+        selectedTenantName: "",
+        connection: {},
+        deleteAllDialog: false
+      };
+    },
+    components: {
+      Request
+    },
+    async created() {
+      this.initializeSignalR();
+      await this.initialize();
+    },
+    destroyed() {
+      this.connection.stop();
+    },
+    computed: {
+      filteredRequests() {
+        let result = this.requests;
+        if (this.searchTerm) {
+          result = result.filter(
+            r => {
+              const searchTerm = this.searchTerm.toLowerCase();
+              const stubId = r.executingStubId ? r.executingStubId.toLowerCase() : "";
+              const url = r.requestParameters.url.toLowerCase();
+              return stubId &&
+                stubId.includes(searchTerm) ||
+                url.includes(searchTerm);
+            }
+          );
+        }
+
+        if (this.selectedTenantName) {
+          result = result.filter(r => r.stubTenant === this.selectedTenantName);
+        }
+
+        return result;
+      }
+    },
+    methods: {
+      async initialize() {
+        const getRequestsPromise = this.$store.dispatch(actionNames.getRequests);
+        const getTenantNamesPromise = this.$store.dispatch(
+          actionNames.getTenantNames
         );
+        this.requests = await getRequestsPromise;
+        this.tenantNames = await getTenantNamesPromise;
+
+        this.initializeSearch();
+      },
+      initializeSearch() {
+        this.searchTerm = this.$route.query.searchTerm;
+        this.selectedTenantName = this.$route.query.stubTenant;
+      },
+      async deleteAllRequests() {
+        this.deleteAllDialog = false;
+        await this.$store.dispatch(actionNames.clearRequests);
+        toastSuccess(resources.requestsDeletedSuccessfully);
+        this.requests = [];
+      },
+      initializeSignalR() {
+        this.connection = new HubConnectionBuilder()
+          .withUrl("/requestHub")
+          .build();
+        this.connection.on("RequestReceived", request =>
+          this.requests.unshift(request)
+        );
+        this.connection
+          .start()
+          .then(() => {
+          })
+          .catch(err => console.error(err.toString()));
       }
-
-      if (this.selectedTenantName) {
-        result = result.filter(r => r.stubTenant === this.selectedTenantName);
+    },
+    watch: {
+      $route() {
+        this.initializeSearch();
       }
-
-      return result;
     }
-  },
-  methods: {
-    async initialize() {
-      const getRequestsPromise = this.$store.dispatch(actionNames.getRequests);
-      const getTenantNamesPromise = this.$store.dispatch(
-        actionNames.getTenantNames
-      );
-      this.requests = await getRequestsPromise;
-      this.tenantNames = await getTenantNamesPromise;
-
-      this.initializeSearch();
-    },
-    initializeSearch() {
-      this.searchTerm = this.$route.query.searchTerm;
-      this.selectedTenantName = this.$route.query.stubTenant;
-    },
-    async deleteAllRequests() {
-      this.deleteAllDialog = false;
-      await this.$store.dispatch(actionNames.clearRequests);
-      toastSuccess(resources.requestsDeletedSuccessfully);
-      this.requests = [];
-    },
-    initializeSignalR() {
-      this.connection = new HubConnectionBuilder()
-        .withUrl("/requestHub")
-        .build();
-      this.connection.on("RequestReceived", request =>
-        this.requests.unshift(request)
-      );
-      this.connection
-        .start()
-        .then(() => {})
-        .catch(err => console.error(err.toString()));
-    }
-  },
-  watch: {
-    $route() {
-      this.initializeSearch();
-    }
-  }
-};
+  };
 </script>
 
 <style scoped>
-.buttons > button {
-  margin-right: 10px;
-  margin-top: 10px;
-}
+  .buttons > button {
+    margin-right: 10px;
+    margin-top: 10px;
+  }
 </style>
--- gui/src/views/Stubs.vue
+++ gui/src/views/Stubs.vue
@@ -5,13 +5,13 @@
       <v-row>
         <v-col class="buttons">
           <v-btn title="Refresh" @click="initialize" color="success"
-            >Refresh
+          >Refresh
           </v-btn>
           <v-btn
             title="Delete all stubs"
             @click="deleteAllDialog = true"
             color="error"
-            >Delete all stubs
+          >Delete all stubs
           </v-btn>
         </v-col>
       </v-row>
@@ -46,7 +46,7 @@
         <v-card-actions>
           <div class="flex-grow-1"></div>
           <v-btn color="green darken-1" text @click="deleteAllDialog = false"
-            >No
+          >No
           </v-btn>
           <v-btn color="green darken-1" text @click="deleteAllStubs">Yes</v-btn>
         </v-card-actions>
@@ -56,88 +56,92 @@
 </template>
 
 <script>
-import Stub from "@/components/Stub";
-import { actionNames } from "@/store/storeConstants";
-import { toastSuccess } from "@/utils/toastUtil";
-import { resources } from "@/shared/resources";
+  import Stub from "@/components/Stub";
+  import {actionNames} from "@/store/storeConstants";
+  import {toastSuccess} from "@/utils/toastUtil";
+  import {resources} from "@/shared/resources";
 
-export default {
-  name: "stubs",
-  data() {
-    return {
-      stubs: [],
-      tenantNames: [],
-      selectedTenantName: "",
-      searchTerm: "",
-      deleteAllDialog: false
-    };
-  },
-  components: {
-    Stub
-  },
-  async created() {
-    await this.initialize();
-  },
-  computed: {
-    filteredStubs() {
-      const compare = (a, b) => {
-        if (a.stub.id < b.stub.id) return -1;
-        if (a.stub.id > b.stub.id) return 1;
-        return 0;
+  export default {
+    name: "stubs",
+    data() {
+      return {
+        stubs: [],
+        tenantNames: [],
+        selectedTenantName: "",
+        searchTerm: "",
+        deleteAllDialog: false
       };
+    },
+    components: {
+      Stub
+    },
+    async created() {
+      await this.initialize();
+    },
+    computed: {
+      filteredStubs() {
+        const compare = (a, b) => {
+          if (a.stub.id < b.stub.id) return -1;
+          if (a.stub.id > b.stub.id) return 1;
+          return 0;
+        };
 
-      let stubs = this.stubs;
-      if (this.searchTerm) {
-        stubs = stubs.filter(
-          s =>
-            s.stub.id.includes(this.searchTerm) ||
-            (s.stub.tenant && s.stub.tenant.includes(this.searchTerm))
+        let stubs = this.stubs;
+        if (this.searchTerm) {
+          stubs = stubs.filter(
+            s => {
+              const searchTerm = this.searchTerm.toLowerCase();
+              const stubId = s.stub.id.toLowerCase();
+              const tenant = s.stub.tenant ? s.stub.tenant.toLowerCase() : "";
+              return stubId.includes(searchTerm) ||
+                tenant && tenant.includes(searchTerm);
+            }
+          );
+        }
+
+        if (this.selectedTenantName) {
+          stubs = stubs.filter(s => s.stub.tenant === this.selectedTenantName);
+        }
+
+        return stubs.sort(compare);
+      }
+    },
+    methods: {
+      async initialize() {
+        const getStubsPromise = this.$store.dispatch(actionNames.getStubs);
+        const getTenantNamesPromise = this.$store.dispatch(
+          actionNames.getTenantNames
         );
+        this.stubs = await getStubsPromise;
+        this.tenantNames = await getTenantNamesPromise;
+
+        this.initializeSearch();
+      },
+      initializeSearch() {
+        this.searchTerm = this.$route.query.searchTerm;
+        this.selectedTenantName = this.$route.query.stubTenant;
+      },
+      async deleteAllStubs() {
+        this.deleteAllDialog = false;
+        await this.$store.dispatch(actionNames.deleteAllStubs);
+        toastSuccess(resources.stubsDeletedSuccessfully);
+        await this.initialize();
+      },
+      async onDeleted() {
+        await this.initialize();
       }
-
-      if (this.selectedTenantName) {
-        stubs = stubs.filter(s => s.stub.tenant === this.selectedTenantName);
+    },
+    watch: {
+      $route() {
+        this.initializeSearch();
       }
-
-      return stubs.sort(compare);
     }
-  },
-  methods: {
-    async initialize() {
-      const getStubsPromise = this.$store.dispatch(actionNames.getStubs);
-      const getTenantNamesPromise = this.$store.dispatch(
-        actionNames.getTenantNames
-      );
-      this.stubs = await getStubsPromise;
-      this.tenantNames = await getTenantNamesPromise;
-
-      this.initializeSearch();
-    },
-    initializeSearch() {
-      this.searchTerm = this.$route.query.searchTerm;
-      this.selectedTenantName = this.$route.query.stubTenant;
-    },
-    async deleteAllStubs() {
-      this.deleteAllDialog = false;
-      await this.$store.dispatch(actionNames.deleteAllStubs);
-      toastSuccess(resources.stubsDeletedSuccessfully);
-      await this.initialize();
-    },
-    async onDeleted() {
-      await this.initialize();
-    }
-  },
-  watch: {
-    $route() {
-      this.initializeSearch();
-    }
-  }
-};
+  };
 </script>
 
 <style scoped>
-.buttons > button {
-  margin-right: 10px;
-  margin-top: 10px;
-}
+  .buttons > button {
+    margin-right: 10px;
+    margin-top: 10px;
+  }
 </style>
