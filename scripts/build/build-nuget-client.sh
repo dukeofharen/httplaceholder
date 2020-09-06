347c7d71e05e2aa0d675a1726a58356d74c4efd4
Duke <github@ducode.org>
2020-09-06 15:00:38 +0000

    Fixed NuGet client build (#37)
    Co-authored-by: Duco <git@ducode.org>

--- scripts/build/build-nuget-client.sh
+++ scripts/build/build-nuget-client.sh
@@ -14,10 +14,16 @@ DIST_DIR=$ROOT_DIR/dist
 mkdir $DIST_DIR
 
 # Pack tool
-cd cs-client/src/HttPlaceholder.Client
+cd $ROOT_DIR/cs-client/src/HttPlaceholder.Client
 dotnet pack -c Release \
     -o $DIST_DIR \
     /p:Version=$VERSION \
     /p:AssemblyVersion=$VERSION \
-    /p:FileVersion=$VERSION
-    
+    /p:FileVersion=$VERSION \
+    /p:Authors=Ducode \
+    /p:Company=Ducode \
+    /p:AssemblyTitle="HttPlaceholder NuGet Client" \
+    /p:Description="HttPlaceholder NuGet Client" \
+    /p:Copyright="2020 Ducode" \
+    /p:RepositoryUrl="https://github.com/dukeofharen/httplaceholder" \
+    /p:RepositoryType=git
