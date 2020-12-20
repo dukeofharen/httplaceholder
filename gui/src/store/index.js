6ba92c83e9d84880c57239d51d2aee052432448b
Duco <git@ducode.org>
2020-12-20 18:03:22 +0100

    Added stubForm vuex module

--- gui/src/store/index.js
+++ gui/src/store/index.js
@@ -4,6 +4,7 @@ import Vuex from "vuex";
 import general from "@/store/modules/general";
 import metadata from "@/store/modules/metadata";
 import requests from "@/store/modules/requests";
+import stubForm from "@/store/modules/stubForm";
 import stubs from "@/store/modules/stubs";
 import tenants from "@/store/modules/tenants";
 import users from "@/store/modules/users";
@@ -14,6 +15,7 @@ export default new Vuex.Store({
   modules: {
     metadata,
     requests,
+    stubForm,
     stubs,
     tenants,
     users,
