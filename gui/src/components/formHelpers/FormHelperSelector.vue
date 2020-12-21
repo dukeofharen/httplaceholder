047978652eb7a247e7d302cc8aa087d8540c5d5d
Duco <git@ducode.org>
2020-12-21 16:27:22 +0100

    UI fixes for FormHelperSelector.vue

--- gui/src/components/formHelpers/FormHelperSelector.vue
+++ gui/src/components/formHelpers/FormHelperSelector.vue
@@ -9,19 +9,21 @@
         </v-list-item-content>
       </v-list-item>
       <template v-if="showList">
-        <v-list-item v-for="(item, index) in formHelperItems" :key="index">
-          <v-list-item-content>
-            <v-list-item-title v-if="item.onClick" @click="item.onClick" class="clickable">{{
-                item.title
-              }}
-            </v-list-item-title>
-            <v-list-item-title :class="{bold: item.divider}" v-else>{{ item.title }}</v-list-item-title>
-            <v-list-item-content v-if="item.subTitle" class="subtitle">{{ item.subTitle }}</v-list-item-content>
-          </v-list-item-content>
-        </v-list-item>
+        <v-list-group v-for="(item, index) in formHelperItems" :key="index">
+          <template v-slot:activator>
+            <v-list-item-title class="main-item">{{ item.title }}</v-list-item-title>
+          </template>
+
+          <v-list-item v-for="(subItem, index) in item.subItems" :key="index" class="sub-item" @click="subItem.onClick">
+            <v-list-item-content>
+              <v-list-item-title v-text="subItem.title" class="clickable"></v-list-item-title>
+              <v-list-item-content class="subtitle">{{ subItem.subTitle }}</v-list-item-content>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
       </template>
-      <HttpMethodSelector v-if="currentSelectedFormHelper === formHelperKeys.httpMethod" />
-      <TenantSelector v-if="currentSelectedFormHelper === formHelperKeys.tenant" />
+      <HttpMethodSelector v-if="currentSelectedFormHelper === formHelperKeys.httpMethod"/>
+      <TenantSelector v-if="currentSelectedFormHelper === formHelperKeys.tenant"/>
     </v-col>
   </v-row>
 </template>
@@ -44,91 +46,92 @@ export default {
       formHelperItems: [
         {
           title: "General",
-          divider: true
-        },
-        {
-          title: "Description",
-          subTitle: tooltipResources.description,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultDescription")
-        },
-        {
-          title: "Priority",
-          subTitle: tooltipResources.priority,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultPriority")
-        },
-        {
-          title: "Tenant",
-          subTitle: tooltipResources.tenant,
-          onClick: () => this.openFormHelper(this.formHelperKeys.tenant)
+          subItems: [
+            {
+              title: "Description",
+              subTitle: tooltipResources.description,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultDescription")
+            },
+            {
+              title: "Priority",
+              subTitle: tooltipResources.priority,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultPriority")
+            },
+            {
+              title: "Tenant",
+              subTitle: tooltipResources.tenant,
+              onClick: () => this.openFormHelper(this.formHelperKeys.tenant)
+            }]
         },
         {
           title: "Request",
-          divider: true
-        },
-        {
-          title: "HTTP method",
-          subTitle: tooltipResources.httpMethod,
-          onClick: () => this.openFormHelper(this.formHelperKeys.httpMethod)
-        },
-        {
-          title: "URL path",
-          subTitle: tooltipResources.urlPath,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultPath")
-        },
-        {
-          title: "Full path",
-          subTitle: tooltipResources.fullPath,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultFullPath")
-        },
-        {
-          title: "Query string",
-          subTitle: tooltipResources.queryString,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultQuery")
-        },
-        {
-          title: "HTTPS",
-          subTitle: tooltipResources.isHttps,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultIsHttps")
-        },
-        {
-          title: "Basic authentication",
-          subTitle: tooltipResources.basicAuthentication,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultBasicAuth")
-        },
-        {
-          title: "Headers",
-          subTitle: tooltipResources.headers,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultRequestHeaders")
-        },
-        {
-          title: "Body",
-          subTitle: tooltipResources.body,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultRequestBody")
-        },
-        {
-          title: "Form body",
-          subTitle: tooltipResources.formBody,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultFormBody")
-        },
-        {
-          title: "Client IP",
-          subTitle: tooltipResources.clientIp,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultClientIp")
-        },
-        {
-          title: "Hostname",
-          subTitle: tooltipResources.hostname,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultHostname")
-        },
-        {
-          title: "JSONPath",
-          subTitle: tooltipResources.jsonPath,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultJsonPath")
-        },
-        {
-          title: "XPath",
-          subTitle: tooltipResources.xpath,
-          onClick: () => this.setDefaultValue("stubForm/setDefaultXPath")
+          subItems: [
+            {
+              title: "HTTP method",
+              subTitle: tooltipResources.httpMethod,
+              onClick: () => this.openFormHelper(this.formHelperKeys.httpMethod)
+            },
+            {
+              title: "URL path",
+              subTitle: tooltipResources.urlPath,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultPath")
+            },
+            {
+              title: "Full path",
+              subTitle: tooltipResources.fullPath,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultFullPath")
+            },
+            {
+              title: "Query string",
+              subTitle: tooltipResources.queryString,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultQuery")
+            },
+            {
+              title: "HTTPS",
+              subTitle: tooltipResources.isHttps,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultIsHttps")
+            },
+            {
+              title: "Basic authentication",
+              subTitle: tooltipResources.basicAuthentication,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultBasicAuth")
+            },
+            {
+              title: "Headers",
+              subTitle: tooltipResources.headers,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultRequestHeaders")
+            },
+            {
+              title: "Body",
+              subTitle: tooltipResources.body,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultRequestBody")
+            },
+            {
+              title: "Form body",
+              subTitle: tooltipResources.formBody,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultFormBody")
+            },
+            {
+              title: "Client IP",
+              subTitle: tooltipResources.clientIp,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultClientIp")
+            },
+            {
+              title: "Hostname",
+              subTitle: tooltipResources.hostname,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultHostname")
+            },
+            {
+              title: "JSONPath",
+              subTitle: tooltipResources.jsonPath,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultJsonPath")
+            },
+            {
+              title: "XPath",
+              subTitle: tooltipResources.xpath,
+              onClick: () => this.setDefaultValue("stubForm/setDefaultXPath")
+            }
+          ]
         }
       ]
     };
@@ -152,6 +155,19 @@ export default {
 </script>
 
 <style scoped>
+.main-item {
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.sub-item {
+  cursor: pointer;
+}
+
+.sub-item:hover {
+  background-color: #f1f1f1;
+}
+
 .bold {
   font-weight: bold;
 }
