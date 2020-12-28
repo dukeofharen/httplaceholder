e5694e61466182a78eec4b12570b7093129efc92
Duco <git@ducode.org>
2020-12-28 23:26:17 +0100

    Added failing unit tests for ContentTypeResponseWriter

---
+++ src/HttPlaceholder.Application/StubExecution/ResponseWriting/Implementations/ContentTypeResponseWriter.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using HttPlaceholder.Domain;
+
+namespace HttPlaceholder.Application.StubExecution.ResponseWriting.Implementations
+{
+    public class ContentTypeResponseWriter : IResponseWriter
+    {
+        public Task<StubResponseWriterResultModel> WriteToResponseAsync(StubModel stub, ResponseModel response) => throw new System.NotImplementedException();
+
+        public int Priority { get; } = -11;
+    }
+}
