4d18c44ca59971d2582eabb17a995e32233d5267
Duco <git@ducode.org>
2020-12-28 19:43:34 +0100

    Added failing unit test

---
+++ src/HttPlaceholder.Tests/Integration/Stubs/StubEnabledDisabledIntegrationTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttPlaceholder.Tests.Integration.Stubs
+{
+    [TestClass]
+    public class StubEnabledDisabledIntegrationTests : StubIntegrationTestBase
+    {
+        [TestInitialize]
+        public void Initialize() => InitializeStubIntegrationTest("integration.yml");
+
+        [TestCleanup]
+        public void Cleanup() => CleanupIntegrationTest();
+
+        [TestMethod]
+        public async Task StubIntegration_StubIsEnabled_ShouldReturnContent()
+        {
+            // arrange
+            var url = $"{TestServer.BaseAddress}enabled";
+
+            // act / assert
+            using var response = await Client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.AreEqual("This stub is enabled.", content);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task StubIntegration_StubIsDisabled_ShouldNotReturnContent()
+        {
+            // arrange
+            var url = $"{TestServer.BaseAddress}disabled";
+
+            // act / assert
+            using var response = await Client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.AreNotEqual("This stub is disabled.", content);
+            Assert.AreEqual(HttpStatusCode.NotImplemented, response.StatusCode);
+        }
+    }
+}
