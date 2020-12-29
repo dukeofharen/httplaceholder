95f363447fa542682f59ef7cd2b786f193d0d2b5
Duco <git@ducode.org>
2020-12-29 11:29:45 +0100

    Added utility for adding entries to dictionary in case insensitive way

---
+++ src/HttPlaceholder.Common.Tests/Utilities/CollectionUtilitiesFacts.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using HttPlaceholder.Common.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttPlaceholder.Common.Tests.Utilities
+{
+    [TestClass]
+    public class CollectionUtilitiesFacts
+    {
+        [TestMethod]
+        public void AddOrReplaceCaseInsensitive_AddEntryWithSameCasing()
+        {
+            // Arrange
+            var dictionary = new Dictionary<string, string> {{"Content-Type", "application/json"}};
+
+            // Act
+            dictionary.AddOrReplaceCaseInsensitive("Content-Type", "application/json");
+
+            // Assert
+            Assert.AreEqual(1, dictionary.Count);
+            Assert.AreEqual("application/json", dictionary["Content-Type"]);
+        }
+
+        [TestMethod]
+        public void AddOrReplaceCaseInsensitive_AddEntryWithDifferentCasing()
+        {
+            // Arrange
+            var dictionary = new Dictionary<string, string> {{"content-type", "application/json"}};
+
+            // Act
+            dictionary.AddOrReplaceCaseInsensitive("Content-Type", "application/json");
+
+            // Assert
+            Assert.AreEqual(1, dictionary.Count);
+            Assert.AreEqual("application/json", dictionary["Content-Type"]);
+        }
+
+        [TestMethod]
+        public void AddOrReplaceCaseInsensitive_AddNewEntry()
+        {
+            // Arrange
+            var dictionary = new Dictionary<string, string> {{"content-type", "application/json"}};
+
+            // Act
+            dictionary.AddOrReplaceCaseInsensitive("Accept", "text/xml");
+
+            // Assert
+            Assert.AreEqual(2, dictionary.Count);
+            Assert.AreEqual("application/json", dictionary["content-type"]);
+            Assert.AreEqual("text/xml", dictionary["Accept"]);
+        }
+    }
+}
---
+++ src/HttPlaceholder.Common/Utilities/CollectionUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HttPlaceholder.Common.Utilities
+{
+    public static class CollectionUtilities
+    {
+        public static void AddOrReplaceCaseInsensitive(this IDictionary<string, string> dict, string key, string value)
+        {
+            var pair = dict.FirstOrDefault(h => string.Equals(key, h.Key, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrWhiteSpace(pair.Key))
+            {
+                dict.Remove(pair.Key);
+            }
+
+            dict.Add(key, value);
+        }
+    }
+}
