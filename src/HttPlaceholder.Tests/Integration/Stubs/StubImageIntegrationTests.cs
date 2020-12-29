c9bf2d1ce04d289a12639b06c602332ccf7a97f1
Duco <git@ducode.org>
2020-12-29 17:19:34 +0100

    Added failing integration tests

---
+++ src/HttPlaceholder.Tests/Integration/Stubs/StubImageIntegrationTests.cs
@@ -0,0 +1,90 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SixLabors.ImageSharp;
+
+namespace HttPlaceholder.Tests.Integration.Stubs
+{
+    [TestClass]
+    public class StubImageIntegrationTests : StubIntegrationTestBase
+    {
+        [TestInitialize]
+        public void Initialize() => InitializeStubIntegrationTest("integration.yml");
+
+        [TestCleanup]
+        public void Cleanup() => CleanupIntegrationTest();
+
+        [TestMethod]
+        public async Task StubIntegration_StubImage_Jpeg()
+        {
+            // Arrange
+            var url = $"{TestServer.BaseAddress}stub-image.jpg";
+
+            // Act
+            var response = await Client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.AreEqual("image/jpeg", response.Headers.Single(h => h.Key == "Content-Type").Value.Single());
+            await using var ms = new MemoryStream(await response.Content.ReadAsByteArrayAsync());
+            using var image = await Image.LoadAsync(ms);
+            Assert.AreEqual(1024, image.Width);
+            Assert.AreEqual(256, image.Height);
+        }
+
+        [TestMethod]
+        public async Task StubIntegration_StubImage_Png()
+        {
+            // Arrange
+            var url = $"{TestServer.BaseAddress}stub-image.png";
+
+            // Act
+            var response = await Client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.AreEqual("image/png", response.Headers.Single(h => h.Key == "Content-Type").Value.Single());
+            await using var ms = new MemoryStream(await response.Content.ReadAsByteArrayAsync());
+            using var image = await Image.LoadAsync(ms);
+            Assert.AreEqual(1024, image.Width);
+            Assert.AreEqual(256, image.Height);
+        }
+
+        [TestMethod]
+        public async Task StubIntegration_StubImage_Bmp()
+        {
+            // Arrange
+            var url = $"{TestServer.BaseAddress}stub-image.bmp";
+
+            // Act
+            var response = await Client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.AreEqual("image/bmp", response.Headers.Single(h => h.Key == "Content-Type").Value.Single());
+            await using var ms = new MemoryStream(await response.Content.ReadAsByteArrayAsync());
+            using var image = await Image.LoadAsync(ms);
+            Assert.AreEqual(1024, image.Width);
+            Assert.AreEqual(256, image.Height);
+        }
+
+        [TestMethod]
+        public async Task StubIntegration_StubImage_Gif()
+        {
+            // Arrange
+            var url = $"{TestServer.BaseAddress}stub-image.gif";
+
+            // Act
+            var response = await Client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.AreEqual("image/gif", response.Headers.Single(h => h.Key == "Content-Type").Value.Single());
+            await using var ms = new MemoryStream(await response.Content.ReadAsByteArrayAsync());
+            using var image = await Image.LoadAsync(ms);
+            Assert.AreEqual(1024, image.Width);
+            Assert.AreEqual(256, image.Height);
+        }
+    }
+}
