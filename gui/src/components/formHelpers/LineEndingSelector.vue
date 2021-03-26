f63894b0efba08cc6469fe43ae4be0b1c5245eed
Duco <git@ducode.org>
2021-03-26 22:07:23 +0100

    Fixed LineEndingSelector

--- gui/src/components/formHelpers/LineEndingSelector.vue
+++ gui/src/components/formHelpers/LineEndingSelector.vue
@@ -1,38 +1,40 @@
 <template>
   <v-row>
     <v-col>
-      <v-list-item>
-        <v-list-item-content
-          class="line-endings"
-          @click="lineEndingSelected(null)"
-        >
-          <v-list-item-title>As provided in response body</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content
+            class="line-endings"
+            @click="lineEndingSelected(null)"
+          >
+            <v-list-item-title>As provided in response body</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-      <v-list-item>
-        <v-list-item-content
-          class="line-endings"
-          @click="lineEndingSelected(lineEndingTypes.unix)"
-        >
-          <v-list-item-title>UNIX line endings</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+        <v-list-item>
+          <v-list-item-content
+            class="line-endings"
+            @click="lineEndingSelected(lineEndingTypes.unix)"
+          >
+            <v-list-item-title>UNIX line endings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-      <v-list-item>
-        <v-list-item-content
-          class="line-endings"
-          @click="lineEndingSelected(lineEndingTypes.windows)"
-        >
-          <v-list-item-title>Windows line endings</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+        <v-list-item>
+          <v-list-item-content
+            class="line-endings"
+            @click="lineEndingSelected(lineEndingTypes.windows)"
+          >
+            <v-list-item-title>Windows line endings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import { lineEndingTypes } from "@/shared/stubFormResources";
+import {lineEndingTypes} from "@/shared/stubFormResources";
 
 export default {
   data() {
