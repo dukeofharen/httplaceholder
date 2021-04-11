8adf8fb70996ac9b61f66721a9c2a7e24aa6877e
Duco <git@ducode.org>
2021-04-11 10:12:45 +0200

    Updated MySQL and MSSQL migration scripts to insert metadata row if it doesn't exist yet

--- src/HttPlaceholder.Persistence/Db/Implementations/SqlServerQueryStore.cs
+++ src/HttPlaceholder.Persistence/Db/Implementations/SqlServerQueryStore.cs
@@ -51,9 +51,13 @@ WHERE stub_id = @StubId";
         public string CleanOldRequestsQuery =>
             @"DELETE FROM requests WHERE ID NOT IN (SELECT TOP (@Limit) ID FROM requests ORDER BY ID DESC)";
 
-        public string GetStubUpdateTrackingIdQuery { get; }
-        public string InsertStubUpdateTrackingIdQuery { get; }
-        public string UpdateStubUpdateTrackingIdQuery { get; }
+        public string GetStubUpdateTrackingIdQuery => "SELECT stub_update_tracking_id FROM metadata";
+
+        public string InsertStubUpdateTrackingIdQuery =>
+            "INSERT INTO metadata (stub_update_tracking_id) VALUES (@StubUpdateTrackingId)";
+
+        public string UpdateStubUpdateTrackingIdQuery =>
+            "UPDATE metadata SET stub_update_tracking_id = @StubUpdateTrackingId";
 
         public string MigrationsQuery => SqlServerResources.MigrateScript;
     }
