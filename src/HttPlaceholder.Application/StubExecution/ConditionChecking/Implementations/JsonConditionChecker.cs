8bbc29d5cd0c94c296f595454c27a7e8fbe3d4a8
Duco <git@ducode.org>
2021-04-13 22:12:46 +0200

    Added empty condition checker for JSON

---
+++ src/HttPlaceholder.Application/StubExecution/ConditionChecking/Implementations/JsonConditionChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HttPlaceholder.Application.Interfaces.Http;
+using HttPlaceholder.Domain;
+
+namespace HttPlaceholder.Application.StubExecution.ConditionChecking.Implementations
+{
+    public class JsonConditionChecker : IConditionChecker
+    {
+        private readonly IHttpContextService _httpContextService;
+
+        public JsonConditionChecker(IHttpContextService httpContextService)
+        {
+            _httpContextService = httpContextService;
+        }
+
+        public ConditionCheckResultModel Validate(string stubId, StubConditionsModel conditions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Priority => 1;
+    }
+}
