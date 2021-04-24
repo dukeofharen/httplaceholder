badb63405d00d6954183dc0b97313828411af7d4
Duke <github@ducode.org>
2021-04-24 12:44:13 +0200

    Feature/postman integration test (#130)
    * Added Postman integration test collection with request condition tests
    
    * Postman collection change
    
    * Regrouping of requests
    
    * Added IT for line endings
    
    * Added IT for dynamic mode UUID
    
    * Added IT for dynamic mode UUID and query string
    
    * Finished ITs for dynamic mode
    
    * Fixed broken tests
    
    * Added IT for reverse proxy
    
    * Fixed slow response test
    
    * Added script for running integration tests with Newman
    
    Co-authored-by: Duco <git@ducode.org>

---
+++ test/exec-tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Execute this script for performing Postman integration tests against HttPlaceholder.
+# You need to have Newman installed (https://github.com/postmanlabs/newman).
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+HTTPL_ROOT_DIR=$DIR/../src/HttPlaceholder
+POSTMAN_PATH=$DIR/HttPlaceholderIntegration.postman_collection.json
+
+# Run HttPlaceholder tests for in memory configuration.
+echo "Testing HttPlaceholder with in memory configuration"
+dotnet run -p $HTTPL_ROOT_DIR --useInMemoryStorage > $DIR/logs.txt 2>&1 &
+sleep 5
+newman run $POSTMAN_PATH --insecure
+sudo killall HttPlaceholder
+
+# Run HttPlaceholder tests for in file storage configuration.
+echo "Testing HttPlaceholder with in file storage configuration"
+FILE_STORAGE_PATH="/tmp/httplaceholder_stubs"
+if [ ! -d "$FILE_STORAGE_PATH" ]; then
+  echo "Creating folder $FILE_STORAGE_PATH"
+  mkdir $FILE_STORAGE_PATH
+fi
+
+dotnet run -p $HTTPL_ROOT_DIR --fileStorageLocation $FILE_STORAGE_PATH > $DIR/logs.txt 2>&1 &
+sleep 5
+newman run $POSTMAN_PATH --insecure
+sudo killall HttPlaceholder
+
+# Run HttPlaceholder tests for in SQLite configuration.
+echo "Testing HttPlaceholder with in SQLite configuration"
+SQLITE_PATH="/tmp/httplaceholder_stubs.db"
+dotnet run -p $HTTPL_ROOT_DIR --sqliteConnectionString "Data Source=$SQLITE_PATH" > $DIR/logs.txt 2>&1 &
+sleep 5
+newman run $POSTMAN_PATH --insecure
+sudo killall HttPlaceholder
