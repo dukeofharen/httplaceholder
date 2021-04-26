34b897fcd7bf958578b4fbc6581875043bd5a858
Duco <git@ducode.org>
2021-04-26 13:56:44 +0200

    Added ModelValidator

---
+++ src/HttPlaceholder.Common/IModelValidator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HttPlaceholder.Application.Interfaces.Validation
+{
+    public interface IModelValidator
+    {
+        IEnumerable<ValidationResult> ValidateModel(object model);
+    }
+}
