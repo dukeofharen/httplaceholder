53961be4116bf68ff42e1974f8e28d84b2abc481
Duco <git@ducode.org>
2021-05-16 14:51:12 +0200

    Added tests for HttPlaceholderClientModule

---
+++ src/HttPlaceholder.Client.Tests/HttPlaceholderClientModuleFacts.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using HttPlaceholder.Client.Implementations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttPlaceholder.Client.Tests
+{
+    [TestClass]
+    public class HttPlaceholderClientModuleFacts
+    {
+        [TestMethod]
+        public void AddHttPlaceholderClient_DotNetConfig_ShouldSuccessfullyRegisterClient()
+        {
+            // Arrange
+            var configDict = new Dictionary<string, string>
+            {
+                {"RootUrl", "http://localhost:5000"}, {"Username", "username"}, {"Password", "password"}
+            };
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(configDict)
+                .Build();
+            var services = new ServiceCollection();
+            services.AddHttPlaceholderClient(config);
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Act
+            var client = (HttPlaceholderClient)serviceProvider.GetRequiredService<IHttPlaceholderClient>();
+
+            // Assert
+            Assert.AreEqual("http://localhost:5000/", client.HttpClient.BaseAddress.OriginalString);
+        }
+
+        [TestMethod]
+        public void AddHttPlaceholderClient_ConfigAsAction_ShouldSuccessfullyRegisterClient()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddHttPlaceholderClient(config =>
+            {
+                config.RootUrl = "http://localhost:5000";
+                config.Username = "username";
+                config.Password = "password";
+            });
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Act
+            var client = (HttPlaceholderClient)serviceProvider.GetRequiredService<IHttPlaceholderClient>();
+
+            // Assert
+            Assert.AreEqual("http://localhost:5000/", client.HttpClient.BaseAddress.OriginalString);
+        }
+    }
+}
