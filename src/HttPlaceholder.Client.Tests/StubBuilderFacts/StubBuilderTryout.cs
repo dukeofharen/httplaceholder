096669d4c606e6221cbd8bb8b9c885057c5cee14
Duco <git@ducode.org>
2021-05-16 12:52:13 +0200

    Implemented builder for StubDto

---
+++ src/HttPlaceholder.Client.Tests/StubBuilderFacts/StubBuilderTryout.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace HttPlaceholder.Client.Tests.StubBuilderFacts
+{
+    [TestClass]
+    public class StubBuilderTryout
+    {
+        [TestMethod]
+        public void Bla123()
+        {
+            var stub = StubBuilder.Begin()
+                .WithId("stub123")
+                .WithJsonObject(new
+                {
+                    duco = "winterwerp"
+                })
+                .Build();
+            var json = JsonConvert.SerializeObject(stub);
+        }
+    }
+}
