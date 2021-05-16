a1e325d9d621ea6ebd158959f954e0b77e0f8100
Duco <git@ducode.org>
2021-05-16 13:06:19 +0200

    Split up stub builder in different classes

---
+++ src/HttPlaceholder.Client/StubBuilders/StubBuilder.cs
@@ -0,0 +1,81 @@
+﻿using HttPlaceholder.Client.Dto.Enums;
+using HttPlaceholder.Client.Dto.Stubs;
+using HttPlaceholder.Client.StubConditionBuilders;
+using HttPlaceholder.Client.StubResponseBuilders;
+
+namespace HttPlaceholder.Client.StubBuilders
+{
+    /// <summary>
+    /// A class for building a <see cref="StubDto"/> in a fluent way.
+    /// </summary>
+    public sealed class StubBuilder
+    {
+        private readonly StubDto _stub = new();
+
+        private StubBuilder()
+        {
+        }
+
+        public static StubBuilder Begin() => new StubBuilder();
+
+        public StubBuilder WithId(string id)
+        {
+            _stub.Id = id;
+            return this;
+        }
+
+        public StubBuilder WithPriority(int priority)
+        {
+            _stub.Priority = priority;
+            return this;
+        }
+
+        public StubBuilder WithPriority(PriorityType priority)
+        {
+            _stub.Priority = (int)priority;
+            return this;
+        }
+
+        public StubBuilder WithTenant(string tenant)
+        {
+            _stub.Tenant = tenant;
+            return this;
+        }
+
+        public StubBuilder WithDescription(string description)
+        {
+            _stub.Description = description;
+            return this;
+        }
+
+        public StubBuilder IsEnabled()
+        {
+            _stub.Enabled = true;
+            return this;
+        }
+
+        public StubBuilder IsDisabled()
+        {
+            _stub.Enabled = false;
+            return this;
+        }
+
+        public StubBuilder WithConditions(StubConditionsDto conditions)
+        {
+            _stub.Conditions = conditions;
+            return this;
+        }
+
+        public StubBuilder WithConditions(StubConditionBuilder builder) => WithConditions(builder.Build());
+
+        public StubBuilder WithResponse(StubResponseDto response)
+        {
+            _stub.Response = response;
+            return this;
+        }
+
+        public StubBuilder WithResponse(StubResponseBuilder builder) => WithResponse(builder.Build());
+
+        public StubDto Build() => _stub;
+    }
+}
---
+++ src/HttPlaceholder.Client/StubBuilders/StubConditionBuilder.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using HttPlaceholder.Client.Dto.Stubs;
+
+namespace HttPlaceholder.Client.StubConditionBuilders
+{
+    public sealed class StubConditionBuilder
+    {
+        private readonly StubConditionsDto _conditions = new StubConditionsDto();
+
+        private StubConditionBuilder()
+        {
+        }
+
+        public StubConditionBuilder Begin() => new StubConditionBuilder();
+
+        public StubConditionBuilder WithHttpMethod(string method)
+        {
+            _conditions.Method = method;
+            return this;
+        }
+
+        public StubConditionBuilder WithHttpMethod(HttpMethod method)
+        {
+            _conditions.Method = method.Method;
+            return this;
+        }
+
+        public StubConditionBuilder WithPath(string path)
+        {
+            EnsureUrlConditions();
+            _conditions.Url.Path = path;
+            return this;
+        }
+
+        public StubConditionBuilder WithQueryStringParameter(string key, string value)
+        {
+            EnsureUrlConditions();
+            _conditions.Url.Query ??= new Dictionary<string, string>();
+            _conditions.Url.Query.Add(key, value);
+            return this;
+        }
+
+        public StubConditionBuilder WithFullPath(string fullPath)
+        {
+            EnsureUrlConditions();
+            _conditions.Url.FullPath = fullPath;
+            return this;
+        }
+
+        public StubConditionBuilder WithHttpsEnabled()
+        {
+            EnsureUrlConditions();
+            _conditions.Url.IsHttps = true;
+            return this;
+        }
+
+        public StubConditionBuilder WithHttpsDisabled()
+        {
+            EnsureUrlConditions();
+            _conditions.Url.IsHttps = false;
+            return this;
+        }
+
+        public StubConditionBuilder WithPostedBodySubstring(string bodySubstring)
+        {
+            var bodyConditions =
+                _conditions.Body != null ? (List<string>)_conditions.Body : new List<string>();
+            bodyConditions.Add(bodySubstring);
+            _conditions.Body = bodyConditions;
+            return this;
+        }
+
+        public StubConditionBuilder WithPostedFormValue(string key, string value)
+        {
+            var formConditions = _conditions.Form != null
+                ? (List<StubFormDto>)_conditions.Form
+                : new List<StubFormDto>();
+            formConditions.Add(new StubFormDto {Key = key, Value = value});
+            _conditions.Form = formConditions;
+            return this;
+        }
+
+        public StubConditionBuilder WithRequestHeader(string key, string value)
+        {
+            _conditions.Headers ??= new Dictionary<string, string>();
+            _conditions.Headers.Add(key, value);
+            return this;
+        }
+
+        public StubConditionBuilder WithXPathCondition(string xpath, IDictionary<string, string> namespaces = null)
+        {
+            var xpathConditions = _conditions.Xpath != null
+                ? (List<StubXpathDto>)_conditions.Xpath
+                : new List<StubXpathDto>();
+            xpathConditions.Add(new StubXpathDto {QueryString = xpath, Namespaces = namespaces});
+            _conditions.Xpath = xpathConditions;
+            return this;
+        }
+
+        public StubConditionBuilder WithJsonPathCondition(string jsonPath)
+        {
+            var jsonpathConditions = _conditions.JsonPath != null
+                ? (List<string>)_conditions.JsonPath
+                : new List<string>();
+            jsonpathConditions.Add(jsonPath);
+            _conditions.JsonPath = jsonpathConditions;
+            return this;
+        }
+
+        public StubConditionBuilder WithBasicAuthentication(string username, string password)
+        {
+            _conditions.BasicAuthentication = new StubBasicAuthenticationDto
+            {
+                Username = username, Password = password
+            };
+            return this;
+        }
+
+        public StubConditionBuilder WithClientIp(string clientIp)
+        {
+            _conditions.ClientIp = clientIp;
+            return this;
+        }
+
+        public StubConditionBuilder WithIpInBlock(string ipStartingRange, string cidr)
+        {
+            _conditions.ClientIp = $"{ipStartingRange}/{cidr}";
+            return this;
+        }
+
+        public StubConditionBuilder WithHost(string hostname)
+        {
+            _conditions.Host = hostname;
+            return this;
+        }
+
+        public StubConditionBuilder WithJsonObject(object jsonObject)
+        {
+            _conditions.Json = jsonObject;
+            return this;
+        }
+
+        public StubConditionBuilder WithJsonArray(object[] jsonArray)
+        {
+            _conditions.Json = jsonArray;
+            return this;
+        }
+
+        public StubConditionsDto Build() => _conditions;
+
+        private void EnsureUrlConditions() => _conditions.Url ??= new StubUrlConditionDto();
+    }
+}
