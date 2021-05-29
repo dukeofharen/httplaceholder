2aacc89c0d3133781a56b37539a6e21c21703709
Duco <git@ducode.org>
2021-05-29 12:56:10 +0200

    Refactored URL, client IP and request headers

--- gui/src/components/requests/Request.vue
+++ gui/src/components/requests/Request.vue
@@ -26,205 +26,198 @@
             @click="createStub"
             title="Create a stub based on the request parameters of this request"
             color="success"
-          >Create stub
+            >Create stub
           </v-btn>
+        </v-col>
+
+        <v-col cols="12">
+          <span class="label">URL</span>
+          <span>{{ request.requestParameters.url }}</span>
+        </v-col>
+
+        <v-col cols="12">
+          <span class="label">Client IP</span>
+          <span>{{ request.requestParameters.clientIp }}</span>
+        </v-col>
+
+        <v-col cols="12">
+          <span class="label">Headers</span>
+          <RequestHeaders :requestParameters="request.requestParameters" />
         </v-col>
 
         <v-col cols="12" v-if="request.requestParameters.body">
           <span class="label">Body</span>
-          <RequestBody v-bind:requestParameters="request.requestParameters" />
+          <RequestBody :requestParameters="request.requestParameters" />
         </v-col>
       </v-row>
-<!--      <v-list-item v-if="request.requestParameters.body">-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>Body</v-list-item-title>-->
-<!--          <RequestBody v-bind:requestParameters="request.requestParameters" />-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-<!--      <v-list-item>-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>URL</v-list-item-title>-->
-<!--          <v-list-item-subtitle-->
-<!--            >{{ request.requestParameters.url }}-->
-<!--          </v-list-item-subtitle>-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-<!--      <v-list-item>-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>Client IP</v-list-item-title>-->
-<!--          <v-list-item-subtitle-->
-<!--            >{{ request.requestParameters.clientIp }}-->
-<!--          </v-list-item-subtitle>-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-<!--      <v-list-item>-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>Headers</v-list-item-title>-->
-<!--          <v-list-item-subtitle>-->
-<!--            <span-->
-<!--              v-for="(value, key) in request.requestParameters.headers"-->
-<!--              :key="key"-->
-<!--            >-->
-<!--              {{ key }}: {{ value }}-->
-<!--              <br />-->
-<!--            </span>-->
-<!--          </v-list-item-subtitle>-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-<!--      <v-list-item v-if="showQueryParameters">-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>Query parameters</v-list-item-title>-->
-<!--          <v-list-item-subtitle>-->
-<!--            <span v-for="(value, key) in queryParameters" :key="key">-->
-<!--              {{ key }}: {{ value }}-->
-<!--              <br />-->
-<!--            </span>-->
-<!--          </v-list-item-subtitle>-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-<!--      <v-list-item>-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>Correlation ID</v-list-item-title>-->
-<!--          <v-list-item-subtitle-->
-<!--            >{{ request.correlationId }}-->
-<!--          </v-list-item-subtitle>-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-<!--      <v-list-item>-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>Executed stub</v-list-item-title>-->
-<!--          <v-list-item-subtitle>-->
-<!--            <router-link-->
-<!--              :to="{-->
-<!--                name: 'stubs',-->
-<!--                query: { searchTerm: request.executingStubId }-->
-<!--              }"-->
-<!--              >{{ request.executingStubId }}-->
-<!--            </router-link>-->
-<!--          </v-list-item-subtitle>-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-<!--      <v-list-item>-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>Stub tenant (category)</v-list-item-title>-->
-<!--          <v-list-item-subtitle>-->
-<!--            <router-link-->
-<!--              :to="{ name: 'stubs', query: { stubTenant: request.stubTenant } }"-->
-<!--              >{{ request.stubTenant }}-->
-<!--            </router-link>-->
-<!--          </v-list-item-subtitle>-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-<!--      <v-list-item>-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>Request time</v-list-item-title>-->
-<!--          <v-list-item-subtitle-->
-<!--            >{{ overviewRequest.requestEndTime | datetime }} (it took-->
-<!--            <em>{{ duration | decimal }}</em> ms)-->
-<!--          </v-list-item-subtitle>-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-<!--      <v-expansion-panels>-->
-<!--        <v-expansion-panel v-if="orderedStubExecutionResults.length">-->
-<!--          <v-expansion-panel-header>-->
-<!--            <strong>Stub execution results</strong>-->
-<!--          </v-expansion-panel-header>-->
-<!--          <v-expansion-panel-content>-->
-<!--            <v-expansion-panels>-->
-<!--              <v-expansion-panel-->
-<!--                v-for="(result, key) in orderedStubExecutionResults"-->
-<!--                :key="key"-->
-<!--              >-->
-<!--                <v-expansion-panel-header>-->
-<!--                  <strong>-->
-<!--                    <span>{{ result.stubId }}</span>-->
-<!--                    <span>&nbsp;</span>-->
-<!--                    <span>(</span>-->
-<!--                    <span>-->
-<!--                      <Bool-->
-<!--                        v-bind:bool="result.passed"-->
-<!--                        trueText="passed"-->
-<!--                        falseText="not passed"-->
-<!--                      />-->
-<!--                    </span>-->
-<!--                    <span>)</span>-->
-<!--                  </strong>-->
-<!--                </v-expansion-panel-header>-->
-<!--                <v-expansion-panel-content>-->
-<!--                  <div v-if="result.conditions.length > 0">-->
-<!--                    <h2>Executed conditions</h2>-->
-<!--                    <div-->
-<!--                      v-for="(condition, key) in result.conditions"-->
-<!--                      :key="key"-->
-<!--                    >-->
-<!--                      <v-list-item>-->
-<!--                        <v-list-item-content>-->
-<!--                          <v-list-item-title>Checker name</v-list-item-title>-->
-<!--                          <v-list-item-subtitle-->
-<!--                            >{{ condition.checkerName }}-->
-<!--                          </v-list-item-subtitle>-->
-<!--                        </v-list-item-content>-->
-<!--                      </v-list-item>-->
-<!--                      <v-list-item>-->
-<!--                        <v-list-item-content>-->
-<!--                          <v-list-item-title-->
-<!--                            >Condition validation-->
-<!--                          </v-list-item-title>-->
-<!--                          <v-list-item-subtitle>-->
-<!--                            <Bool-->
-<!--                              v-bind:bool="-->
-<!--                                condition.conditionValidation ===-->
-<!--                                  conditionValidationType.Valid-->
-<!--                              "-->
-<!--                              trueText="passed"-->
-<!--                              falseText="not passed"-->
-<!--                            />-->
-<!--                          </v-list-item-subtitle>-->
-<!--                        </v-list-item-content>-->
-<!--                      </v-list-item>-->
-<!--                      <v-list-item v-if="condition.log">-->
-<!--                        <v-list-item-content>-->
-<!--                          <v-list-item-title>Log</v-list-item-title>-->
-<!--                          <v-list-item-subtitle-->
-<!--                            >{{ condition.log }}-->
-<!--                          </v-list-item-subtitle>-->
-<!--                        </v-list-item-content>-->
-<!--                      </v-list-item>-->
-<!--                      <v-divider></v-divider>-->
-<!--                    </div>-->
-<!--                  </div>-->
-<!--                </v-expansion-panel-content>-->
-<!--              </v-expansion-panel>-->
-<!--            </v-expansion-panels>-->
-<!--          </v-expansion-panel-content>-->
-<!--        </v-expansion-panel>-->
-<!--        <v-expansion-panel v-if="orderedStubResponseWriterResults.length">-->
-<!--          <v-expansion-panel-header>-->
-<!--            <strong>Response writer results</strong>-->
-<!--          </v-expansion-panel-header>-->
-<!--          <v-expansion-panel-content>-->
-<!--            <v-list-item-->
-<!--              v-for="(result, key) in orderedStubResponseWriterResults"-->
-<!--              :key="key"-->
-<!--            >-->
-<!--              <v-list-item-content>-->
-<!--                <v-list-item-title-->
-<!--                  >{{ result.responseWriterName }}-->
-<!--                </v-list-item-title>-->
-<!--                <v-list-item-subtitle>-->
-<!--                  <Bool-->
-<!--                    v-bind:bool="result.executed"-->
-<!--                    trueText="executed"-->
-<!--                    falseText="not executed"-->
-<!--                  />-->
-<!--                  <br />-->
-<!--                  <span v-if="result.log">{{ result.log }}</span>-->
-<!--                </v-list-item-subtitle>-->
-<!--              </v-list-item-content>-->
-<!--            </v-list-item>-->
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+      <!--      <v-list-item>-->
+      <!--        <v-list-item-content>-->
+      <!--          <v-list-item-title>Headers</v-list-item-title>-->
+      <!--          <v-list-item-subtitle>-->
+      <!--            <span-->
+      <!--              v-for="(value, key) in request.requestParameters.headers"-->
+      <!--              :key="key"-->
+      <!--            >-->
+      <!--              {{ key }}: {{ value }}-->
+      <!--              <br />-->
+      <!--            </span>-->
+      <!--          </v-list-item-subtitle>-->
+      <!--        </v-list-item-content>-->
+      <!--      </v-list-item>-->
+      <!--      <v-list-item v-if="showQueryParameters">-->
+      <!--        <v-list-item-content>-->
+      <!--          <v-list-item-title>Query parameters</v-list-item-title>-->
+      <!--          <v-list-item-subtitle>-->
+      <!--            <span v-for="(value, key) in queryParameters" :key="key">-->
+      <!--              {{ key }}: {{ value }}-->
+      <!--              <br />-->
+      <!--            </span>-->
+      <!--          </v-list-item-subtitle>-->
+      <!--        </v-list-item-content>-->
+      <!--      </v-list-item>-->
+      <!--      <v-list-item>-->
+      <!--        <v-list-item-content>-->
+      <!--          <v-list-item-title>Correlation ID</v-list-item-title>-->
+      <!--          <v-list-item-subtitle-->
+      <!--            >{{ request.correlationId }}-->
+      <!--          </v-list-item-subtitle>-->
+      <!--        </v-list-item-content>-->
+      <!--      </v-list-item>-->
+      <!--      <v-list-item>-->
+      <!--        <v-list-item-content>-->
+      <!--          <v-list-item-title>Executed stub</v-list-item-title>-->
+      <!--          <v-list-item-subtitle>-->
+      <!--            <router-link-->
+      <!--              :to="{-->
+      <!--                name: 'stubs',-->
+      <!--                query: { searchTerm: request.executingStubId }-->
+      <!--              }"-->
+      <!--              >{{ request.executingStubId }}-->
+      <!--            </router-link>-->
+      <!--          </v-list-item-subtitle>-->
+      <!--        </v-list-item-content>-->
+      <!--      </v-list-item>-->
+      <!--      <v-list-item>-->
+      <!--        <v-list-item-content>-->
+      <!--          <v-list-item-title>Stub tenant (category)</v-list-item-title>-->
+      <!--          <v-list-item-subtitle>-->
+      <!--            <router-link-->
+      <!--              :to="{ name: 'stubs', query: { stubTenant: request.stubTenant } }"-->
+      <!--              >{{ request.stubTenant }}-->
+      <!--            </router-link>-->
+      <!--          </v-list-item-subtitle>-->
+      <!--        </v-list-item-content>-->
+      <!--      </v-list-item>-->
+      <!--      <v-list-item>-->
+      <!--        <v-list-item-content>-->
+      <!--          <v-list-item-title>Request time</v-list-item-title>-->
+      <!--          <v-list-item-subtitle-->
+      <!--            >{{ overviewRequest.requestEndTime | datetime }} (it took-->
+      <!--            <em>{{ duration | decimal }}</em> ms)-->
+      <!--          </v-list-item-subtitle>-->
+      <!--        </v-list-item-content>-->
+      <!--      </v-list-item>-->
+      <!--      <v-expansion-panels>-->
+      <!--        <v-expansion-panel v-if="orderedStubExecutionResults.length">-->
+      <!--          <v-expansion-panel-header>-->
+      <!--            <strong>Stub execution results</strong>-->
+      <!--          </v-expansion-panel-header>-->
+      <!--          <v-expansion-panel-content>-->
+      <!--            <v-expansion-panels>-->
+      <!--              <v-expansion-panel-->
+      <!--                v-for="(result, key) in orderedStubExecutionResults"-->
+      <!--                :key="key"-->
+      <!--              >-->
+      <!--                <v-expansion-panel-header>-->
+      <!--                  <strong>-->
+      <!--                    <span>{{ result.stubId }}</span>-->
+      <!--                    <span>&nbsp;</span>-->
+      <!--                    <span>(</span>-->
+      <!--                    <span>-->
+      <!--                      <Bool-->
+      <!--                        v-bind:bool="result.passed"-->
+      <!--                        trueText="passed"-->
+      <!--                        falseText="not passed"-->
+      <!--                      />-->
+      <!--                    </span>-->
+      <!--                    <span>)</span>-->
+      <!--                  </strong>-->
+      <!--                </v-expansion-panel-header>-->
+      <!--                <v-expansion-panel-content>-->
+      <!--                  <div v-if="result.conditions.length > 0">-->
+      <!--                    <h2>Executed conditions</h2>-->
+      <!--                    <div-->
+      <!--                      v-for="(condition, key) in result.conditions"-->
+      <!--                      :key="key"-->
+      <!--                    >-->
+      <!--                      <v-list-item>-->
+      <!--                        <v-list-item-content>-->
+      <!--                          <v-list-item-title>Checker name</v-list-item-title>-->
+      <!--                          <v-list-item-subtitle-->
+      <!--                            >{{ condition.checkerName }}-->
+      <!--                          </v-list-item-subtitle>-->
+      <!--                        </v-list-item-content>-->
+      <!--                      </v-list-item>-->
+      <!--                      <v-list-item>-->
+      <!--                        <v-list-item-content>-->
+      <!--                          <v-list-item-title-->
+      <!--                            >Condition validation-->
+      <!--                          </v-list-item-title>-->
+      <!--                          <v-list-item-subtitle>-->
+      <!--                            <Bool-->
+      <!--                              v-bind:bool="-->
+      <!--                                condition.conditionValidation ===-->
+      <!--                                  conditionValidationType.Valid-->
+      <!--                              "-->
+      <!--                              trueText="passed"-->
+      <!--                              falseText="not passed"-->
+      <!--                            />-->
+      <!--                          </v-list-item-subtitle>-->
+      <!--                        </v-list-item-content>-->
+      <!--                      </v-list-item>-->
+      <!--                      <v-list-item v-if="condition.log">-->
+      <!--                        <v-list-item-content>-->
+      <!--                          <v-list-item-title>Log</v-list-item-title>-->
+      <!--                          <v-list-item-subtitle-->
+      <!--                            >{{ condition.log }}-->
+      <!--                          </v-list-item-subtitle>-->
+      <!--                        </v-list-item-content>-->
+      <!--                      </v-list-item>-->
+      <!--                      <v-divider></v-divider>-->
+      <!--                    </div>-->
+      <!--                  </div>-->
+      <!--                </v-expansion-panel-content>-->
+      <!--              </v-expansion-panel>-->
+      <!--            </v-expansion-panels>-->
+      <!--          </v-expansion-panel-content>-->
+      <!--        </v-expansion-panel>-->
+      <!--        <v-expansion-panel v-if="orderedStubResponseWriterResults.length">-->
+      <!--          <v-expansion-panel-header>-->
+      <!--            <strong>Response writer results</strong>-->
+      <!--          </v-expansion-panel-header>-->
+      <!--          <v-expansion-panel-content>-->
+      <!--            <v-list-item-->
+      <!--              v-for="(result, key) in orderedStubResponseWriterResults"-->
+      <!--              :key="key"-->
+      <!--            >-->
+      <!--              <v-list-item-content>-->
+      <!--                <v-list-item-title-->
+      <!--                  >{{ result.responseWriterName }}-->
+      <!--                </v-list-item-title>-->
+      <!--                <v-list-item-subtitle>-->
+      <!--                  <Bool-->
+      <!--                    v-bind:bool="result.executed"-->
+      <!--                    trueText="executed"-->
+      <!--                    falseText="not executed"-->
+      <!--                  />-->
+      <!--                  <br />-->
+      <!--                  <span v-if="result.log">{{ result.log }}</span>-->
+      <!--                </v-list-item-subtitle>-->
+      <!--              </v-list-item-content>-->
+      <!--            </v-list-item>-->
+      <!--          </v-expansion-panel-content>-->
+      <!--        </v-expansion-panel>-->
+      <!--      </v-expansion-panels>-->
     </v-expansion-panel-content>
   </v-expansion-panel>
 </template>
@@ -233,6 +226,7 @@
 import RequestBody from "@/components/requests/RequestBody";
 import Bool from "@/components/requests/Bool";
 import HttpMethod from "@/components/requests/HttpMethod";
+import RequestHeaders from "@/components/requests/RequestHeaders";
 import { parseUrl } from "@/utils/urlFunctions";
 import { toastError, toastSuccess } from "@/utils/toastUtil";
 import { resources } from "@/shared/resources";
@@ -266,7 +260,8 @@ export default {
   components: {
     RequestBody,
     Bool,
-    HttpMethod
+    HttpMethod,
+    RequestHeaders
   },
   computed: {
     orderedStubExecutionResults() {
---
+++ gui/src/components/requests/RequestHeaders.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-simple-table dense>
+    <template v-slot:default>
+      <tbody>
+        <tr v-for="(value, key) in requestParameters.headers">
+          <td>{{ key }}</td>
+          <td>{{ value }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  name: "RequestHeaders",
+  props: ["requestParameters"]
+};
+</script>
+
+<style scoped></style>
