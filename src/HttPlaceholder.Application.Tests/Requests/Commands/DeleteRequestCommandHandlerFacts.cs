a60dc6040eba86f3a4e4824c976be3d54bf663ec
Duco <git@ducode.org>
2021-06-06 19:57:04 +0200

    Added failing unit test for DeleteRequestCommandHandler

---
+++ src/HttPlaceholder.Application.Tests/Requests/Commands/DeleteRequestCommandHandlerFacts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using HttPlaceholder.Application.Requests.Commands.DeleteRequest;
+using HttPlaceholder.Application.StubExecution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace HttPlaceholder.Application.Tests.Requests.Commands
+{
+    [TestClass]
+    public class DeleteRequestCommandHandlerFacts
+    {
+        private readonly Mock<IStubContext> _mockStubContext = new();
+        private DeleteRequestCommandHandler _handler;
+
+        [TestInitialize]
+        public void Initialize() => _handler = new DeleteRequestCommandHandler(_mockStubContext.Object);
+
+        [TestCleanup]
+        public void Cleanup() => _mockStubContext.VerifyAll();
+
+        [TestMethod]
+        public async Task Handle_HappyFlow()
+        {
+            // Arrange
+            var request = new DeleteRequestCommand(Guid.NewGuid().ToString());
+
+            // Act
+            await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            _mockStubContext.Verify(m => m.DeleteRequestAsync(request.CorrelationId));
+;        }
+    }
+}
--- src/HttPlaceholder.Application/Requests/Commands/DeleteRequest/DeleteRequestCommandHandler.cs
+++ src/HttPlaceholder.Application/Requests/Commands/DeleteRequest/DeleteRequestCommandHandler.cs
@@ -1,11 +1,19 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using HttPlaceholder.Application.StubExecution;
 using MediatR;
 
 namespace HttPlaceholder.Application.Requests.Commands.DeleteRequest
 {
     public class DeleteRequestCommandHandler : IRequestHandler<DeleteRequestCommand>
     {
+        private readonly IStubContext _stubContext;
+
+        public DeleteRequestCommandHandler(IStubContext stubContext)
+        {
+            _stubContext = stubContext;
+        }
+
         public Task<Unit> Handle(DeleteRequestCommand request, CancellationToken cancellationToken) => throw new System.NotImplementedException();
     }
 }
