3e9db887a3c136e44427a974b24c4e07c770f9c5
Duco <git@ducode.org>
2021-08-15 19:51:47 +0200

    Added StubJsonPathModel

---
+++ src/HttPlaceholder.Client/Dto/Stubs/StubJsonPathModel.cs
@@ -0,0 +1,18 @@
+﻿namespace HttPlaceholder.Client.Dto.Stubs
+{
+    /// <summary>
+    /// A model for storing data for the JSONPath condition checker.
+    /// </summary>
+    public class StubJsonPathModel
+    {
+        /// <summary>
+        /// Gets or sets the JSONPath query.
+        /// </summary>
+        public string Query { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expected value.
+        /// </summary>
+        public string ExpectedValue { get; set; }
+    }
+}
