30f6e7178b564a6244981022169eb6cc838b9f6f
Duco <git@ducode.org>
2021-09-19 11:45:40 +0200

    Added Bootstrap plugin with imports

--- guiv3/src/main.js
+++ guiv3/src/main.js
@@ -4,4 +4,7 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+// Plugins
+import "@/plugins/bootstrap";
+
 createApp(App).use(store).use(router).mount("#app");
