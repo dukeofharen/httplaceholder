4aea0fd94869d9d596bf49a7f1051e1cbbabfc10
Duco <git@ducode.org>
2021-09-24 21:55:13 +0200

    JSON fixes in api.js

--- guiv3/src/utils/api.js
+++ guiv3/src/utils/api.js
@@ -1,17 +1,36 @@
 const handleResponse = (response) => {
+  const headers = {};
+  for (let header of response.headers.entries()) {
+    headers[header[0]] = header[1];
+  }
+
+  const contentType = headers["content-type"];
+  let isJson = false;
+  if (contentType && contentType.includes("application/json")) {
+    isJson = true;
+  }
+
   if (!response.ok) {
     const error = new Error(response.statusText);
-    error.json = response.json();
+    error.json = isJson ? response.json() : response.text();
     throw error;
   }
 
-  return response.json();
+  return isJson ? response.json() : response.text();
 };
 
 export function get(url, options) {
   options = options || {};
   return fetch(url, {
     method: "get",
+    headers: options.headers || {},
+  }).then(handleResponse);
+}
+
+export function del(url, options) {
+  options = options || {};
+  return fetch(url, {
+    method: "delete",
     headers: options.headers || {},
   }).then(handleResponse);
 }
