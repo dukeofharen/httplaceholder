081617e0eafe5334d4f6c83fa4ac8b8c4ad0a70f
Duco <git@ducode.org>
2021-10-02 14:43:36 +0200

    Added control + s saving

--- guiv3/src/views/StubForm.vue
+++ guiv3/src/views/StubForm.vue
@@ -1,40 +1,42 @@
 <template>
-  <h1>{{ title }}</h1>
+  <div @keydown="checkSave">
+    <h1>{{ title }}</h1>
 
-  <div class="row">
-    <div class="col-md-12">
-      Fill in the stub below in YAML format and click on "Save". For examples,
-      visit
-      <a href="https://github.com/dukeofharen/httplaceholder" target="_blank"
-        >https://github.com/dukeofharen/httplaceholder</a
-      >.
+    <div class="row">
+      <div class="col-md-12">
+        Fill in the stub below in YAML format and click on "Save". For examples,
+        visit
+        <a href="https://github.com/dukeofharen/httplaceholder" target="_blank"
+          >https://github.com/dukeofharen/httplaceholder</a
+        >.
+      </div>
     </div>
-  </div>
 
-  <FormHelperSelector />
+    <FormHelperSelector />
 
-  <div class="row mt-2">
-    <div class="col-md-12">
-      <codemirror v-model="input" :options="cmOptions" />
+    <div class="row mt-2">
+      <div class="col-md-12">
+        <codemirror v-model="input" :options="cmOptions" />
+      </div>
     </div>
-  </div>
 
-  <div class="row mt-2">
-    <div class="col-md-12">
-      <button class="btn btn-success me-2" @click="save">Save</button>
-      <button
-        type="button"
-        class="btn btn-danger"
-        @click="showResetModal = true"
-      >
-        Reset
-      </button>
-      <modal
-        title="Reset to defaults?"
-        :yes-click-function="reset"
-        :show-modal="showResetModal"
-        @close="showResetModal = false"
-      />
+    <div class="row mt-2">
+      <div class="col-md-12">
+        <button class="btn btn-success me-2" @click="save">Save</button>
+        <button
+          type="button"
+          class="btn btn-danger"
+          @click="showResetModal = true"
+        >
+          Reset
+        </button>
+        <modal
+          title="Reset to defaults?"
+          :yes-click-function="reset"
+          :show-modal="showResetModal"
+          @close="showResetModal = false"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -96,6 +98,12 @@ export default {
       input.value = resources.defaultStub;
       stubId.value = "";
     };
+    const checkSave = async (e) => {
+      e.preventDefault();
+      if (e.ctrlKey && e.key === "s") {
+        await save();
+      }
+    };
 
     // Lifecycle
     onMounted(async () => {
@@ -122,6 +130,7 @@ export default {
       save,
       showResetModal,
       reset,
+      checkSave,
     };
   },
 };
