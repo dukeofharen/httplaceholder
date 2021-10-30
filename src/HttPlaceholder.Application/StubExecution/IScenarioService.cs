777f999dcca9e93027c57eb5b69f7b9aedbb9b3f
Duco <git@ducode.org>
2021-10-30 14:41:58 +0200

    Added method to ScenarioService for retrieving hit count

--- src/HttPlaceholder.Application/StubExecution/IScenarioService.cs
+++ src/HttPlaceholder.Application/StubExecution/IScenarioService.cs
@@ -8,7 +8,14 @@
         /// <summary>
         /// Increases the hit count of a specific scenario.
         /// </summary>
-        /// <param name="scenario"></param>
+        /// <param name="scenario">The scenario name. Is case insensitive.</param>
         void IncreaseHitCount(string scenario);
+
+        /// <summary>
+        /// Get the hit count of a specific scenario.
+        /// </summary>
+        /// <param name="scenario">The scenario name. Is case insensitive.</param>
+        /// <returns>The hit count.</returns>
+        int? GetHitCount(string scenario);
     }
 }
--- src/HttPlaceholder.Application/StubExecution/Implementations/ScenarioService.cs
+++ src/HttPlaceholder.Application/StubExecution/Implementations/ScenarioService.cs
@@ -22,13 +22,30 @@ namespace HttPlaceholder.Application.StubExecution.Implementations
 
             lock (_scenarioStateStore.GetScenarioLock(scenario))
             {
-                var scenarioState = _scenarioStateStore.GetScenario(scenario) ??
-                                     _scenarioStateStore.AddScenario(
-                                        scenario,
-                                        new ScenarioStateModel(scenario));
+                var scenarioState = GetOrAddScenarioState(scenario);
                 scenarioState.HitCount++;
                 _scenarioStateStore.UpdateScenario(scenario, scenarioState);
             }
         }
+
+        public int? GetHitCount(string scenario)
+        {
+            if (string.IsNullOrWhiteSpace(scenario))
+            {
+                return null;
+            }
+
+            lock (_scenarioStateStore.GetScenarioLock(scenario))
+            {
+                var scenarioState = GetOrAddScenarioState(scenario);
+                return scenarioState.HitCount;
+            }
+        }
+
+        private ScenarioStateModel GetOrAddScenarioState(string scenario) =>
+            _scenarioStateStore.GetScenario(scenario) ??
+            _scenarioStateStore.AddScenario(
+                scenario,
+                new ScenarioStateModel(scenario));
     }
 }
