cf211f3a508c57f498465eb81e3914ccb6c11d05
Duco <git@ducode.org>
2021-11-05 19:05:42 +0100

    Added scenario stub stuff to client

--- src/HttPlaceholder.Client/Dto/Stubs/StubScenarioScenarioDto.cs
+++ src/HttPlaceholder.Client/Dto/Stubs/StubScenarioScenarioDto.cs
@@ -1,36 +1,28 @@
-﻿using HttPlaceholder.Application.Interfaces.Mappings;
-using HttPlaceholder.Domain;
-using YamlDotNet.Serialization;
-
-namespace HttPlaceholder.Dto.v1.Stubs
+﻿namespace HttPlaceholder.Client.Dto.Stubs
 {
     /// <summary>
     /// A model for storing all scenario conditions for a stub
     /// </summary>
-    public class StubConditionScenarioDto : IMapFrom<StubConditionScenarioModel>, IMapTo<StubConditionScenarioModel>
+    public class StubConditionScenarioDto
     {
         /// <summary>
         /// Gets or sets the inclusive min hit count.
         /// </summary>
-        [YamlMember(Alias = "minHits")]
         public int? MinHits { get; set; }
 
         /// <summary>
         /// Gets or sets the exclusive max hit count.
         /// </summary>
-        [YamlMember(Alias = "maxHits")]
         public int? MaxHits { get; set; }
 
         /// <summary>
         /// Gets or sets the exact hits count.
         /// </summary>
-        [YamlMember(Alias = "exactHits")]
         public int? ExactHits { get; set; }
 
         /// <summary>
         /// Gets or sets the state the scenario should be in.
         /// </summary>
-        [YamlMember(Alias = "scenarioState")]
         public string ScenarioState { get; set; }
     }
 }
