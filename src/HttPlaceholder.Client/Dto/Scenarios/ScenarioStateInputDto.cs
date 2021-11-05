196eea765087924c50747eb8ef08b991884f51e7
Duco <git@ducode.org>
2021-11-05 19:07:37 +0100

    Added DTOs for scenario API

---
+++ src/HttPlaceholder.Client/Dto/Scenarios/ScenarioStateInputDto.cs
@@ -0,0 +1,18 @@
+﻿namespace HttPlaceholder.Client.Dto.Scenarios
+{
+    /// <summary>
+    /// A model that is used to set the scenario.
+    /// </summary>
+    public class ScenarioStateInputDto
+    {
+        /// <summary>
+        /// Gets or sets the state the scenario is in.
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of times the scenario has been hit.
+        /// </summary>
+        public int HitCount { get; set; }
+    }
+}
