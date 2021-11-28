c51da88dbff2b95856404de5cf06b1dc2bd4d7de
Duco <git@ducode.org>
2021-11-28 17:52:10 +0100

    Added empty CurlToHttpRequestMapper.cs

---
+++ src/HttPlaceholder.Application/StubExecution/ICurlToHttpRequestMapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using HttPlaceholder.Application.StubExecution.Models;
+using HttPlaceholder.Domain;
+
+namespace HttPlaceholder.Application.StubExecution
+{
+    /// <summary>
+    /// Describes a class that is being used to convert one or multiple cURL commands to <see cref="HttpRequestModel"/>.
+    /// </summary>
+    public interface ICurlToHttpRequestMapper
+    {
+        /// <summary>
+        /// Converts one or multiple cURL commands to <see cref="HttpRequestModel"/>.
+        /// </summary>
+        /// <param name="commands">The cURL command(s).</param>
+        /// <returns>A list of <see cref="StubModel"/>.</returns>
+        IEnumerable<StubModel> MapCurlCommandsToHttpRequest(string commands);
+    }
+}
---
+++ src/HttPlaceholder.Application/StubExecution/Implementations/CurlToHttpRequestMapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using HttPlaceholder.Domain;
+
+namespace HttPlaceholder.Application.StubExecution.Implementations
+{
+    /// <inheritdoc/>
+    internal class CurlToHttpRequestMapper : ICurlToHttpRequestMapper
+    {
+        /// <inheritdoc/>
+        public IEnumerable<StubModel> MapCurlCommandsToHttpRequest(string commands) =>
+            throw new System.NotImplementedException();
+    }
+}
