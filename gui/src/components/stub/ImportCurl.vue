9bce5f5535077d1296007778e06752bea65e0805
Duco <git@ducode.org>
2021-12-02 20:06:21 +0100

    Added explanation to import form

--- gui/src/components/stub/ImportCurl.vue
+++ gui/src/components/stub/ImportCurl.vue
@@ -4,6 +4,42 @@
     use a cURL command you have lying around or you can copy/paste a cURL
     command from the developer console from your browser.
   </div>
+  <div class="mb-2">
+    <button
+      class="btn btn-outline-primary btn-sm"
+      @click="howToOpen = !howToOpen"
+    >
+      How to
+    </button>
+  </div>
+  <div v-if="howToOpen">
+    <div class="row">
+      <div class="col-md-12">
+        You can copy/paste a cURL command from your browser. In most popular web
+        browsers, you can do this by going to the developer tools, going to the
+        "Network" tab and selecting the request where you would like to have the
+        cURL request for.
+      </div>
+    </div>
+    <div class="row mb-2">
+      <div class="col-md-4">
+        <img src="@/assets/curl_copy_firefox.png" />
+        <em>Example in Firefox</em>
+      </div>
+      <div class="col-md-4">
+        <img src="@/assets/curl_copy_chrome.png" />
+        <em
+          >Example in Chrome. In Chrome, you can either select "Copy as cURL" or
+          "Copy all as cURL".
+        </em>
+      </div>
+      <div class="col-md-12 mb-2 mt-2">
+        <button class="btn btn-outline-primary btn-sm" @click="insertExample">
+          Insert example
+        </button>
+      </div>
+    </div>
+  </div>
   <div class="mb-2" v-if="!stubsYaml">
     <textarea class="form-control" v-model="curlInput"></textarea>
   </div>
@@ -52,6 +88,7 @@ export default {
     // Data
     const curlInput = ref("");
     const stubsYaml = ref("");
+    const howToOpen = ref(false);
 
     // Computed
     const importButtonEnabled = computed(() => !!curlInput.value);
@@ -99,6 +136,9 @@ export default {
       curlInput.value = "";
       stubsYaml.value = "";
     };
+    const insertExample = () => {
+      curlInput.value = resources.exampleCurlInput;
+    };
     const handleSave = async (e) => {
       if (shouldSave(e)) {
         e.preventDefault();
@@ -127,6 +167,8 @@ export default {
       reset,
       handleSave,
       importButtonEnabled,
+      howToOpen,
+      insertExample,
     };
   },
 };
@@ -139,5 +181,9 @@ textarea {
   overflow-wrap: normal;
   overflow-x: scroll;
   min-height: 300px;
+}
+
+img {
+  width: 100%;
 }
 </style>
