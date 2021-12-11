7d9a4713471d973ab29d6f41b390fd3707a2d52b
Duco <git@ducode.org>
2021-12-11 20:12:33 +0100

    Updated script that creates GitHub release: changelog is now sent to release

--- scripts/publish/publish-to-github.ps1
+++ scripts/publish/publish-to-github.ps1
@@ -16,11 +16,40 @@ $repo = "httplaceholder"
 $userAgent = "$owner/$repo"
 $accept = "application/vnd.github.v3+json"
 
+# Parse changelog so the changelog entry of the latest release is parsed.
+$changelogPath = Join-Path $PSScriptRoot ".." ".." "CHANGELOG" -Resolve
+$changelog = Get-Content $changelogPath
+$firstVersionFound = $false
+$changelogLines = @()
+foreach ($line in $changelog)
+{
+    if ($line -match '\[[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}(\.[0-9]{1,})?\]')
+    {
+        # Line is version number.
+        if (!$firstVersionFound)
+        {
+            $firstVersionFound = $true
+        }
+        else
+        {
+            break
+        }
+    }
+    elseif(![string]::IsNullOrWhiteSpace($line.Trim()))
+    {
+        $changelogLines += $line
+    }
+}
+
+$changelogForVersion = [string]::Join("`n", $changelogLines)
+
 # Create GitHub release
 $tagName = "v$version"
-$release = @{ tag_name = $tagName; target_commitish = $commitHash; name = $tagName; body = ""; draft = $true }
+$release = @{ tag_name = $tagName; target_commitish = $commitHash; name = $tagName; body = $changelogForVersion; draft = $true }
 $releaseJson = ConvertTo-Json $release
-$createReleaseResponse = Invoke-WebRequest -Uri "https://api.github.com/repos/$owner/$repo/releases" `
+# $releaseUrl = "https://api.github.com/repos/$owner/$repo/releases"
+$releaseUrl = "http://localhost:5000/repos/$owner/$repo/releases"
+$createReleaseResponse = Invoke-WebRequest -Uri $releaseUrl`
     -Method Post `
     -Body $releaseJson `
     -Headers @{ "Authorization" = "token $apiKey"; "User-Agent" = $userAgent; "Accept" = $accept } `
@@ -34,11 +63,11 @@ $files = Get-ChildItem $distFolder
 foreach ($file in $files)
 {
     $filename = [System.IO.Path]::GetFileName($file)
-    $fullUploadUrl = "$($uploadUrl)?name=$filename"
+    $fullUploadUrl = "$( $uploadUrl )?name=$filename"
     Write-Host "Uploading file $file to GitHub Releases on URL $fullUploadUrl"
     Invoke-WebRequest -Uri $fullUploadUrl `
-        -Method Post `
-        -Headers @{ "Authorization" = "token $apiKey"; "User-Agent" = $userAgent; "Accept" = $accept; "Content-Type" = "application/octet-stream" } `
-        -UseBasicParsing `
-        -InFile $file
+         -Method Post `
+         -Headers @{ "Authorization" = "token $apiKey"; "User-Agent" = $userAgent; "Accept" = $accept; "Content-Type" = "application/octet-stream" } `
+         -UseBasicParsing `
+         -InFile $file
 }
