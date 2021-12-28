ee7dbf7774c196fe0c341b76494f0fc20493d3ae
Duco <git@ducode.org>
2021-12-28 16:39:16 +0100

    Added HttpResponseModel

---
+++ src/HttPlaceholder.Application/StubExecution/Models/HttpResponseModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace HttPlaceholder.Application.StubExecution.Models;
+
+/// <summary>
+/// A model that contains a representation of an HTTP response.
+/// </summary>
+public class HttpResponseModel
+{
+    /// <summary>
+    /// Gets or sets the HTTP status code.
+    /// </summary>
+    public int StatusCode { get; set; }
+
+    /// <summary>
+    /// Gets or sets the headers.
+    /// </summary>
+    public IDictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
+
+    /// <summary>
+    /// Gets or sets the HTTP response content.
+    /// </summary>
+    public string Content { get; set; }
+}
