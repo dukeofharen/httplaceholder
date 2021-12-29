4bae3a5672e9ccf2ae21b56ad9bc9ee4da99207c
Duco <git@ducode.org>
2021-12-29 11:37:27 +0100

    Implemented HttpResponseToStubResponseService

---
+++ src/HttPlaceholder.Application.Tests/StubExecution/Implementations/HttpResponseToStubResponseServiceFacts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HttPlaceholder.Application.StubExecution.Implementations;
+using HttPlaceholder.Application.StubExecution.Models;
+using HttPlaceholder.Application.StubExecution.ResponseToStubResponseHandlers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.AutoMock;
+
+namespace HttPlaceholder.Application.Tests.StubExecution.Implementations;
+
+[TestClass]
+public class HttpResponseToStubResponseServiceFacts
+{
+    private readonly AutoMocker _mocker = new();
+    private readonly Mock<IResponseToStubResponseHandler> _handlerMock1 = new();
+    private readonly Mock<IResponseToStubResponseHandler> _handlerMock2 = new();
+
+    [TestInitialize]
+    public void Initialize() =>
+        _mocker.Use<IEnumerable<IResponseToStubResponseHandler>>(new[]
+        {
+            _handlerMock1.Object, _handlerMock2.Object
+        });
+
+    [TestMethod]
+    public async Task ConvertToResponseAsync_HappyFlow()
+    {
+        // Arrange
+        var service = _mocker.CreateInstance<HttpResponseToStubResponseService>();
+        var response = new HttpResponseModel();
+
+        // Act
+        var result = await service.ConvertToResponseAsync(response);
+
+        // Assert
+        _handlerMock1.Verify(m => m.HandleStubGenerationAsync(response, result));
+        _handlerMock2.Verify(m => m.HandleStubGenerationAsync(response, result));
+    }
+}
--- src/HttPlaceholder.Application/StubExecution/Implementations/HttpResponseToStubResponseService.cs
+++ src/HttPlaceholder.Application/StubExecution/Implementations/HttpResponseToStubResponseService.cs
@@ -1,13 +1,39 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using HttPlaceholder.Application.StubExecution.Models;
+using HttPlaceholder.Application.StubExecution.ResponseToStubResponseHandlers;
 using HttPlaceholder.Domain;
+using Microsoft.Extensions.Logging;
 
 namespace HttPlaceholder.Application.StubExecution.Implementations;
 
 /// <inheritdoc />
 public class HttpResponseToStubResponseService : IHttpResponseToStubResponseService
 {
+    private readonly IEnumerable<IResponseToStubResponseHandler> _handlers;
+    private readonly ILogger<HttpResponseToStubResponseService> _logger;
+
+    public HttpResponseToStubResponseService(
+        IEnumerable<IResponseToStubResponseHandler> handlers,
+        ILogger<HttpResponseToStubResponseService> logger)
+    {
+        _handlers = handlers;
+        _logger = logger;
+    }
+
     /// <inheritdoc />
-    public Task<StubResponseModel> ConvertToResponseAsync(HttpResponseModel response) =>
-        throw new System.NotImplementedException();
+    public async Task<StubResponseModel> ConvertToResponseAsync(HttpResponseModel response)
+    {
+        var stubResponse = new StubResponseModel();
+        foreach (var handler in _handlers.OrderByDescending(h => h.Priority))
+        {
+            var executed =
+                await handler.HandleStubGenerationAsync(response, stubResponse);
+            _logger.LogDebug(
+                $"Handler '{handler.GetType().Name}' " + (executed ? "executed" : "not executed") + ".");
+        }
+
+        return stubResponse;
+    }
 }
