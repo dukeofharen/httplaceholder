70ca79bb8b340e71943a2d727ad8d8ed295327c0
Duco <git@ducode.org>
2021-12-29 12:01:43 +0100

    Implemented ContentTypeHandler

---
+++ src/HttPlaceholder.Application.Tests/StubExecution/ResponseToStubResponseHandlers/ContentTypeHandlerFacts.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using HttPlaceholder.Application.StubExecution.Models;
+using HttPlaceholder.Application.StubExecution.ResponseToStubResponseHandlers;
+using HttPlaceholder.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttPlaceholder.Application.Tests.StubExecution.ResponseToStubResponseHandlers;
+
+[TestClass]
+public class ContentTypeHandlerFacts
+{
+    private readonly ContentTypeHandler _handler = new();
+
+    [TestMethod]
+    public async Task HandleStubGenerationAsync_NoContentTypeHeaderSet_ShouldReturnFalse()
+    {
+        // Arrange
+        var response = new HttpResponseModel {Headers = {{"x-api-key", "1223"}}};
+        var stubResponse = new StubResponseModel();
+
+        // Act
+        var result = await _handler.HandleStubGenerationAsync(response, stubResponse);
+
+        // Assert
+        Assert.IsFalse(result);
+        Assert.IsNull(stubResponse.ContentType);
+    }
+
+    [DataTestMethod]
+    [DataRow("content-type")]
+    [DataRow("CONTENT-TYPE")]
+    [DataRow("Content-Type")]
+    public async Task HandleStubGenerationAsync_ContentTypeHeaderSet_ShouldReturnTrue(string headerKey)
+    {
+        // Arrange
+        var response = new HttpResponseModel
+        {
+            Headers = {{"x-api-key", "1223"}, {headerKey, "application/json; charset=UTF-8"}}
+        };
+        var stubResponse = new StubResponseModel();
+
+        // Act
+        var result = await _handler.HandleStubGenerationAsync(response, stubResponse);
+
+        // Assert
+        Assert.IsTrue(result);
+        Assert.AreEqual("application/json; charset=UTF-8", stubResponse.ContentType);
+    }
+}
--- src/HttPlaceholder.Application/StubExecution/ResponseToStubResponseHandlers/ContentTypeHandler.cs
+++ src/HttPlaceholder.Application/StubExecution/ResponseToStubResponseHandlers/ContentTypeHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using HttPlaceholder.Application.StubExecution.Models;
+using HttPlaceholder.Common.Utilities;
 using HttPlaceholder.Domain;
 
 namespace HttPlaceholder.Application.StubExecution.ResponseToStubResponseHandlers;
@@ -10,7 +11,17 @@ namespace HttPlaceholder.Application.StubExecution.ResponseToStubResponseHandler
 public class ContentTypeHandler : IResponseToStubResponseHandler
 {
     /// <inheritdoc />
-    public Task<bool> HandleStubGenerationAsync(HttpResponseModel response, StubResponseModel stubResponseModel) => throw new System.NotImplementedException();
+    public Task<bool> HandleStubGenerationAsync(HttpResponseModel response, StubResponseModel stubResponseModel)
+    {
+        var headerValue = response.Headers.CaseInsensitiveSearch("content-type");
+        if (string.IsNullOrWhiteSpace(headerValue))
+        {
+            return Task.FromResult(false);
+        }
+
+        stubResponseModel.ContentType = headerValue;
+        return Task.FromResult(true);
+    }
 
     /// <inheritdoc />
     public int Priority => -1;
