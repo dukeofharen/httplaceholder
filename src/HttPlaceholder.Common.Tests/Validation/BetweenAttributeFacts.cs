23eb46cd1aa4f83aa4b0409d3c582a9b9aedd84c
Duco <git@ducode.org>
2022-01-30 21:59:20 +0100

    Added unit tests for BetweenAttribute

---
+++ src/HttPlaceholder.Common.Tests/Validation/BetweenAttributeFacts.cs
@@ -0,0 +1,29 @@
+﻿using HttPlaceholder.Common.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttPlaceholder.Common.Tests.Validation;
+
+[TestClass]
+public class BetweenAttributeFacts
+{
+    [DataTestMethod]
+    [DataRow(4, 3, 5, false, false, true)]
+    [DataRow(3, 3, 5, false, false, true)]
+    [DataRow(5, 3, 5, false, false, false)]
+    [DataRow(5, 3, 5, true, false, true)]
+    [DataRow(0, 3, 5, false, true, true)]
+    [DataRow(null, 3, 5, false, true, true)]
+    [DataRow(0, 3, 5, false, false, false)]
+    [DataRow(null, 3, 5, false, false, false)]
+    public void IsValid(int? input, int min, int max, bool including, bool allowDefault, bool isValid)
+    {
+        // Arrange
+        var attribute = new BetweenAttribute(min, max, including, allowDefault);
+
+        // Act
+        var result = attribute.IsValid(input);
+
+        // Assert
+        Assert.AreEqual(isValid, result);
+    }
+}
