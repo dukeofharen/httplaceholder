3cfa7b838c0442b9905a809566b7ce83ebd842d7
Duco <git@ducode.org>
2022-02-05 22:00:04 +0100

    Implemented CleanOldRequestsJob

--- src/HttPlaceholder/HostedServices/CleanOldRequestsJob.cs
+++ src/HttPlaceholder/HostedServices/CleanOldRequestsJob.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using HttPlaceholder.Application.StubExecution;
 using HttPlaceholder.Common;
 using Microsoft.Extensions.Logging;
 
@@ -9,12 +10,16 @@ namespace HttPlaceholder.HostedServices;
 /// </summary>
 public class CleanOldRequestsJob : BackgroundService
 {
+    private readonly IStubContext _stubContext;
+
     /// <summary>
     /// Constructs a <see cref="CleanOldRequestsJob"/> instance.
     /// </summary>
-    public CleanOldRequestsJob(ILogger<BackgroundService> logger, IDateTime dateTime, IAsyncService asyncService) :
+    public CleanOldRequestsJob(ILogger<BackgroundService> logger, IDateTime dateTime, IAsyncService asyncService,
+        IStubContext stubContext) :
         base(logger, dateTime, asyncService)
     {
+        _stubContext = stubContext;
     }
 
     /// <inheritdoc />
@@ -27,5 +32,9 @@ public class CleanOldRequestsJob : BackgroundService
     public override string Description => "A job for cleaning old requests.";
 
     /// <inheritdoc />
-    public override Task ProcessAsync() => throw new System.NotImplementedException();
+    public override async Task ProcessAsync()
+    {
+        Logger.LogDebug("Cleaning old requests.");
+        await _stubContext.CleanOldRequestResultsAsync();
+    }
 }
