ee6ae6706ae30972d49e05752fca19e36be95e0f
Duco <git@ducode.org>
2022-02-12 16:29:53 +0100

    Installed Pinia

--- gui/src/main.js
+++ gui/src/main.js
@@ -3,9 +3,10 @@ import App from "@/App.vue";
 import "@/registerServiceWorker";
 import router from "@/router";
 import store from "@/store";
+import { createPinia } from "pinia";
 import "@/plugins";
 import registerGlobalComponents from "@/plugins/global-components";
 
-const vueApp = createApp(App).use(store).use(router);
+const vueApp = createApp(App).use(store).use(router).use(createPinia());
 registerGlobalComponents(vueApp);
 vueApp.mount("#app");
