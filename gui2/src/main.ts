7951b5ab1728da2cffdf2886e2229d47b20e07f1
Duco <git@ducode.org>
2022-02-18 21:03:48 +0100

    Added manifest stuff

--- gui2/src/main.ts
+++ gui2/src/main.ts
@@ -4,6 +4,7 @@ import "@/plugins";
 import registerGlobalComponents from "@/plugins/global-components";
 import App from "./App.vue";
 import router from "./router";
+import "@/registerServiceWorker";
 
 const app = createApp(App);
 
