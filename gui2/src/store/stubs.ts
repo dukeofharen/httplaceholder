b3d8c5dafe94325242440dd4acc1ab1daeace007
Duco <git@ducode.org>
2022-02-19 15:25:41 +0100

    Converted stubs store to typescript

--- gui2/src/store/stubs.ts
+++ gui2/src/store/stubs.ts
@@ -1,46 +1,58 @@
 import { defineStore } from "pinia";
 import { del, get, post, put } from "@/utils/api";
 import yaml from "js-yaml";
+import type { FullStubModel } from "@/domain/stub/full-stub-model";
+import type { FullStubOverviewModel } from "@/domain/stub/full-stub-overview-model";
+
+export interface UpdateStubInputModel {
+  input: string;
+  stubId: string;
+}
+
+export interface CreateStubBasedOnRequestInputModel {
+  correlationId: string;
+  doNotCreateStub?: boolean;
+}
 
 export const useStubsStore = defineStore({
   id: "stubs",
   state: () => ({}),
   getters: {},
   actions: {
-    getStub(stubId) {
+    getStub(stubId: string): Promise<FullStubModel> {
       return get(`/ph-api/stubs/${stubId}`)
         .then((response) => Promise.resolve(response))
         .catch((error) => Promise.reject(error));
     },
-    getStubsOverview() {
+    getStubsOverview(): Promise<FullStubOverviewModel[]> {
       return get("/ph-api/stubs/overview")
         .then((response) => Promise.resolve(response))
         .catch((error) => Promise.reject(error));
     },
-    getStubs() {
+    getStubs(): Promise<FullStubModel[]> {
       return get("/ph-api/stubs")
         .then((response) => Promise.resolve(response))
         .catch((error) => Promise.reject(error));
     },
-    async flipEnabled(stubId) {
+    async flipEnabled(stubId: string): Promise<boolean> {
       const stub = (await this.getStub(stubId)).stub;
       stub.enabled = !stub.enabled;
       await put(`/ph-api/stubs/${stubId}`, stub);
       return stub.enabled;
     },
-    async enableStub(stubId) {
+    async enableStub(stubId: string): Promise<boolean> {
       const stub = (await this.getStub(stubId)).stub;
       stub.enabled = true;
       await put(`/ph-api/stubs/${stubId}`, stub);
       return stub.enabled;
     },
-    async disableStub(stubId) {
+    async disableStub(stubId: string): Promise<boolean> {
       const stub = (await this.getStub(stubId)).stub;
       stub.enabled = false;
       await put(`/ph-api/stubs/${stubId}`, stub);
       return stub.enabled;
     },
-    deleteStub(stubId) {
+    deleteStub(stubId: string) {
       return del(`/ph-api/stubs/${stubId}`)
         .then((response) => Promise.resolve(response))
         .catch((error) => Promise.reject(error));
@@ -50,7 +62,7 @@ export const useStubsStore = defineStore({
         .then((response) => Promise.resolve(response))
         .catch((error) => Promise.reject(error));
     },
-    addStubs(input) {
+    addStubs(input: string): Promise<FullStubModel[]> {
       const parsedObject = yaml.load(input);
       const stubsArray = Array.isArray(parsedObject)
         ? parsedObject
@@ -59,13 +71,13 @@ export const useStubsStore = defineStore({
         .then((response) => Promise.resolve(response))
         .catch((error) => Promise.reject(error));
     },
-    updateStub(payload) {
+    updateStub(payload: UpdateStubInputModel) {
       const parsedObject = yaml.load(payload.input);
       return put(`/ph-api/stubs/${payload.stubId}`, parsedObject)
         .then((response) => Promise.resolve(response))
         .catch((error) => Promise.reject(error));
     },
-    createStubBasedOnRequest(payload) {
+    createStubBasedOnRequest(payload: CreateStubBasedOnRequestInputModel) {
       return post(`/ph-api/requests/${payload.correlationId}/stubs`, {
         doNotCreateStub:
           payload.doNotCreateStub !== null ? payload.doNotCreateStub : false,
