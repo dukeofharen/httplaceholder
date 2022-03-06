5b037746609e77f3fe62ebba0461cfc36feed750
Duco <git@ducode.org>
2022-03-06 13:36:29 +0100

    Eslint fix

--- gui/.eslintrc.cjs
+++ gui/.eslintrc.cjs
@@ -12,12 +12,7 @@ module.exports = {
   env: {
     "vue/setup-compiler-macros": true,
   },
-  overrides: [
-    {
-      files: ["cypress/integration/**.spec.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
-  ],
+  overrides: [],
   rules: {
     "vue/multi-word-component-names": 0,
     "@typescript-eslint/no-explicit-any": 0,
