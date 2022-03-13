66efdf9a339c5e3ce2e48408c202799ac92dc317
Duco <git@ducode.org>
2022-03-13 17:19:01 +0100

    Styling fixes in rendered docs

--- gui/scripts/docs-parsing/parse-docs.js
+++ gui/scripts/docs-parsing/parse-docs.js
@@ -7,13 +7,14 @@ const {
   copySync,
   readFileSync,
   writeFileSync,
-} = require("./helper/file");
+} = require("../helper/file");
 const { parse } = require("marked");
 const { render } = require("mustache");
 
-const publicRootDir = join(__dirname, "../public");
+const publicRootDir = join(__dirname, "../../public");
 const publicDocsRootDir = join(publicRootDir, "docs");
-const docsRootDir = join(__dirname, "../../docs");
+const docsRootDir = join(__dirname, "../../../docs");
+const staticDir = join(__dirname, "static");
 
 if (exists(publicDocsRootDir)) {
   console.log(`Removing directory ${publicDocsRootDir}.`);
@@ -25,6 +26,9 @@ ensureDirSync(publicDocsRootDir);
 
 console.log(`Copying contents from ${docsRootDir} to ${publicDocsRootDir}`);
 copySync(docsRootDir, publicDocsRootDir);
+
+console.log(`Copying contents from ${staticDir} to ${publicDocsRootDir}.`);
+copySync(staticDir, publicDocsRootDir);
 
 const docsMdPath = join(publicDocsRootDir, "docs.md");
 const docsHtmlPath = join(publicDocsRootDir, "index.html");
