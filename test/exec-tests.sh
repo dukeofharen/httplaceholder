1eac2d56e1a101c736b4948fad749fe3d3a061c1
Duco <git@ducode.org>
2022-04-03 16:55:49 +0200

    Fixed tests

--- test/exec-tests.sh
+++ test/exec-tests.sh
@@ -22,7 +22,7 @@ sudo docker-compose -f "$DEVENV_SCRIPT_PATH" up -d
 
 # Run HttPlaceholder tests for in memory configuration.
 echo "Testing HttPlaceholder with in memory configuration"
-dotnet run --project $HTTPL_ROOT_DIR --useInMemoryStorage > $DIR/logs/httplaceholder-in-memory.txt 2>&1 &
+dotnet run --project $HTTPL_ROOT_DIR --useInMemoryStorage --storeResponses > $DIR/logs/httplaceholder-in-memory.txt 2>&1 &
 sleep 5
 newman run $POSTMAN_PATH --insecure > $DIR/logs/test-in-memory.txt
 assert-test-ok
@@ -36,7 +36,7 @@ if [ ! -d "$FILE_STORAGE_PATH" ]; then
   mkdir $FILE_STORAGE_PATH
 fi
 
-dotnet run --project $HTTPL_ROOT_DIR --fileStorageLocation $FILE_STORAGE_PATH > $DIR/logs/httplaceholder-file-storage.txt 2>&1 &
+dotnet run --project $HTTPL_ROOT_DIR --fileStorageLocation $FILE_STORAGE_PATH  --storeResponses > $DIR/logs/httplaceholder-file-storage.txt 2>&1 &
 sleep 5
 newman run $POSTMAN_PATH --insecure > $DIR/logs/test-file-storage.txt
 assert-test-ok
@@ -49,7 +49,7 @@ if [ -f "$SQLITE_PATH" ]; then
   sudo rm $SQLITE_PATH
 fi
 
-dotnet run --project $HTTPL_ROOT_DIR --sqliteConnectionString "Data Source=$SQLITE_PATH" > $DIR/logs/httplaceholder-sqlite.txt 2>&1 &
+dotnet run --project $HTTPL_ROOT_DIR --sqliteConnectionString "Data Source=$SQLITE_PATH;Foreign Keys=True" --storeResponses > $DIR/logs/httplaceholder-sqlite.txt 2>&1 &
 sleep 5
 newman run $POSTMAN_PATH --insecure > $DIR/logs/test-sqlite.txt
 assert-test-ok
@@ -57,7 +57,7 @@ sudo killall HttPlaceholder
 
 # Run HttPlaceholder tests for in MySQL configuration.
 echo "Testing HttPlaceholder with in MySQL configuration"
-dotnet run --project $HTTPL_ROOT_DIR --mysqlConnectionString "Server=localhost;Database=httplaceholder;Uid=root;Pwd=root;Allow User Variables=true" > $DIR/logs/httplaceholder-mysql.txt 2>&1 &
+dotnet run --project $HTTPL_ROOT_DIR --mysqlConnectionString "Server=localhost;Database=httplaceholder;Uid=root;Pwd=root;Allow User Variables=true" --storeResponses > $DIR/logs/httplaceholder-mysql.txt 2>&1 &
 sleep 5
 newman run $POSTMAN_PATH --insecure > $DIR/logs/test-mysql.txt
 assert-test-ok
@@ -65,7 +65,7 @@ sudo killall HttPlaceholder
 
 # Run HttPlaceholder tests for in MSSQL configuration.
 echo "Testing HttPlaceholder with in MSSQL configuration"
-dotnet run --project $HTTPL_ROOT_DIR --sqlServerConnectionString "Server=localhost,1433;Database=httplaceholder;User Id=sa;Password=Password123!" > $DIR/logs/httplaceholder-mssql.txt 2>&1 &
+dotnet run --project $HTTPL_ROOT_DIR --sqlServerConnectionString "Server=localhost,1433;Database=httplaceholder;User Id=sa;Password=Password123!" --storeResponses > $DIR/logs/httplaceholder-mssql.txt 2>&1 &
 sleep 5
 newman run $POSTMAN_PATH --insecure > $DIR/logs/test-mssql.txt
 assert-test-ok
