c7aa9be8d0769e9a2280d5b2e33585ae4bab4f6f
Duco <git@ducode.org>
2022-04-24 21:39:55 +0200

    Added unit test for ConfigurationHelper

---
+++ src/HttPlaceholder.Infrastructure.Tests/Configuration/ConfigurationHelperFacts.cs
@@ -0,0 +1,18 @@
+﻿using HttPlaceholder.Infrastructure.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttPlaceholder.Infrastructure.Tests.Configuration;
+
+[TestClass]
+public class ConfigurationHelperFacts
+{
+    [TestMethod]
+    public void GetConfigKeyMetadata_HappyFlow()
+    {
+        // Act
+        var result = ConfigurationHelper.GetConfigKeyMetadata();
+
+        // Assert
+        Assert.AreEqual(23, result.Count);
+    }
+}
