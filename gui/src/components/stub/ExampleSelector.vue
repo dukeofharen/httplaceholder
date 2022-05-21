e10611a5128b29194bd3413d383ce3bc81fd1e0e
Duco <git@ducode.org>
2022-05-21 12:44:56 +0200

    Added empty ExampleSelector

---
+++ gui/src/components/stub/ExampleSelector.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>moi</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ExampleSelector",
+});
+</script>
+
+<style scoped></style>
