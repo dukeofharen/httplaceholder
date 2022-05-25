a762f8a1098a24037e7b4b41b0c59ddf26da90f8
Duco <git@ducode.org>
2022-05-25 21:55:05 +0200

    Added new variable parsing handler

---
+++ src/HttPlaceholder.Application/StubExecution/ResponseVariableParsingHandlers/RootUrlResponseVariableParsingHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HttPlaceholder.Application.Interfaces.Http;
+
+namespace HttPlaceholder.Application.StubExecution.ResponseVariableParsingHandlers;
+
+/// <summary>
+/// Response variable parsing handler that is used to insert the root URL (so the URL without path + query string) in the response.
+/// </summary>
+internal class RootUrlResponseVariableParsingHandler : IResponseVariableParsingHandler
+{
+    private readonly IHttpContextService _httpContextService;
+
+    public RootUrlResponseVariableParsingHandler(IHttpContextService httpContextService)
+    {
+        _httpContextService = httpContextService;
+    }
+
+    public string Name => "root_url";
+
+    public string FullName => "Root URL";
+
+    public string Example => "((root_url))";
+
+    public string Parse(string input, IEnumerable<Match> matches)
+    {
+        var enumerable = matches as Match[] ?? matches.ToArray();
+        if (!enumerable.Any())
+        {
+            return input;
+        }
+
+        var url = _httpContextService.RootUrl;
+        return enumerable
+            .Where(match => match.Groups.Count >= 2)
+            .Aggregate(input, (current, match) => current.Replace(match.Value, url));
+    }
+}
