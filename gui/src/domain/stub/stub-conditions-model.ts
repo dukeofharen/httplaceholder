7744acb3205b870c384a3a20074cbacc072f61e3
Duco <git@ducode.org>
2022-06-06 14:49:20 +0200

    Small fix

--- gui/src/domain/stub/stub-conditions-model.ts
+++ gui/src/domain/stub/stub-conditions-model.ts
@@ -1,6 +1,5 @@
 import type { StubUrlConditionModel } from "@/domain/stub/stub-url-condition-model";
 import type { StubFormModel } from "@/domain/stub/stub-form-model";
-import type { HashMap } from "@/domain/hash-map";
 import type { StubXpathModel } from "@/domain/stub/stub-xpath-model";
 import type { StubBasicAuthenticationModel } from "@/domain/stub/stub-basic-authentication-model";
 import type { StubConditionsScenarioModel } from "@/domain/stub/stub-conditions-scenario-model";
