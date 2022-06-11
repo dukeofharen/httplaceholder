830d38bcb126a4dda5dfba5a5a38271a1cd014a3
Duco <git@ducode.org>
2022-06-11 14:19:43 +0200

    Updated set-version script

--- scripts/build/set-version.sh
+++ scripts/build/set-version.sh
@@ -14,6 +14,10 @@ CHANGELOG_PATH="$ROOT_FOLDER/CHANGELOG"
 
 echo "Determining version"
 BASE_VERSION=$(head -n 1 $CHANGELOG_PATH | sed 's/\[//' | sed 's/\]//')
+if [ "$BASE_VERSION" = "vnext" ]; then
+  BASE_VERSION=$(date +"%Y.%-m.%-d")
+fi
+
 VERSION="$BASE_VERSION.$1"
 echo "Version is $VERSION"
 echo "$VERSION" > "$ROOT_FOLDER/version.txt"
