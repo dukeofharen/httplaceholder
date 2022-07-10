7dfa42e4b91f740aae31afd42fd94fa051a270c0
Duco <git@ducode.org>
2022-07-10 12:02:39 +0200

    Implemented ScenarioStateVariableParsingHandler

---
+++ src/HttPlaceholder.Application.Tests/StubExecution/ResponseVariableParsingHandlers/ScenarioStateVariableParsingHandlerFacts.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using HttPlaceholder.Application.StubExecution;
+using HttPlaceholder.Application.StubExecution.Implementations;
+using HttPlaceholder.Application.StubExecution.ResponseVariableParsingHandlers;
+using HttPlaceholder.Domain;
+using HttPlaceholder.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq.AutoMock;
+
+namespace HttPlaceholder.Application.Tests.StubExecution.ResponseVariableParsingHandlers;
+
+[TestClass]
+public class ScenarioStateVariableParsingHandlerFacts
+{
+    private readonly AutoMocker _mocker = new();
+
+    [TestCleanup]
+    public void Cleanup() => _mocker.VerifyAll();
+
+    [TestMethod]
+    public void Parse_NoMatches_ShouldReturnInputAsIs()
+    {
+        // Arrange
+        var handler = _mocker.CreateInstance<ScenarioStateVariableParsingHandler>();
+        const string input = "the input";
+
+        // Act
+        var result = handler.Parse(input, Array.Empty<Match>(), new StubModel());
+
+        // Assert
+        Assert.AreEqual(input, result);
+    }
+
+    [TestMethod]
+    public void Parse_Matches_ShouldParseState()
+    {
+        // Arrange
+        var handler = _mocker.CreateInstance<ScenarioStateVariableParsingHandler>();
+        var mockScenarioStateStore = _mocker.GetMock<IScenarioStateStore>();
+        const string input =
+            "((scenario_state)) ((scenario_state:scenario_exists)) ((scenario_state:scenario_doesnt_exist))";
+        const string expectedResult = "state_from_stub state_from_other_scenario ";
+
+        var stubModel = new StubModel {Scenario = "stub-scenario"};
+
+        mockScenarioStateStore
+            .Setup(m => m.GetScenario(stubModel.Scenario))
+            .Returns(new ScenarioStateModel {State = "state_from_stub"});
+        mockScenarioStateStore
+            .Setup(m => m.GetScenario("scenario_exists"))
+            .Returns(new ScenarioStateModel {State = "state_from_other_scenario"});
+        mockScenarioStateStore
+            .Setup(m => m.GetScenario("scenario_doesnt_exist"))
+            .Returns((ScenarioStateModel)null);
+
+        // Act
+        var matches = ResponseVariableParser.VarRegex.Matches(input);
+        var result = handler.Parse(input, matches, stubModel);
+
+        // Assert
+        Assert.AreEqual(expectedResult, result);
+    }
+}
---
+++ src/HttPlaceholder.Application/StubExecution/ResponseVariableParsingHandlers/ScenarioStateVariableParsingHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HttPlaceholder.Domain;
+
+namespace HttPlaceholder.Application.StubExecution.ResponseVariableParsingHandlers;
+
+/// <summary>
+/// Response variable handler that is used to insert the state of a specific scenario in the response
+/// </summary>
+internal class ScenarioStateVariableParsingHandler : IResponseVariableParsingHandler
+{
+    private readonly IScenarioStateStore _scenarioStateStore;
+
+    public ScenarioStateVariableParsingHandler(IScenarioStateStore scenarioStateStore)
+    {
+        _scenarioStateStore = scenarioStateStore;
+    }
+
+    /// <inheritdoc />
+    public string Name => "scenario_state";
+
+    /// <inheritdoc />
+    public string FullName => "Scenario state";
+
+    /// <inheritdoc />
+    public string Example => "((scenario_state:scenario name))";
+
+    /// <inheritdoc />
+    public string Parse(string input, IEnumerable<Match> matches, StubModel stub)
+    {
+        var enumerable = matches as Match[] ?? matches.ToArray();
+        if (!enumerable.Any())
+        {
+            return input;
+        }
+
+        foreach (var match in enumerable)
+        {
+            string state;
+            var scenarioName = match.Groups.Count == 3 && !string.IsNullOrWhiteSpace(match.Groups[2].Value)
+                ? match.Groups[2].Value
+                : stub.Scenario ?? string.Empty;
+            if (string.IsNullOrWhiteSpace(scenarioName))
+            {
+                state = string.Empty;
+            }
+            else
+            {
+                var scenario = _scenarioStateStore.GetScenario(scenarioName);
+                state = scenario?.State;
+            }
+
+            input = input.Replace(match.Value, state);
+        }
+
+        return input;
+    }
+}
