0138d137d9c53bb37ea14b371736f746872c6ddc
Duco <git@ducode.org>
2022-07-11 22:13:02 +0200

    Implemented ScenarioHitCountVariableParsingHandler

---
+++ src/HttPlaceholder.Application.Tests/StubExecution/ResponseVariableParsingHandlers/ScenarioHitCountVariableParsingHandlerFacts.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using HttPlaceholder.Application.StubExecution;
+using HttPlaceholder.Application.StubExecution.Implementations;
+using HttPlaceholder.Application.StubExecution.ResponseVariableParsingHandlers;
+using HttPlaceholder.Domain;
+using HttPlaceholder.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq.AutoMock;
+
+namespace HttPlaceholder.Application.Tests.StubExecution.ResponseVariableParsingHandlers;
+
+[TestClass]
+public class ScenarioHitCountVariableParsingHandlerFacts
+{
+    private readonly AutoMocker _mocker = new();
+
+    [TestCleanup]
+    public void Cleanup() => _mocker.VerifyAll();
+
+    [TestMethod]
+    public void Parse_NoMatches_ShouldReturnInputAsIs()
+    {
+        // Arrange
+        var handler = _mocker.CreateInstance<ScenarioHitCountVariableParsingHandler>();
+        const string input = "the input";
+
+        // Act
+        var result = handler.Parse(input, Array.Empty<Match>(), new StubModel());
+
+        // Assert
+        Assert.AreEqual(input, result);
+    }
+
+    [TestMethod]
+    public void Parse_Matches_ShouldParseHitCount()
+    {
+        // Arrange
+        var handler = _mocker.CreateInstance<ScenarioHitCountVariableParsingHandler>();
+        var mockScenarioStateStore = _mocker.GetMock<IScenarioStateStore>();
+        const string input =
+            "((scenario_hitcount)) ((scenario_hitcount:scenario_exists)) ((scenario_hitcount:scenario_doesnt_exist))";
+        const string expectedResult = "3 1337 ";
+
+        var stubModel = new StubModel {Scenario = "stub-scenario"};
+
+        mockScenarioStateStore
+            .Setup(m => m.GetScenario(stubModel.Scenario))
+            .Returns(new ScenarioStateModel {HitCount = 3});
+        mockScenarioStateStore
+            .Setup(m => m.GetScenario("scenario_exists"))
+            .Returns(new ScenarioStateModel {HitCount = 1337});
+        mockScenarioStateStore
+            .Setup(m => m.GetScenario("scenario_doesnt_exist"))
+            .Returns((ScenarioStateModel)null);
+
+        // Act
+        var matches = ResponseVariableParser.VarRegex.Matches(input);
+        var result = handler.Parse(input, matches, stubModel);
+
+        // Assert
+        Assert.AreEqual(expectedResult, result);
+    }
+}
---
+++ src/HttPlaceholder.Application/StubExecution/ResponseVariableParsingHandlers/ScenarioHitCountVariableParsingHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HttPlaceholder.Domain;
+
+namespace HttPlaceholder.Application.StubExecution.ResponseVariableParsingHandlers;
+
+/// <summary>
+/// Response variable handler that is used to insert the hit count of a specific scenario in the response
+/// </summary>
+internal class ScenarioHitCountVariableParsingHandler : IResponseVariableParsingHandler
+{
+    private readonly IScenarioStateStore _scenarioStateStore;
+
+    public ScenarioHitCountVariableParsingHandler(IScenarioStateStore scenarioStateStore)
+    {
+        _scenarioStateStore = scenarioStateStore;
+    }
+
+    /// <inheritdoc />
+    public string Name => "scenario_hitcount";
+
+    /// <inheritdoc />
+    public string FullName => "Scenario hit count";
+
+    /// <inheritdoc />
+    public string Example => "((scenario_hitcount:scenario name))";
+
+    /// <inheritdoc />
+    public string Parse(string input, IEnumerable<Match> matches, StubModel stub)
+    {
+        var enumerable = matches as Match[] ?? matches.ToArray();
+        if (!enumerable.Any())
+        {
+            return input;
+        }
+
+        foreach (var match in enumerable)
+        {
+            int? hitCount = null;
+            var scenarioName = match.Groups.Count == 3 && !string.IsNullOrWhiteSpace(match.Groups[2].Value)
+                ? match.Groups[2].Value
+                : stub.Scenario ?? string.Empty;
+            if (!string.IsNullOrWhiteSpace(scenarioName))
+            {
+                var scenario = _scenarioStateStore.GetScenario(scenarioName);
+                hitCount = scenario?.HitCount;
+            }
+
+            var hitCountText = !hitCount.HasValue ? string.Empty : hitCount.Value.ToString();
+            input = input.Replace(match.Value, hitCountText);
+        }
+
+        return input;
+    }
+}
