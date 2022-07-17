5de31c114ed2d45a5e1dd1c7e5c16a2feeae1086
Duco <git@ducode.org>
2022-07-17 08:36:15 +0200

    Added script to set build status

---
+++ scripts/build/set-build-status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+STATE="${1-failure}"
+DESCRIPTION=""
+if [ "$STATE" = "failure" ] || [ "$STATE" = "error" ]; then
+  DESCRIPTION="The build failed."
+fi
+if [ "$STATE" = "pending" ]; then
+  DESCRIPTION="The build is pending."
+fi
+if [ "$STATE" = "success" ]; then
+  DESCRIPTION="The build succeeded."
+fi
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: token $GITHUB_API_KEY" \
+  https://api.github.com/repos/dukeofharen/httplaceholder/statuses/$CI_COMMIT_SHA \
+  -d "{\"state\":\"$STATE\",\"target_url\":\"https://gitlab.com/ducode/httplaceholder/-/pipelines/$CI_CONCURRENT_ID\",\"description\":\"$DESCRIPTION\",\"context\":\"continuous-integration/gitlab-ci\"}"
