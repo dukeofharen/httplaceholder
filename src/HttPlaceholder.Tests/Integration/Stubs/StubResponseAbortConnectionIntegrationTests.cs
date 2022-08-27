efeaad5d2b63501a62306a105d54e9736d8967b7
Duco <git@ducode.org>
2022-08-27 20:01:59 +0200

    Added integration test for aborted connection

---
+++ src/HttPlaceholder.Tests/Integration/Stubs/StubResponseAbortConnectionIntegrationTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttPlaceholder.Tests.Integration.Stubs;
+
+[TestClass]
+public class StubResponseAbortConnectionIntegrationTests : StubIntegrationTestBase
+{
+    [TestInitialize]
+    public void Initialize() => InitializeStubIntegrationTest("Resources/integration.yml");
+
+    [TestCleanup]
+    public void Cleanup() => CleanupIntegrationTest();
+
+    [TestMethod]
+    public async Task StubIntegration_AbortConnection()
+    {
+        // Arrange
+        var url = $"{TestServer.BaseAddress}response-abort-connection";
+
+        // Act
+        using var response = await Client.GetAsync(url);
+
+        // Assert
+        // Not really possible to test an aborted connection in an integration test, so let's test it like this.
+        Assert.IsTrue(Requests.Any(r => r.ExecutingStubId == "response-abort-connection"));
+    }
+}
