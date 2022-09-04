d2915468724bbc972ddad808a6afb3802929f713
Duco <git@ducode.org>
2022-09-04 19:59:44 +0200

    Added config table to settings page

--- gui/src/views/Settings.vue
+++ gui/src/views/Settings.vue
@@ -31,27 +31,60 @@
         </div>
       </div>
     </div>
+
+    <div class="row mt-3">
+      <div class="col-md-12">
+        <h2>HttPlaceholder configuration</h2>
+
+        <p>
+          HttPlaceholder was started with the following settings. The settings
+          are read-only and can only be set when starting the application.
+        </p>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Setting</th>
+              <th scope="col">Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item of config" :key="item.key">
+              <td :title="item.description">{{ item.key }}</td>
+              <td>{{ item.value }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import { useSettingsStore } from "@/store/settings";
-import { defineComponent } from "vue";
 import type { SettingsModel } from "@/domain/settings-model";
+import { useConfigurationStore } from "@/store/configuration";
+import type { ConfigurationModel } from "@/domain/stub/configuration-model";
 
 export default defineComponent({
   name: "Settings",
   setup() {
     const generalStore = useSettingsStore();
+    const configurationStore = useConfigurationStore();
 
     // Data
     const settings = ref<SettingsModel>(generalStore.getSettings);
+    const config = ref<ConfigurationModel[]>([]);
 
     // Methods
     const saveSettings = () => generalStore.storeSettings(settings.value);
 
-    return { settings, saveSettings };
+    // Lifecycle
+    onMounted(async () => {
+      config.value = await configurationStore.getConfiguration();
+    });
+
+    return { settings, saveSettings, config };
   },
 });
 </script>
