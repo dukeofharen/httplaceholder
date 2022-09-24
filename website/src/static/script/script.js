1ac765ce3c8fec9e9d0124cc978d026fd91e2dff
Duco <git@ducode.org>
2022-09-24 17:38:40 +0200

    Added button for switching theme

--- website/src/static/script/script.js
+++ website/src/static/script/script.js
@@ -4,8 +4,8 @@ const themeType = {
 };
 
 function switchTheme(theme) {
-    const html = document.getElementsByTagName("html");
-    const currentTheme = html[0].getAttribute("data-theme");
+    const html = document.getElementsByTagName("html")[0];
+    const currentTheme = html.getAttribute("data-theme");
     let newTheme;
     if (theme) {
         newTheme = theme === themeType.dark ? themeType.dark : themeType.light;
@@ -13,10 +13,24 @@ function switchTheme(theme) {
         newTheme = currentTheme === themeType.dark ? themeType.light : themeType.dark;
     }
 
-    html[0].setAttribute("data-theme", newTheme);
+    html.setAttribute("data-theme", newTheme);
+
+    const themeSwitcherIcons = document.getElementsByClassName("theme-switch");
+    for (let i = 0, max = themeSwitcherIcons.length; i < max; i++) {
+        themeSwitcherIcons[i].style.display = "none";
+    }
+    
+    const themeIconCssSelector = newTheme === themeType.dark ? "sun" : "moon";
+    const themeIconElement = document.getElementsByClassName(themeIconCssSelector)[0];
+    themeIconElement.style.display = "inline";
 }
 
 window.addEventListener('DOMContentLoaded', function () {
     const theme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? themeType.dark : themeType.light;
     switchTheme(theme);
+    
+    const themeSwitcherButton = document.getElementById("theme-switcher");
+    themeSwitcherButton.addEventListener("click", function() {
+        switchTheme();
+    })
 });
