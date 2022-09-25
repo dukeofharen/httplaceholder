441f9dc10ce14ade9b999be6c6895b74197be6b1
Duco <git@ducode.org>
2022-09-25 17:07:35 +0200

    Implemented script for copying code to clipboard

--- website/src/static/script/script.js
+++ website/src/static/script/script.js
@@ -3,6 +3,38 @@ const themeType = {
     light: "light"
 };
 
+function fallbackCopyTextToClipboard(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        return new Promise((resolve, reject) => {
+            try {
+                fallbackCopyTextToClipboard(text);
+                resolve();
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    return navigator.clipboard.writeText(text);
+}
+
 function switchTheme(theme) {
     const html = document.getElementsByTagName("html")[0];
     const currentTheme = html.getAttribute("data-theme");
@@ -19,22 +51,34 @@ function switchTheme(theme) {
     for (let i = 0, max = themeSwitcherIcons.length; i < max; i++) {
         themeSwitcherIcons[i].style.display = "none";
     }
-    
+
     const themeIconCssSelector = newTheme === themeType.dark ? "sun" : "moon";
     const themeIconElement = document.getElementsByClassName(themeIconCssSelector)[0];
     themeIconElement.style.display = "inline";
 }
 
+function onCopyLinkClick(e) {
+    const target = e.target;
+    const parent = target.parentNode;
+    const code = parent.getElementsByTagName("code")[0];
+    copyTextToClipboard(code.innerText).then(() => console.log("Copied to clipboard."));
+}
+
 function setupCopyableScripts() {
-    
+    const copyLinks = document.getElementsByClassName("copy-icon");
+    for (let i = 0, max = copyLinks.length; i < max; i++) {
+        copyLinks[i].addEventListener("click", onCopyLinkClick);
+    }
 }
 
 window.addEventListener('DOMContentLoaded', function () {
     const theme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? themeType.dark : themeType.light;
     switchTheme(theme);
-    
+
     const themeSwitcherButton = document.getElementById("theme-switcher");
-    themeSwitcherButton.addEventListener("click", function() {
+    themeSwitcherButton.addEventListener("click", function () {
         switchTheme();
     });
+
+    setupCopyableScripts();
 });
