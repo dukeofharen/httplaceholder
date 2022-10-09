dc571917c578c61fc05141e0071db3dd2a7b33c9
Duco <git@ducode.org>
2022-10-09 12:04:31 +0200

    Remove volumes before running integration tests

--- test/exec-tests.sh
+++ test/exec-tests.sh
@@ -18,7 +18,8 @@ function assert-test-ok() {
 
 # Starting up database containers.
 DEVENV_SCRIPT_PATH="$DIR/../scripts/dev/docker-compose.yml"
-sudo docker-compose -f "$DEVENV_SCRIPT_PATH" up -d
+docker-compose -f "$DEVENV_SCRIPT_PATH" down -v
+docker-compose -f "$DEVENV_SCRIPT_PATH" up -d
 
 # Run HttPlaceholder tests for in memory configuration.
 echo "Testing HttPlaceholder with in memory configuration"
@@ -72,4 +73,4 @@ assert-test-ok
 sudo killall HttPlaceholder
 
 # Stop the Docker containers.
-sudo docker-compose -f "$DEVENV_SCRIPT_PATH" down
+docker-compose -f "$DEVENV_SCRIPT_PATH" down
