2e3654d75aef6e529b48d84a9f3e76b3006e8ea7
Duco <git@ducode.org>
2022-10-09 15:06:43 +0200

    Added method for cleaning path

---
+++ src/HttPlaceholder.Common.Tests/Utilities/PathUtilitiesFacts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using HttPlaceholder.Common.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttPlaceholder.Common.Tests.Utilities;
+
+[TestClass]
+public class PathUtilitiesFacts
+{
+    [DataTestMethod]
+    [DataRow("Folder/File.png", "Folder/File.png")]
+    [DataRow("Folder\\File.png", "Folder/File.png")]
+    [DataRow("", "")]
+    [DataRow(null, null)]
+    [DataRow("Folder/../File.png", "Folder/File.png")]
+    [DataRow("Folder\\..\\File.png", "Folder/File.png")]
+    [DataRow("Folder/$/File.png", "Folder/File.png")]
+    [DataRow("Folder\\$\\File.png", "Folder/File.png")]
+    [DataRow("Folder/?/File.png", "Folder/File.png")]
+    [DataRow("Folder\\?\\File.png", "Folder/File.png")]
+    public void CleanPath_HappyFlow(string input, string expectedOutput)
+    {
+        // Act
+        var result = PathUtilities.CleanPath(input);
+
+        // Assert
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Assert.AreEqual(expectedOutput, input);
+        }
+        else
+        {
+            var resultParts = SplitPath(result);
+            var expectedOutputParts = SplitPath(expectedOutput);
+            Assert.IsTrue(resultParts.SequenceEqual(expectedOutputParts), $"Expected output is {expectedOutput} but actual output is {result}.");
+        }
+    }
+
+    private static string[] SplitPath(string path) => path.Split(new[] {"/", "\\"}, StringSplitOptions.None);
+}
---
+++ src/HttPlaceholder.Common/Utilities/PathUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace HttPlaceholder.Common.Utilities;
+
+/// <summary>
+/// Contains several helper methods for working with file paths.
+/// </summary>
+public static class PathUtilities
+{
+    private static readonly string[] _unsafeStrings = {"$", "..", "?"};
+
+    /// <summary>
+    /// A method for removing unsafe characters from a path.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static string CleanPath(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            return path;
+        }
+
+        var parts = path.Split(new[] {"/", "\\"}, StringSplitOptions.None);
+        parts = parts.Where(p => !_unsafeStrings.Any(p.Contains)).ToArray();
+        return string.Join(Path.DirectorySeparatorChar, parts);
+    }
+}
