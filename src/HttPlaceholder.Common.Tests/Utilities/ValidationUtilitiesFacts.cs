a12b605c70799726c74c926bb27bc211df01a54e
Duco <git@ducode.org>
2022-10-17 22:24:40 +0200

    Added ValidationUtilities

---
+++ src/HttPlaceholder.Common.Tests/Utilities/ValidationUtilitiesFacts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using HttPlaceholder.Common.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttPlaceholder.Common.Tests.Utilities;
+
+[TestClass]
+public class ValidationUtilitiesFacts
+{
+    [TestMethod]
+    public async Task IfNull_ResultIsNotNull()
+    {
+        // Arrange
+        var task = Task.FromResult("test123");
+
+        // Act
+        var result = await task.IfNull(() => throw new InvalidOperationException());
+
+        // Assert
+        Assert.AreEqual("test123", result);
+    }
+
+    [TestMethod]
+    public async Task IfNull_ResultIsNull()
+    {
+        // Arrange
+        var task = Task.FromResult((string)null);
+
+        // Act / Assert
+        await Assert.ThrowsExceptionAsync<InvalidOperationException>(() => task.IfNull(() => throw new InvalidOperationException()));
+    }
+}
---
+++ src/HttPlaceholder.Common/Utilities/ValidationUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HttPlaceholder.Common.Utilities;
+
+/// <summary>
+/// A static class that contains several validation utilities.
+/// </summary>
+public static class ValidationUtilities
+{
+    /// <summary>
+    /// A method that executes a given action if the passed task returns a null value.
+    /// </summary>
+    /// <param name="task">The task to be executed.</param>
+    /// <param name="action">The action to be executed if the task returns null.</param>
+    /// <typeparam name="T">The return type of the task.</typeparam>
+    /// <returns>The result of the executed task.</returns>
+    public static async Task<T> IfNull<T>(this Task<T> task, Action action)
+    {
+        var result = await task;
+        if (result == null)
+        {
+            action.Invoke();
+        }
+
+        return result;
+    }
+}
