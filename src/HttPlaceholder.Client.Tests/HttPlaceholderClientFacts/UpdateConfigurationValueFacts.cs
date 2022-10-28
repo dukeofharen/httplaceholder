c018fe1e62d2e9bac5cf667e9ceb2f63bb8dc6c2
Duco <git@ducode.org>
2022-10-28 21:02:54 +0200

    Updated client

---
+++ src/HttPlaceholder.Client.Tests/HttPlaceholderClientFacts/UpdateConfigurationValueFacts.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Http;
+using HttPlaceholder.Client.Dto.Configuration;
+using HttPlaceholder.Client.Exceptions;
+using HttPlaceholder.Client.Implementations;
+using RichardSzalay.MockHttp;
+
+namespace HttPlaceholder.Client.Tests.HttPlaceholderClientFacts;
+
+[TestClass]
+public class UpdateConfigurationValueFacts : BaseClientTest
+{
+    [TestMethod]
+    public async Task UpdateConfigurationValueAsync_ExceptionInRequest_ShouldThrowHttPlaceholderClientException()
+    {
+        // Arrange
+        var client = new HttPlaceholderClient(CreateHttpClient(mock => mock
+            .When(HttpMethod.Patch, $"{BaseUrl}ph-api/configuration")
+            .Respond(HttpStatusCode.BadRequest, "text/plain", "Error occurred!")));
+
+        // Act
+        var exception =
+            await Assert.ThrowsExceptionAsync<HttPlaceholderClientException>(() =>
+                client.UpdateConfigurationValueAsync(
+                    new UpdateConfigurationValueInputDto {ConfigurationKey = "storeResponses", NewValue = "false"}));
+
+        // Assert
+        Assert.AreEqual("Status code '400' returned by HttPlaceholder with message 'Error occurred!'",
+            exception.Message);
+    }
+
+    [TestMethod]
+    public async Task UpdateConfigurationValueAsync_ShouldUpdateConfiguration()
+    {
+        // Arrange
+        var client = new HttPlaceholderClient(CreateHttpClient(mock => mock
+            .When(HttpMethod.Patch, $"{BaseUrl}ph-api/configuration")
+            .WithPartialContent("storeResponses")
+            .WithPartialContent("false")
+            .Respond(HttpStatusCode.NoContent)));
+
+        // Act / Assert
+        await client.UpdateConfigurationValueAsync(
+            new UpdateConfigurationValueInputDto {ConfigurationKey = "storeResponses", NewValue = "false"});
+    }
+}
---
+++ src/HttPlaceholder.Client/Dto/Configuration/UpdateConfigurationValueInputDto.cs
@@ -0,0 +1,17 @@
+﻿namespace HttPlaceholder.Client.Dto.Configuration;
+
+/// <summary>
+///     A class used for performing a runtime update on a configuration value
+/// </summary>
+public class UpdateConfigurationValueInputDto
+{
+    /// <summary>
+    ///     The configuration key.
+    /// </summary>
+    public string ConfigurationKey { get; set; }
+
+    /// <summary>
+    ///     The new configuration value.
+    /// </summary>
+    public string NewValue { get; set; }
+}
