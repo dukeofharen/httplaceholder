34e0dfd7db6263b00d03900a0543e979f46dd0d9
Duco <git@ducode.org>
2022-11-12 12:39:51 +0100

    Added ByteUtilities facts

---
+++ src/HttPlaceholder.Common/Utilities/ByteUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace HttPlaceholder.Common.Utilities;
+
+/// <summary>
+///     A utility class used for working with bytes and byte arrays.
+/// </summary>
+public static class ByteUtilities
+{
+    /// <summary>
+    ///     Checks whether the source is a valid ASCII string.
+    /// </summary>
+    /// <param name="source">The byte array.</param>
+    /// <returns>True if the source is a valid ASCII string; false otherwise.</returns>
+    public static bool IsValidAscii(this byte[] source) => source == null || source.All(b => b <= 127);
+}
