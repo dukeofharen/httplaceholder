1b35f3870ebad3d704a296d020a4b7397fdb911a
Duco <git@ducode.org>
2022-11-28 21:09:03 +0100

    Small fix to build scripts

--- scripts/build/build-osx.sh
+++ scripts/build/build-osx.sh
@@ -23,6 +23,7 @@ fi
 # Publish application
 cd src/HttPlaceholder
 dotnet publish --configuration=release \
+    --self-contained \
     --runtime=osx-x64 \
     /p:Version=$VERSION \
     /p:AssemblyVersion=$VERSION \
