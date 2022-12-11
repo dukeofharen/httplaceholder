616bfb7ab6a13eda09677e37a3af6cde53cf41e2
Duco <git@ducode.org>
2022-12-11 17:04:35 +0100

    Fixed Linux Vagrant box

--- vagrant/linux/bootstrap.sh
+++ vagrant/linux/bootstrap.sh
@@ -1,53 +1,39 @@
 #!/bin/bash
-
 # Install Nginx
-apt update
-apt install nginx -y
+sudo apt update
+sudo apt install nginx -y
 
 # Install .NET Core
-apt update
-apt install -y dotnet6
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt update
+sudo apt install -y dotnet-sdk-7.0
 
 # Install PIP for Python
-apt install -y python3-pip
+sudo apt install -y python3-pip
 
 # Install Node.js
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-#. "$HOME/.nvm/nvm.sh"
-#nvm install --lts
-#nvm use --lts
-NODE_BIN_PATH=/bin/node
-if [ ! -f "$NODE_BIN_PATH" ]; then
-    echo "Node not installed; installing now."
-    wget https://nodejs.org/dist/v16.14.0/node-v16.14.0-linux-x64.tar.xz -O node.tar.xz
-    NODE_EXTRACT_PATH=/opt/node
-    if [ -d "$NODE_EXTRACT_PATH" ]; then
-        echo "Deleting path $NODE_EXTRACT_PATH"
-        rm -r $NODE_EXTRACT_PATH
-    fi
-
-    mkdir $NODE_EXTRACT_PATH
-    tar -xf node.tar.xz -C $NODE_EXTRACT_PATH
-    ln -sf /opt/node/node-v16.14.0-linux-x64/bin/node /bin/node
-    ln -sf /opt/node/node-v16.14.0-linux-x64/lib/node_modules/npm/bin/npm-cli.js /bin/npm
-    ln -sf /opt/node/node-v16.14.0-linux-x64/lib/node_modules/npm/bin/npx-cli.js /bin/npx
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+. "$HOME/.nvm/nvm.sh"
+nvm install --lts
+nvm use --lts
 
 # Creating HttPlaceholder folder
 export INSTALL_PATH=/opt/httplaceholder
 if [ -d "$INSTALL_PATH" ]; then
     echo "Deleting path $INSTALL_PATH"
-    rm -r $INSTALL_PATH
+    sudo rm -r $INSTALL_PATH
 fi
 
 echo "Creating path $INSTALL_PATH"
-mkdir $INSTALL_PATH
+sudo mkdir $INSTALL_PATH
 
 # Copy source to user home folder
 NEW_SOURCE_PATH=/home/vagrant/httplaceholder
 if [ -d "$NEW_SOURCE_PATH" ]; then
     echo "Deleting path $NEW_SOURCE_PATH"
-    rm -r $NEW_SOURCE_PATH
+    sudo rm -r $NEW_SOURCE_PATH
 fi
 
 mkdir $NEW_SOURCE_PATH
@@ -56,56 +42,57 @@ rsync -av --progress /httplaceholder/. $NEW_SOURCE_PATH --exclude bin --exclude 
 
 # Build HttPlaceholder
 PROJECT_PATH=$NEW_SOURCE_PATH/src/HttPlaceholder/HttPlaceholder.csproj
-dotnet publish $PROJECT_PATH -c Release --runtime linux-x64 -o $INSTALL_PATH
+sudo dotnet publish $PROJECT_PATH -c Release --runtime linux-x64 -o $INSTALL_PATH --no-self-contained
 
 # Build HttPlaceholder GUI
 GUI_PROJECT_PATH=$NEW_SOURCE_PATH/gui
 cd "$GUI_PROJECT_PATH"
 if [ -d "dist" ]; then
   echo "Deleting folder dist"
-  rm -r "dist"
+  sudo rm -r "dist"
 fi
 
-sudo npm install
-sudo npm run build
+npm install
+npm run build
 
 GUI_PATH=$INSTALL_PATH/gui
 if [ -d "$GUI_PATH" ]; then
   echo "Re-creating folder $GUI_PATH"
-  rm -r "$GUI_PATH"  
+  sudo rm -r "$GUI_PATH"  
 fi
 
-mkdir "$GUI_PATH"
-cp -r dist/. "$GUI_PATH"
+sudo mkdir "$GUI_PATH"
+sudo cp -r dist/. "$GUI_PATH"
 
 # Build docs for use in UI
 DOCS_SOURCE_DIR="$NEW_SOURCE_PATH/docs/httpl-docs"
 GUI_DOCS_DIR="$GUI_PATH/docs"
 if [ -d "$GUI_DOCS_DIR" ]; then
   echo "Deleting folder $GUI_DOCS_DIR"
-  rm -r "$GUI_DOCS_DIR"
+  sudo rm -r "$GUI_DOCS_DIR"
 fi
 
-mkdir "$GUI_DOCS_DIR"
+sudo mkdir "$GUI_DOCS_DIR"
 
 echo "Building documentation"
 cd "$DOCS_SOURCE_DIR"
 pip install mkdocs
 python3 sync.py
-mkdocs build
-cp -r site/. "$GUI_DOCS_DIR"
+/home/vagrant/.local/bin/mkdocs build
+sudo cp -r site/. "$GUI_DOCS_DIR"
 
 # Move config to correct location
 export DATA_PATH=/etc/httplaceholder
 if [ ! -d "$DATA_PATH" ]; then
     echo "Creating path $DATA_PATH"
-    mkdir $DATA_PATH
+    sudo mkdir $DATA_PATH
 fi
 
-cp $NEW_SOURCE_PATH/vagrant/linux/config.json $DATA_PATH
+sudo cp $NEW_SOURCE_PATH/vagrant/linux/config.json $DATA_PATH
 
 # Install HttPlaceholder systemd service
-envsubst < $NEW_SOURCE_PATH/vagrant/linux/httplaceholder.service > /etc/systemd/system/httplaceholder.service
+envsubst < $NEW_SOURCE_PATH/vagrant/linux/httplaceholder.service > /home/vagrant/httplaceholder.service
+sudo cp /home/vagrant/httplaceholder.service /etc/systemd/system/httplaceholder.service 
 sudo systemctl enable httplaceholder
 sudo systemctl stop httplaceholder
 sudo systemctl daemon-reload
@@ -117,10 +104,10 @@ sudo rm /etc/nginx/sites-enabled/default
 
 # Create self signed certificate for website
 if [ ! -f "$DATA_PATH/nginx.key" ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=NL/ST=Drenthe/L=Eelde/O=IT/CN=localhost" -keyout $DATA_PATH/nginx.key -out $DATA_PATH/nginx.crt
+    sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=NL/ST=Drenthe/L=Eelde/O=IT/CN=localhost" -keyout $DATA_PATH/nginx.key -out $DATA_PATH/nginx.crt
 fi
 
 # Install HttPlaceholder reverse proxy site in Nginx
 sudo cp $NEW_SOURCE_PATH/vagrant/linux/httplaceholder.conf /etc/nginx/sites-available
-ln -sf /etc/nginx/sites-available/httplaceholder.conf /etc/nginx/sites-enabled/httplaceholder.conf
-systemctl restart nginx
+sudo ln -sf /etc/nginx/sites-available/httplaceholder.conf /etc/nginx/sites-enabled/httplaceholder.conf
+sudo systemctl restart nginx
