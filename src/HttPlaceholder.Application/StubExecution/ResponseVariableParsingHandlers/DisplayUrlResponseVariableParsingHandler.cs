473905645eaf6f7c02264176bf195af55cf18b20
Duco W <github@ducode.org>
2022-12-17 20:58:49 +0100

    Updated display URL parser to also support regular expressions (#297)
    * Added extra scenario to RequestBodyResponseVariableParsingHandlerFacts
    
    * Implemented regex in DisplayUrlResponseVariableParsingHandler
    
    * Updated sample and Postman integration tests
    
    * Updated CHANGELOG
    
    * Updated docs
    
    Co-authored-by: Duco <git@ducode.org>

--- src/HttPlaceholder.Application/StubExecution/ResponseVariableParsingHandlers/DisplayUrlResponseVariableParsingHandler.cs
+++ src/HttPlaceholder.Application/StubExecution/ResponseVariableParsingHandlers/DisplayUrlResponseVariableParsingHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using HttPlaceholder.Application.Infrastructure.DependencyInjection;
 using HttPlaceholder.Application.Interfaces.Http;
 using HttPlaceholder.Common;
 using HttPlaceholder.Domain;
+using Microsoft.Extensions.Logging;
 
 namespace HttPlaceholder.Application.StubExecution.ResponseVariableParsingHandlers;
 
@@ -15,11 +17,14 @@ namespace HttPlaceholder.Application.StubExecution.ResponseVariableParsingHandle
 internal class DisplayUrlResponseVariableParsingHandler : BaseVariableParsingHandler, ISingletonService
 {
     private readonly IHttpContextService _httpContextService;
+    private readonly ILogger<DisplayUrlResponseVariableParsingHandler> _logger;
 
-    public DisplayUrlResponseVariableParsingHandler(IHttpContextService httpContextService, IFileService fileService) :
+    public DisplayUrlResponseVariableParsingHandler(IHttpContextService httpContextService, IFileService fileService,
+        ILogger<DisplayUrlResponseVariableParsingHandler> logger) :
         base(fileService)
     {
         _httpContextService = httpContextService;
+        _logger = logger;
     }
 
     /// <inheritdoc />
@@ -29,12 +34,50 @@ internal class DisplayUrlResponseVariableParsingHandler : BaseVariableParsingHan
     public override string FullName => "Display URL";
 
     /// <inheritdoc />
-    public override string[] Examples => new[] {$"(({Name}))"};
+    public override string[] Examples => new[] {$"(({Name}))", $@"(({Name}:'\/users\/([0-9]{3})\/orders'))"};
 
     /// <inheritdoc />
     protected override Task<string> InsertVariablesAsync(string input, Match[] matches, StubModel stub,
         CancellationToken cancellationToken) =>
         Task.FromResult(matches
             .Where(match => match.Groups.Count >= 2)
-            .Aggregate(input, (current, match) => current.Replace(match.Value, _httpContextService.DisplayUrl)));
+            .Aggregate(input, (current, match) => HandleDisplayUrl(match, current, _httpContextService.DisplayUrl)));
+
+    private string HandleDisplayUrl(Match match, string current, string url)
+    {
+        var result = string.Empty;
+        if (match.Groups.Count != 3)
+        {
+            _logger.LogWarning(
+                $"Number of regex matches for variable parser {GetType().Name} was {match.Groups.Count}, which should be 3.");
+        }
+        else if (string.IsNullOrWhiteSpace(match.Groups[2].Value))
+        {
+            result = url;
+        }
+        else
+        {
+            var regexValue = match.Groups[2].Value;
+            try
+            {
+                var regex = new Regex(regexValue, RegexOptions.Multiline,
+                    TimeSpan.FromSeconds(Constants.RegexTimeoutSeconds));
+                var matches = regex.Matches(url).ToArray();
+                if (matches.Length >= 1 && matches[0].Groups.Count > 1)
+                {
+                    result = matches[0].Groups[1].Value;
+                }
+                else
+                {
+                    _logger.LogInformation($"No result found in display URL for regular expression '{regexValue}'.");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, $"Error occurred while executing regex '{regexValue}' on display URL.'");
+            }
+        }
+
+        return current.Replace(match.Value, result);
+    }
 }
