1e588083991bf74e46afa9ca73787046fee41848
Duco <git@ducode.org>
2023-01-20 21:45:00 +0100

    React to "Cmd + S"

--- gui/src/utils/event.ts
+++ gui/src/utils/event.ts
@@ -1,5 +1,5 @@
 export function shouldSave(e: KeyboardEvent): boolean {
-  return e.ctrlKey && (e.key === "s" || e.key === "Enter");
+  return (e.ctrlKey || e.metaKey) && (e.key === "s" || e.key === "Enter");
 }
 
 export function escapePressed(e: KeyboardEvent): boolean {
