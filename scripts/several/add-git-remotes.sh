f78b564492e78991ee27e8a22d956b3e8c3e05be
Duco <git@ducode.org>
2023-04-26 23:06:01 +0200

    Removed unused files

--- scripts/several/add-git-remotes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git remote add all git@github.com:dukeofharen/httplaceholder.git
-git remote set-url --add all git@gitlab.com:ducode/httplaceholder.git
-
-# Now you can push with git push all --all
