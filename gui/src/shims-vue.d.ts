a32b6b3813b8c660e790e5d7771ffbc8ecbd017f
Duco <git@ducode.org>
2023-05-10 22:43:30 +0200

    Added shims file to supress TS errors

---
+++ gui/src/shims-vue.d.ts
@@ -0,0 +1,9 @@
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<
+    NonNullable<unknown>,
+    NonNullable<unknown>,
+    any
+  >;
+  export default component;
+}
