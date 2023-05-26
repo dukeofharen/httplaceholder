a73880c22bdb544fd46d4a381302f818f625f1eb
Duco <git@ducode.org>
2023-05-26 21:19:03 +0200

    Only show simple editor if stub is too large

--- gui/src/views/StubForm.vue
+++ gui/src/views/StubForm.vue
@@ -11,7 +11,7 @@
 
     <FormHelperSelector v-if="showFormHelperSelector" />
 
-    <div class="row mt-3">
+    <div class="row mt-3" v-if="showEditorTypeButtons">
       <div class="col-md-12">
         <button
           class="btn btn-outline btn-sm me-2"
@@ -115,6 +115,12 @@ export default defineComponent({
         ? editorTypes.simple
         : editorTypes.codemirror;
     });
+    const showEditorTypeButtons = computed(() => {
+      return (
+        selectedEditorType.value === editorTypes.simple ||
+        stubFormStore.getInputLength > simpleEditorThreshold
+      );
+    });
 
     // Functions
     const initialize = async () => {
@@ -172,6 +178,7 @@ export default defineComponent({
       selectedEditorType,
       editorType,
       docsUrl,
+      showEditorTypeButtons,
     };
   },
 });
