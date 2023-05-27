e467b2a043019f1ba1962830ac318d3606e52ce1
Duco <git@ducode.org>
2023-05-27 15:26:07 +0200

    Render image in binary body

--- gui/src/components/request/BinaryRequestBody.vue
+++ gui/src/components/request/BinaryRequestBody.vue
@@ -3,9 +3,12 @@
     <div class="card-body">
       <div class="row">
         <div class="col-md-12">
-          <button class="btn btn-sm btn-outline-primary" @click="download">
+          <button class="btn btn-sm btn-outline-primary mb-2" @click="download">
             Download
           </button>
+          <div v-if="bodyType === bodyTypes.image">
+            <img :src="'data:' + contentType + ';base64,' + body" />
+          </div>
         </div>
       </div>
     </div>
@@ -13,10 +16,17 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 import { RequestResultModel } from "@/domain/request/request-result-model";
 import { downloadBlob } from "@/utils/download";
 import { base64ToBlob } from "@/utils/text";
+import { imageMimeTypes, pdfMimeType } from "@/constants/technical";
+
+const bodyTypes = {
+  image: "image",
+  pdf: "pdf",
+  other: "other",
+};
 
 export default defineComponent({
   name: "BinaryRequestBody",
@@ -27,17 +37,50 @@ export default defineComponent({
     },
   },
   setup(props) {
+    // Computed
+    const body = computed(() => {
+      return props.request.requestParameters.body;
+    });
+    const contentType = computed(() => {
+      const headers = props.request.requestParameters.headers;
+      const contentTypeHeaderKey = Object.keys(headers).find(
+        (k) => k.toLowerCase() === "content-type"
+      );
+      if (!contentTypeHeaderKey) {
+        return "";
+      }
+
+      return headers[contentTypeHeaderKey].toLowerCase().split(";")[0];
+    });
+    const bodyType = computed(() => {
+      const type = contentType.value;
+      if (!type) {
+        return bodyTypes.other;
+      }
+
+      if (imageMimeTypes.find((m) => type.includes(m))) {
+        return bodyTypes.image;
+      }
+
+      if (type.includes(pdfMimeType)) {
+        return bodyTypes.pdf;
+      }
+
+      return bodyTypes.other;
+    });
+
     // Methods
     const download = () => {
-      downloadBlob(
-        "file.bin",
-        base64ToBlob(props.request.requestParameters.body)
-      );
+      downloadBlob("file.bin", base64ToBlob(body.value));
     };
 
-    return { download };
+    return { download, bodyType, bodyTypes, contentType, body };
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+img {
+  max-width: 100%;
+}
+</style>
--- gui/src/constants/technical.ts
+++ gui/src/constants/technical.ts
@@ -4,3 +4,15 @@ export const dateTimeFormat = "YYYY-MM-DD HH:mm:ss";
 // Editor constants.
 export const simpleEditorThreshold = 4000;
 export const requestBodyLineLimit = 100; // The number of lines a request can be before a "show more" button is shown.
+
+export const imageMimeTypes = [
+  "image/apng",
+  "image/avif",
+  "image/gif",
+  "image/jpeg",
+  "image/png",
+  "image/svg+xml",
+  "image/webp",
+];
+
+export const pdfMimeType = "application/pdf";
