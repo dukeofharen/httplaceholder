e865fba9cb0ed3465cafc7ed7eb47bea600c02dc
Duco <git@ducode.org>
2023-06-02 23:42:12 +0200

    Implemented first version of string replace

--- src/HttPlaceholder.Application/StubExecution/ResponseWriters/StringReplaceResponseWriter.cs
+++ src/HttPlaceholder.Application/StubExecution/ResponseWriters/StringReplaceResponseWriter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using HttPlaceholder.Application.Infrastructure.DependencyInjection;
@@ -18,12 +19,22 @@ public class StringReplaceResponseWriter : IResponseWriter, ISingletonService
     public Task<StubResponseWriterResultModel> WriteToResponseAsync(StubModel stub, ResponseModel response,
         CancellationToken cancellationToken)
     {
-        var replace = stub.Response?.Replace;
-        if (replace == null || !replace.Any())
+        var replace = stub.Response?.Replace?.ToArray();
+        if (replace == null || !replace.Any() || response.Body == null || !response.Body.Any())
         {
             return Task.FromResult(StubResponseWriterResultModel.IsNotExecuted(GetType().Name));
         }
 
+        var body = Encoding.UTF8.GetString(response.Body);
+        body = replace.Aggregate(body, PerformReplace);
+        response.Body = Encoding.UTF8.GetBytes(body);
+
         return Task.FromResult(StubResponseWriterResultModel.IsExecuted(GetType().Name));
+    }
+
+    private static string PerformReplace(string body, StubResponseReplaceModel model)
+    {
+        // TODO add option to ignore case.
+        return body.Replace(model.Text, model.ReplaceWith);
     }
 }
