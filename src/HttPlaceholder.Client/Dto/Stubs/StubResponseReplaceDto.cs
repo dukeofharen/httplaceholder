be79faf8320393d9ec3cc4779a8d3183b9940d9b
Duco <git@ducode.org>
2023-06-07 21:57:25 +0200

    Added string replace DTOs to client

--- src/HttPlaceholder.Client/Dto/Stubs/StubResponseReplaceDto.cs
+++ src/HttPlaceholder.Client/Dto/Stubs/StubResponseReplaceDto.cs
@@ -1,35 +1,27 @@
-﻿using HttPlaceholder.Application.Interfaces.Mappings;
-using HttPlaceholder.Domain;
-using YamlDotNet.Serialization;
-
-namespace HttPlaceholder.Web.Shared.Dto.v1.Stubs;
+﻿namespace HttPlaceholder.Client.Dto.Stubs;
 
 /// <summary>
 ///     A model which contains the configuration needed to do a string or regex replace on the stub response.
 /// </summary>
-public class StubResponseReplaceDto : IMapFrom<StubResponseReplaceModel>, IMapTo<StubResponseReplaceModel>
+public class StubResponseReplaceDto
 {
     /// <summary>
     ///     The text to look for. Set either this or regex, not both.
     /// </summary>
-    [YamlMember(Alias = "text")]
     public string Text { get; set; }
 
     /// <summary>
     ///     Whether to ignore the casing when looking for <see cref="Text"/>.
     /// </summary>
-    [YamlMember(Alias = "ignoreCase")]
     public bool IgnoreCase { get; set; } = true;
 
     /// <summary>
     ///     The regex expression to look for. Set either this or text, not both.
     /// </summary>
-    [YamlMember(Alias = "regex")]
     public string Regex { get; set; }
 
     /// <summary>
     ///     The value the found text or regex matches should be replaced with.
     /// </summary>
-    [YamlMember(Alias = "replaceWith")]
     public string ReplaceWith { get; set; }
 }
