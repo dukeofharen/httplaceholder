12127d84a8693cdca33a909a6fa4ecb5ac754d54
Duco <git@ducode.org>
2023-08-02 19:35:38 +0200

    Added basic web infrastructure module

---
+++ src/HttPlaceholder.WebInfrastructure/WebInfrastructureModule.cs
@@ -0,0 +1,16 @@
+﻿namespace HttPlaceholder.WebInfrastructure;
+
+/// <summary>
+///     A static class to register dependencies in the WebInfrastructure project.
+/// </summary>
+public static class WebInfrastructureModule
+{
+    /// <summary>
+    ///     Registers all calsses in the WebInfrastructure module on the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    public static IServiceCollection AddWebInfrastructureModule(this IServiceCollection services)
+    {
+        return services;
+    }
+}
