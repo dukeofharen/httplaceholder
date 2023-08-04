3673979fbf546af181fe896e062362fb6b3d6783
Duco <git@ducode.org>
2023-08-04 21:12:54 +0200

    Moved scheduled job logic to application project

---
+++ src/HttPlaceholder.Application/ScheduledJobs/Queries/GetScheduledJobNames/GetScheduledJobNamesQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using HttPlaceholder.Application.Interfaces.HostedServices;
+using MediatR;
+
+namespace HttPlaceholder.Application.ScheduledJobs.Queries.GetScheduledJobNames;
+
+/// <summary>
+///     A query handler for retrieving all scheduled job names.
+/// </summary>
+public class GetScheduledJobNamesQueryHandler : IRequestHandler<GetScheduledJobNamesQuery, IEnumerable<string>>
+{
+    private readonly IEnumerable<ICustomHostedService> _hostedServices;
+
+    /// <summary>
+    ///     Constructs an <see cref="GetScheduledJobNamesQueryHandler"/> instance.
+    /// </summary>
+    public GetScheduledJobNamesQueryHandler(IEnumerable<ICustomHostedService> hostedServices)
+    {
+        _hostedServices = hostedServices;
+    }
+
+    /// <inheritdoc />
+    public Task<IEnumerable<string>> Handle(GetScheduledJobNamesQuery request, CancellationToken cancellationToken) =>
+        Task.FromResult(_hostedServices.Select(s => s.Key));
+}
