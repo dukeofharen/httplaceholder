fdc042f1b566c484ee875ffd98a5e3c3adf1ba8c
Duco <git@ducode.org>
2023-08-05 17:14:02 +0200

    Updated MediatR to 12

---
+++ src/HttPlaceholder.Application/Infrastructure/MediatR/AuditBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace HttPlaceholder.Application.Infrastructure.MediatR;
+
+/// <summary>
+///     A MediatR behavior for logging calls to MediatR.
+/// </summary>
+public class AuditBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    /// <inheritdoc />
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        Console.WriteLine(request.GetType().FullName);
+        return await next();
+    }
+}
