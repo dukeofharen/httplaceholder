654fe712ca5a0ca31cccc2aac6493593b624e1fb
Duco <git@ducode.org>
2023-09-04 21:46:48 +0200

    Introduced StubRequestContext

---
+++ src/HttPlaceholder.Application/StubExecution/IStubRequestContext.cs
@@ -0,0 +1,14 @@
+﻿namespace HttPlaceholder.Application.StubExecution;
+
+/// <summary>
+///     Describes a class that is used to set and get stub related data for a specific HTTP request.
+/// </summary>
+public interface IStubRequestContext
+{
+    /// <summary>
+    ///     Gets or sets the stub distribution key. Returns null if stubs, requests and other models should not be split.
+    ///     The distribution key is used to, for example, be able to save stubs and requests for a specific user
+    ///     (e.g. in case of a SaaS version of the application where multiple users' stubs are saved in the same data source).
+    /// </summary>
+    string DistributionKey { get; set; }
+}
---
+++ src/HttPlaceholder.Application/StubExecution/Implementations/StubRequestContext.cs
@@ -0,0 +1,28 @@
+﻿using HttPlaceholder.Application.Infrastructure.DependencyInjection;
+using HttPlaceholder.Application.Interfaces.Http;
+
+namespace HttPlaceholder.Application.StubExecution.Implementations;
+
+/// <summary>
+///     A class that is used to set and get stub related data for a specific HTTP request.
+/// </summary>
+public class StubRequestContext : IStubRequestContext, ISingletonService
+{
+    private const string DistributionKeyKey = "distributionKey";
+    private readonly IHttpContextService _httpContextService;
+
+    /// <summary>
+    ///     Constructs a <see cref="StubRequestContext"/>.
+    /// </summary>
+    public StubRequestContext(IHttpContextService httpContextService)
+    {
+        _httpContextService = httpContextService;
+    }
+
+    /// <inheritdoc />
+    public string DistributionKey
+    {
+        get => _httpContextService.GetItem<string>(DistributionKeyKey);
+        set => _httpContextService.SetItem(DistributionKeyKey, value);
+    }
+}
