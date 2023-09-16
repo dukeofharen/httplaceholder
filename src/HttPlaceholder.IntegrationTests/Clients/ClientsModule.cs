8a12fb52b0aa0786b1b822b6b3822ce47818fe1d
Duco <git@ducode.org>
2023-09-16 14:18:14 +0200

    Removed integration tests

--- src/HttPlaceholder.IntegrationTests/Clients/ClientsModule.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace HttPlaceholder.IntegrationTests.Clients;
-
-public static class ClientsModule
-{
-    public static IServiceCollection AddClientsModule(this IServiceCollection services) =>
-        services.AddTransient<DevelopmentClient>();
-}
--- src/HttPlaceholder.IntegrationTests/Config/HttPlaceholderSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HttPlaceholder.IntegrationTests.Config;
-
-public class HttPlaceholderSettings
-{
-    public string HttpUrl { get; set; }
-
-    public string HttpsUrl { get; set; }
-}
