45e60740d0b07aa55f7fec5900e0c0b2711bc335
Duco <git@ducode.org>
2023-09-16 16:24:45 +0200

    Test script fixes

--- test/run.sh
+++ test/run.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-set -e
-set -u
+set -eu
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-HTTPL_ROOT_URL="${1-http://localhost:5000}"
+HTTPL_ROOT_URL="http://localhost:5000"
+VERBOSE=""
+
+while getopts ":v:u:" o; do
+  case "${o}" in
+  v)
+    VERBOSE=${OPTARG}
+    ;;
+  u)
+    HTTPL_ROOT_URL=${OPTARG}
+    ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 echo "Running integration tests for URL $HTTPL_ROOT_URL"
 
-hurl --test --glob "$DIR/tests/**/*.hurl" --variable rootUrl=$HTTPL_ROOT_URL --verbose
+COMMAND="hurl --test --glob '$DIR/tests/**/*.hurl' --variable rootUrl=$HTTPL_ROOT_URL"
+if [ "$VERBOSE" = "verbose" ]; then
+  COMMAND="$COMMAND --verbose"
+fi
+
+eval "$COMMAND"
+# echo "$COMMAND"
