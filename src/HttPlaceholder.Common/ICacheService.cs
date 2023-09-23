9326b9d7ff04f704c996dcb90bcb48c854a5f9cc
Duco <git@ducode.org>
2023-09-23 21:40:02 +0200

    Introduced CacheService

---
+++ src/HttPlaceholder.Common/ICacheService.cs
@@ -0,0 +1,22 @@
+﻿namespace HttPlaceholder.Common;
+
+/// <summary>
+///     Describes a class that is used as cache.
+/// </summary>
+public interface ICacheService
+{
+    /// <summary>
+    ///     Retrieves a cache item from the current scope (e.g. HttpContext).
+    /// </summary>
+    /// <param name="key">The item key.</param>
+    /// <typeparam name="TObject">The type of the object.</typeparam>
+    /// <returns>The object.</returns>
+    TObject GetScopedItem<TObject>(string key);
+
+    /// <summary>
+    ///     Puts a cache item on the current scope (e.g. HttpContext).
+    /// </summary>
+    /// <param name="key">The item key.</param>
+    /// <param name="item">The item to store.</param>
+    void SetScopedItem(string key, object item);
+}
