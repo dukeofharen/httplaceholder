a09c57d949c6e6ba8adad263a7a1566799668642
Duco <git@ducode.org>
2024-07-11 09:48:16 +0200

    Added samples for JSONPath string replace

---
+++ docs/samples/22-string-regex-jsonpath-replace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+. $DIR/setup.sh
+# String replace
+curl --location --request GET "$HTTPL_ROOT_URL/string-replace" -D-
+
+# Regex replace
+curl --location --request GET "$HTTPL_ROOT_URL/regex-replace?queryString=Bassie" -D-
+
+# String and regex replace
+curl --location --request GET "$HTTPL_ROOT_URL/string-and-regex-replace" -D-
+
+# JSONPath replace
+curl --location --request GET "$HTTPL_ROOT_URL/jsonpath-replace?q1=Delfzijl" -D-
