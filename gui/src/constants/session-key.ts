c4681b7034eb3dbdc09044147b471fa4844531e1
Duco <git@ducode.org>
2024-08-10 20:44:45 +0200

    Moved session-key constants

--- gui/src/constants/session-key.ts
+++
@@ -1,7 +0,0 @@
-export enum SessionKey {
-  intermediateStub = "intermediateStub",
-  settings = "settings",
-  userToken = "userToken",
-  stubsFilter = "stubsFilter",
-  requestsFilter = "requestsFilter",
-}
