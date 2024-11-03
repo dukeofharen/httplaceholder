06d32e031fb2e207dc485b9b04e998b3ab7c0fc7
Duco <git@ducode.org>
2024-11-03 21:43:16 +0100

    Simplification in string-checking-keywords.ts

--- gui/src/domain/string-checking-keywords.ts
+++ gui/src/domain/string-checking-keywords.ts
@@ -31,112 +31,11 @@ export const keywords = {
 };
 
 export function getStringCheckingKeywords(): StringCheckingKeyword[] {
-  const result = [
-    {
-      key: keywords.equals,
-      name: translate("stringChecking.equals"),
-      description: translate("stringChecking.equalsDescription"),
-    },
-    {
-      key: keywords.equalsci,
-      name: translate("stringChecking.equalsci"),
-      description: translate("stringChecking.equalsciDescription"),
-    },
-    {
-      key: keywords.notequals,
-      name: translate("stringChecking.notequals"),
-      description: translate("stringChecking.notequalsDescription"),
-    },
-    {
-      key: keywords.notequalsci,
-      name: translate("stringChecking.notequalsci"),
-      description: translate("stringChecking.notequalsciDescription"),
-    },
-    {
-      key: keywords.contains,
-      name: translate("stringChecking.contains"),
-      description: translate("stringChecking.containsDescription"),
-    },
-    {
-      key: keywords.containsci,
-      name: translate("stringChecking.containsci"),
-      description: translate("stringChecking.containsciDescription"),
-    },
-    {
-      key: keywords.notcontains,
-      name: translate("stringChecking.notcontains"),
-      description: translate("stringChecking.notcontainsDescription"),
-    },
-    {
-      key: keywords.notcontainsci,
-      name: translate("stringChecking.notcontainsci"),
-      description: translate("stringChecking.notcontainsciDescription"),
-    },
-    {
-      key: keywords.startswith,
-      name: translate("stringChecking.startswith"),
-      description: translate("stringChecking.startswithDescription"),
-    },
-    {
-      key: keywords.startswithci,
-      name: translate("stringChecking.startswithci"),
-      description: translate("stringChecking.startswithciDescription"),
-    },
-    {
-      key: keywords.doesnotstartwith,
-      name: translate("stringChecking.doesnotstartwith"),
-      description: translate("stringChecking.doesnotstartwithDescription"),
-    },
-    {
-      key: keywords.doesnotstartwithci,
-      name: translate("stringChecking.doesnotstartwithci"),
-      description: translate("stringChecking.doesnotstartwithciDescription"),
-    },
-    {
-      key: keywords.endswith,
-      name: translate("stringChecking.endswith"),
-      description: translate("stringChecking.endswithciDescription"),
-    },
-    {
-      key: keywords.endswithci,
-      name: translate("stringChecking.endswithci"),
-      description: translate("stringChecking.endswithDescription"),
-    },
-    {
-      key: keywords.doesnotendwith,
-      name: translate("stringChecking.doesnotendwith"),
-      description: translate("stringChecking.doesnotendwithDescription"),
-    },
-    {
-      key: keywords.doesnotendwithci,
-      name: translate("stringChecking.doesnotendwithci"),
-      description: translate("stringChecking.doesnotendwithciDescription"),
-    },
-    {
-      key: keywords.regex,
-      name: translate("stringChecking.regex"),
-      description: translate("stringChecking.regexDescription"),
-    },
-    {
-      key: keywords.regexnomatches,
-      name: translate("stringChecking.regexnomatches"),
-      description: translate("stringChecking.regexnomatchesDescription"),
-    },
-    {
-      key: keywords.minlength,
-      name: translate("stringChecking.minlength"),
-      description: translate("stringChecking.minLengthDescription"),
-    },
-    {
-      key: keywords.maxlength,
-      name: translate("stringChecking.maxlength"),
-      description: translate("stringChecking.maxlengthDescription"),
-    },
-    {
-      key: keywords.exactlength,
-      name: translate("stringChecking.exactlength"),
-      description: translate("stringChecking.exactlengthDescription"),
-    },
-  ];
-  return result;
+  return Object.keys(keywords).map((k) => {
+    return {
+      key: k,
+      name: translate(`stringChecking.${k}`),
+      description: translate(`stringChecking.${k}Description`),
+    };
+  }) as StringCheckingKeyword[];
 }
