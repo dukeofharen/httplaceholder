a7c7b5229863b16f19aa5d661af3d57c0036f7bf
Duco <git@ducode.org>
2025-01-01 19:35:00 +0100

    NavMenu refactoring

--- gui2/src/App.vue
+++ gui2/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import Navigation from '@/components/Navigation.vue'
+import NavMenu from '@/components/nav/NavMenu.vue'
 </script>
 
 <template>
-  <Navigation />
+  <NavMenu />
   <div class="p-4 sm:ml-64">
     <RouterView />
   </div>
---
+++ gui2/src/components/nav/NavMenu.vue
@@ -0,0 +1,148 @@
+<script setup lang="ts">
+import { translate } from '@/utils/translate.ts'
+import { renderDocLink } from '@/utils/doc.ts'
+import { useUsersStore } from '@/stores/users.ts'
+import { useRouter } from 'vue-router'
+import { computed } from 'vue'
+import { useMetadataStore } from '@/stores/metadata.ts'
+
+const userStore = useUsersStore()
+const metadataStore = useMetadataStore()
+const router = useRouter()
+
+// Data
+const plainMenuItems = [
+  {
+    title: translate('sidebar.requests'),
+    icon: 'eye',
+    routeName: 'Requests',
+    hideWhenAuthEnabledAndNotLoggedIn: true,
+  },
+  {
+    title: translate('sidebar.stubs'),
+    icon: 'code-slash',
+    routeName: 'Stubs',
+    hideWhenAuthEnabledAndNotLoggedIn: true,
+  },
+  {
+    title: translate('sidebar.addStubs'),
+    icon: 'plus',
+    routeName: 'StubForm',
+    hideWhenAuthEnabledAndNotLoggedIn: true,
+  },
+  {
+    title: translate('sidebar.importStubs'),
+    icon: 'arrow-up-short',
+    routeName: 'ImportStubs',
+    hideWhenAuthEnabledAndNotLoggedIn: true,
+  },
+  {
+    title: translate('sidebar.scenarios'),
+    icon: 'card-list',
+    routeName: 'Scenarios',
+    hideWhenAuthEnabledAndNotLoggedIn: true,
+  },
+  {
+    title: translate('sidebar.docs'),
+    icon: 'file-earmark-text',
+    url: renderDocLink(),
+    targetBlank: true,
+    onlyShowWhenLoggedInAndAuthEnabled: false,
+  },
+  {
+    title: translate('sidebar.apiDocs'),
+    icon: 'file-earmark-text',
+    url: '/swagger/index.html',
+    targetBlank: true,
+    onlyShowWhenLoggedInAndAuthEnabled: false,
+  },
+  {
+    title: translate('sidebar.settings'),
+    icon: 'wrench',
+    routeName: 'Settings',
+    hideWhenAuthEnabledAndNotLoggedIn: true,
+  },
+  {
+    title: translate('sidebar.logOut'),
+    icon: 'box-arrow-left',
+    onlyShowWhenLoggedInAndAuthEnabled: true,
+    onClick: async () => {
+      userStore.logOut()
+      await router.push({ name: 'Login' })
+    },
+  },
+]
+
+// Computed
+const menuItems = computed(() => {
+  const isAuthenticated = userStore.getAuthenticated
+  const authEnabled = metadataStore.getAuthenticationEnabled
+  return plainMenuItems.filter(
+    (i) =>
+      (i.onlyShowWhenLoggedInAndAuthEnabled && isAuthenticated && authEnabled) ||
+      (i.hideWhenAuthEnabledAndNotLoggedIn && authEnabled && isAuthenticated) ||
+      (i.hideWhenAuthEnabledAndNotLoggedIn && !authEnabled) ||
+      i.onlyShowWhenLoggedInAndAuthEnabled === false,
+  )
+})
+</script>
+
+<template>
+  <button
+    data-drawer-target="default-sidebar"
+    data-drawer-toggle="default-sidebar"
+    aria-controls="default-sidebar"
+    type="button"
+    class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+  >
+    <span class="sr-only">Open sidebar</span>
+    <svg
+      class="w-6 h-6"
+      aria-hidden="true"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        clip-rule="evenodd"
+        fill-rule="evenodd"
+        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+      ></path>
+    </svg>
+  </button>
+
+  <aside
+    id="default-sidebar"
+    class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+    aria-label="Sidebar"
+  >
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <ul class="space-y-2 font-medium">
+        <li>
+          <a
+            href="#"
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+          >
+            <svg
+              class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 21"
+            >
+              <path
+                d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"
+              />
+              <path
+                d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"
+              />
+            </svg>
+            <span class="ms-3">Dashboard</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </aside>
+</template>
+
+<style scoped></style>
