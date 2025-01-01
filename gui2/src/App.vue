50035335a3338414377cb2949b0dc0cc028e3179
Duco <git@ducode.org>
2025-01-01 11:30:03 +0100

    Added basic navigation

--- gui2/src/App.vue
+++ gui2/src/App.vue
@@ -1,18 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Navigation from '@/components/Navigation.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Navigation />
+  <div class="p-4 sm:ml-64">
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
