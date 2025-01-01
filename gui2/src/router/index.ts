8ad7f06e72581daf6819692e59c8769100f0bda6
Duco <git@ducode.org>
2025-01-01 11:10:22 +0100

    Removed unnecessary stuff

--- gui2/src/router/index.ts
+++ gui2/src/router/index.ts
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
