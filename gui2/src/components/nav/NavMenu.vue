743f41cf69cfaf3fa666686ad4f8c02010a962bf
Duco <git@ducode.org>
2025-01-01 22:00:02 +0100

    Open mobile menu on button click

--- gui2/src/components/nav/NavMenu.vue
+++ gui2/src/components/nav/NavMenu.vue
@@ -3,16 +3,18 @@ import { translate } from '@/utils/translate.ts'
 import { renderDocLink } from '@/utils/doc.ts'
 import { useUsersStore } from '@/stores/users.ts'
 import { useRouter } from 'vue-router'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useMetadataStore } from '@/stores/metadata.ts'
 import NavItem from '@/components/nav/NavItem.vue'
 import type { MenuItemModel } from '@/domain/menu-item-model.ts'
+import { Bars3Icon } from '@heroicons/vue/24/solid'
 
 const userStore = useUsersStore()
 const metadataStore = useMetadataStore()
 const router = useRouter()
 
 // Data
+const mobileMenuOpen = ref(false)
 const plainMenuItems: MenuItemModel[] = [
   {
     title: translate('sidebar.requests'),
@@ -95,27 +97,17 @@ const menuItems = computed<MenuItemModel[]>(() => {
     data-drawer-toggle="default-sidebar"
     aria-controls="default-sidebar"
     type="button"
+    @click="mobileMenuOpen = !mobileMenuOpen"
     class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
   >
     <span class="sr-only">Open sidebar</span>
-    <svg
-      class="w-6 h-6"
-      aria-hidden="true"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        clip-rule="evenodd"
-        fill-rule="evenodd"
-        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-      ></path>
-    </svg>
+    <Bars3Icon class="size-6" />
   </button>
 
   <aside
     id="default-sidebar"
-    class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+    class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform sm:translate-x-0"
+    :class="{ '-translate-x-full': !mobileMenuOpen, 'transform-none': mobileMenuOpen }"
     aria-label="Sidebar"
   >
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
