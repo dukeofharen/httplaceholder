242171945f47875269e88d21ddcff904b463d5be
Duco <git@ducode.org>
2025-01-02 14:25:28 +0100

    Configured proxy

--- gui2/vite.config.ts
+++ gui2/vite.config.ts
@@ -6,13 +6,33 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/ph-api': {
+        target: 'http://localhost:5000',
+      },
+      '/swagger': {
+        target: 'http://localhost:5000',
+      },
+      '/requestHub': {
+        target: 'http://localhost:5000',
+        ws: true,
+      },
+      '/scenarioHub': {
+        target: 'http://localhost:5000',
+        ws: true,
+      },
+      '/stubHub': {
+        target: 'http://localhost:5000',
+        ws: true,
+      },
+    },
+  },
+  base: '',
 })
