d876893b9245bd5f122ae89020fd98addc3b2b64
Duco <git@ducode.org>
2025-01-05 17:23:36 +0100

    Added basic RequestDetails componewnt

--- gui2/src/components/request/RequestComponent.vue
+++ gui2/src/components/request/RequestComponent.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import type { PropType } from 'vue'
+import { type PropType, ref } from 'vue'
 import type { RequestOverviewModel } from '@/domain/request/request-overview-model.ts'
 import RequestHeading from '@/components/request/RequestHeading.vue'
+import RequestDetails from '@/components/request/RequestDetails.vue'
 
 const props = defineProps({
   overviewRequest: {
@@ -9,11 +10,24 @@ const props = defineProps({
     required: true,
   },
 })
+
+// Data
+const showDetails = ref(false)
+
+// Functions
+function onRequestHeadingClick() {
+  showDetails.value = !showDetails.value
+}
 </script>
 
 <template>
   <div class="border font-mono border-gray-500 p-2">
-    <RequestHeading :overview-request="props.overviewRequest" />
+    <RequestHeading
+      :overview-request="props.overviewRequest"
+      class="cursor-pointer"
+      @click="onRequestHeadingClick"
+    />
+    <RequestDetails v-if="showDetails" :overview-request="props.overviewRequest" />
   </div>
 </template>
 
---
+++ gui2/src/components/request/RequestDetails.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import type { RequestOverviewModel } from '@/domain/request/request-overview-model.ts'
+
+const props = defineProps({
+  overviewRequest: {
+    type: Object as PropType<RequestOverviewModel>,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  {{ props.overviewRequest }}
+</template>
+
+<style scoped></style>
