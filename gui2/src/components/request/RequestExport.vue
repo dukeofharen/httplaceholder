d9d342b406508aea281d9048ab0a34dae919642a
Duco <git@ducode.org>
2025-02-02 20:22:05 +0100

    Fix in RequestExport

--- gui2/src/components/request/RequestExport.vue
+++ gui2/src/components/request/RequestExport.vue
@@ -107,9 +107,11 @@ watch(exportType, async (newType) => {
   <div class="flex flex-col gap-1">
     <SelectInput id="exportType" :items="selectListItems" v-model="exportType" />
     <div v-if="showExportResult" class="mt-2">
-      <div v-if="showExportResultText" class="flex flex-row items-center gap-2">
-        <ClipboardIcon class="size-6 cursor-pointer" @click="copy" />
-        <CodeHighlight :code="exportResult" :language="language" />
+      <div v-if="showExportResultText" class="flex flex-row items-center gap-2 overflow-x-scroll">
+        <div>
+          <ClipboardIcon class="size-6 cursor-pointer" @click="copy" />
+        </div>
+        <CodeHighlight class="" :code="exportResult" :language="language" />
       </div>
       <template v-else>
         <ButtonComponent
@@ -117,8 +119,8 @@ watch(exportType, async (newType) => {
           @click="download"
           :title="$translate('request.downloadExportedRequest')"
           :dense="true"
-          >{{ $translate('general.download') }}</ButtonComponent
-        >
+          >{{ $translate('general.download') }}
+        </ButtonComponent>
       </template>
     </div>
   </div>
