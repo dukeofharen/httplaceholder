fc7e74e5e7008ff9ca4a8d16b8033e0bd1f3d457
Duco <git@ducode.org>
2025-02-08 14:37:29 +0100

    Added table for request params

--- gui2/src/components/request/RequestData.vue
+++ gui2/src/components/request/RequestData.vue
@@ -10,6 +10,9 @@ const props = defineProps({
   },
 })
 
+const trClass = 'hover:bg-gray-100 dark:hover:bg-gray-700'
+const tdClass = 'py-1 px-2 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'
+
 // Computed
 const requestParams = computed(() => props.request.requestParameters)
 const requestTime = computed(() => formatDateTime(props.request?.requestEndTime))
@@ -20,44 +23,64 @@ const duration = computed(() => {
 </script>
 
 <template>
-  <div>
-    <table>
-      <tr>
-        <td>{{ $translate('request.url') }}</td>
-        <td>{{ requestParams.url }}</td>
-      </tr>
-      <tr>
-        <td>{{ $translate('request.clientIp') }}</td>
-        <td>{{ requestParams.clientIp }}</td>
-      </tr>
-      <tr>
-        <td>{{ $translate('request.correlationId') }}</td>
-        <td>{{ props.request.correlationId }}</td>
-      </tr>
-      <tr>
-        <td>{{ $translate('request.executedStub') }}</td>
-        <td>
-          <router-link :to="{ name: 'Stubs', query: { tenant: props.request.executingStubId } }">{{
-            props.request.executingStubId
-          }}</router-link>
-        </td>
-      </tr>
-      <tr>
-        <td>{{ $translate('request.stubTenant') }}</td>
-        <td>
-          <router-link :to="{ name: 'Stubs', query: { tenant: props.request.stubTenant } }">{{
-            props.request.stubTenant
-          }}</router-link>
-        </td>
-      </tr>
-      <tr>
-        <td>{{ $translate('request.requestTime') }}</td>
-        <td>
-          <span
-            >{{ requestTime }} ({{ $vsprintf($translate('request.itTookMs'), [duration]) }})</span
-          >
-        </td>
-      </tr>
+  <div class="max-w-full overflow-x-scroll">
+    <table class="min-w-full max-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+      <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+        <tr :class="trClass">
+          <td :class="tdClass">
+            {{ $translate('request.url') }}
+          </td>
+          <td :class="tdClass">
+            {{ requestParams.url }}
+          </td>
+        </tr>
+        <tr :class="trClass">
+          <td :class="tdClass">
+            {{ $translate('request.clientIp') }}
+          </td>
+          <td :class="tdClass">
+            {{ requestParams.clientIp }}
+          </td>
+        </tr>
+        <tr :class="trClass">
+          <td :class="tdClass">
+            {{ $translate('request.correlationId') }}
+          </td>
+          <td :class="tdClass">
+            {{ props.request.correlationId }}
+          </td>
+        </tr>
+        <tr :class="trClass">
+          <td :class="tdClass">
+            {{ $translate('request.executedStub') }}
+          </td>
+          <td :class="tdClass">
+            <router-link :to="{ name: 'Stubs', query: { tenant: props.request.executingStubId } }"
+              >{{ props.request.executingStubId }}
+            </router-link>
+          </td>
+        </tr>
+        <tr :class="trClass">
+          <td :class="tdClass">
+            {{ $translate('request.stubTenant') }}
+          </td>
+          <td :class="tdClass">
+            <router-link :to="{ name: 'Stubs', query: { tenant: props.request.stubTenant } }"
+              >{{ props.request.stubTenant }}
+            </router-link>
+          </td>
+        </tr>
+        <tr :class="trClass">
+          <td :class="tdClass">
+            {{ $translate('request.requestTime') }}
+          </td>
+          <td :class="tdClass">
+            <span
+              >{{ requestTime }} ({{ $vsprintf($translate('request.itTookMs'), [duration]) }})</span
+            >
+          </td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
