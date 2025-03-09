dbd8dccef596a91a836e47d914cb569dcb06857f
Duco <git@ducode.org>
2025-03-09 13:48:04 +0100

    Refactored UploadStubs

--- gui/src/components/import/UploadStubs.vue
+++ gui/src/components/import/UploadStubs.vue
@@ -11,47 +11,39 @@
   </span>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { handleHttpError } from '@/utils/error'
 import { useRouter } from 'vue-router'
 import { success } from '@/utils/toast'
 import { useStubsStore } from '@/store/stubs'
-import { defineComponent } from 'vue'
 import { vsprintf } from 'sprintf-js'
 import type { FileUploadedModel } from '@/domain/file-uploaded-model'
 import { useGeneralStore } from '@/store/general'
 import { translate } from '@/utils/translate'
 
-export default defineComponent({
-  name: 'UploadStubs',
-  setup() {
-    const stubStore = useStubsStore()
-    const generalStore = useGeneralStore()
-    const router = useRouter()
+const stubStore = useStubsStore()
+const generalStore = useGeneralStore()
+const router = useRouter()
 
-    // Methods
-    const onAllUploaded = async (files: FileUploadedModel[]) => {
-      for (const file of files) {
-        if (!file.success) {
-          continue
-        }
-
-        try {
-          await stubStore.addStubs(file.result)
-          success(vsprintf(translate('uploadStubs.stubsInFileAddedSuccessfully'), [file.filename]))
-        } catch (e) {
-          handleHttpError(e)
-        }
-      }
-
-      generalStore.doHideLoader()
-      await router.push({ name: 'Stubs' })
-    }
-    const beforeUpload = () => {
-      generalStore.doShowLoader()
+// Methods
+const onAllUploaded = async (files: FileUploadedModel[]) => {
+  for (const file of files) {
+    if (!file.success) {
+      continue
     }
 
-    return { onAllUploaded, beforeUpload }
-  },
-})
+    try {
+      await stubStore.addStubs(file.result)
+      success(vsprintf(translate('uploadStubs.stubsInFileAddedSuccessfully'), [file.filename]))
+    } catch (e) {
+      handleHttpError(e)
+    }
+  }
+
+  generalStore.doHideLoader()
+  await router.push({ name: 'Stubs' })
+}
+const beforeUpload = () => {
+  generalStore.doShowLoader()
+}
 </script>
