5d36bb0ce434f4a40c41e33271a0426dca5a8a42
Duco <git@ducode.org>
2025-03-09 13:19:25 +0100

    Refactored CodeHighlight

--- gui/src/components/hljs/CodeHighlight.vue
+++ gui/src/components/hljs/CodeHighlight.vue
@@ -2,51 +2,44 @@
   <pre ref="codeBlock" :class="languageClass">{{ code }}</pre>
 </template>
 
-<script lang="ts">
-import hljs from 'highlight.js/lib/core'
+<script setup lang="ts">
+import hljs from 'highlight.js'
 import { computed, onMounted, ref, watch } from 'vue'
-import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'CodeHighlight',
-  props: {
-    language: {
-      type: String,
-      default: 'plaintext',
-    },
-    code: {
-      type: String,
-      required: true,
-    },
+const props = defineProps({
+  language: {
+    type: String,
+    default: 'plaintext',
   },
-  setup(props) {
-    // Refs
-    const codeBlock = ref<HTMLElement>()
-
-    // Computed
-    const languageClass = computed(() => props.language)
-
-    // Functions
-    const reloadCode = () => {
-      setTimeout(() => {
-        if (codeBlock.value) {
-          hljs.highlightElement(codeBlock.value)
-        }
-      }, 10)
-    }
-
-    // Lifecycle
-    onMounted(() => reloadCode())
-
-    // Watch
-    watch(
-      () => props.code,
-      () => reloadCode(),
-    )
-
-    return { codeBlock, languageClass }
+  code: {
+    type: String,
+    required: true,
   },
 })
+
+// Data
+const codeBlock = ref<HTMLElement>()
+
+// Computed
+const languageClass = computed(() => props.language)
+
+// Functions
+const reloadCode = () => {
+  setTimeout(() => {
+    if (codeBlock.value) {
+      hljs.highlightElement(codeBlock.value)
+    }
+  }, 10)
+}
+
+// Lifecycle
+onMounted(() => reloadCode())
+
+// Watch
+watch(
+  () => props.code,
+  () => reloadCode(),
+)
 </script>
 
 <style scoped></style>
